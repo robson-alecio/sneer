5adf504b8c404f771b034cb2a0dcf07c8643eb15
rodrigob <rodrigob@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2005-05-12 06:11:00 +0000

    addContact
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@22 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/main/sneer/Sneer.java
+++ sneer/src/main/sneer/Sneer.java
@@ -1,21 +1,59 @@
 package sneer;
 
+import java.io.IOException;
+
+import org.prevayler.foundation.network.ObjectSocket;
+import org.prevayler.foundation.network.OldNetwork;
+import org.prevayler.foundation.network.OldNetworkImpl;
+
+import sneer.remote.RemoteLife;
+
 public class Sneer {
 	
 	public interface User {
-		String promptName();
+		String name();
+
+		String giveNickname();
+
+		String informTcpAddress();
+
+		void lamentException(IOException e);
+
+		void checkOutNewContacts();
 	}
 	
 	private final Life _life;
 	private final User _user;
+	private final OldNetwork _network = new OldNetworkImpl();
 
 	public Sneer(User user) {
 		if (null == user) throw new IllegalArgumentException();
 		_user = user;
-		_life = new LifeImpl(_user.promptName());
+		_life = new LifeImpl(_user.name());
 	}
 	
 	public Life life() {
 		return _life;
+	}
+
+	public void addContact() {
+		String nickname = _user.giveNickname();
+		String tcpAddress = _user.informTcpAddress();
+		String[] addressParts = tcpAddress.split(":");
+		String ipAddress = addressParts[0];
+		int port = 5901;
+		if (addressParts.length > 1) {
+			port = Integer.parseInt(addressParts[1]);
+		}
+		ObjectSocket objectSocket;
+		try {
+			objectSocket = _network.openSocket(ipAddress, port);
+		} catch (IOException e) {
+			_user.lamentException(e);
+			return;
+		}
+		LifeView contact = RemoteLife.createWith("ignored", objectSocket);
+		_life.giveSomebodyANickname(contact, nickname);
+		_user.checkOutNewContacts();
 	}
 }

7dd5485becf17f38d9bcb093f7113da70bcfba50
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2005-07-16 13:33:49 +0000

    Copyright notice added.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@65 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer.ui/src/sneer/ui/EclipseSneerUser.java
+++ sneer.ui/src/sneer/ui/EclipseSneerUser.java
@@ -1,3 +1,6 @@
+//Copyright (C) 2005 Klaus Wuestefeld and Rodrigo B. de Oliveira
+//This is free software. It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the license distributed along with this file for more details.
+
 package sneer.ui;
 
 
--- sneer.ui/src/sneer/ui/SneerUIPlugin.java
+++ sneer.ui/src/sneer/ui/SneerUIPlugin.java
@@ -1,3 +1,6 @@
+//Copyright (C) 2005 Klaus Wuestefeld and Rodrigo B. de Oliveira
+//This is free software. It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the license distributed along with this file for more details.
+
 package sneer.ui;
 
 import java.util.MissingResourceException;
@@ -38,7 +41,7 @@ public class SneerUIPlugin extends AbstractUIPlugin {
 		
 		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
 		_user = new EclipseSneerUser(shell);
-		_sneer = new Sneer(_user);		
+		_sneer = new Sneer(_user);
 	}
 
 	/**

eea03bf19770e9ef7324de4d1c569c01fabd51db
humbertosoares <humbertosoares@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2005-08-03 15:52:33 +0000

    little mistakes in todo
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@78 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/main/sneer/life/View.java
+++ sneer/src/main/sneer/life/View.java
@@ -6,7 +6,7 @@ public interface View {
 
 	/**
 	 * Returns the Date this View was seen for the last time. If this returns
-	 * null, all other values in the View are undefined (thay can be null).
+	 * null, all other values in the View are undefined (they can be null).
 	 */
 	Date lastSightingDate();
 

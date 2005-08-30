f386908a86081bd26694b5d97abed3a38c97dbc3
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2005-08-30 11:47:11 +0000

    Refactoring: Views moved to wheel.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@114 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/main/wheelexperiments/views/SetView.java
+++ sneer/src/main/wheelexperiments/views/SetView.java
@@ -2,7 +2,7 @@
 //This is free software. It is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the license distributed along with this file for more details.
 //Contributions: Kalecser Kurtz, Fabio Roger Manera.
 
-package views;
+package wheelexperiments.views;
 
 
 public interface SetView<T>  {

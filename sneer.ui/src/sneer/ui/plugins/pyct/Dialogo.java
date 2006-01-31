6ccaa7deee6415cf5ffb14ee7e4f7e705b6a4a25
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-01-31 01:52:22 +0000

    Cleaning up.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@238 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer.ui/src/sneer/ui/plugins/pyct/Dialogo.java
@@ -0,0 +1,27 @@
+package sneer.ui.plugins.pyct;
+
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+public class Dialogo {
+
+public void Abrir() {
+	String salva;
+	Display display = new Display ();
+	Shell shell = new Shell (display);
+	shell.open ();
+	FileDialog dialog = new FileDialog (shell, SWT.SAVE);
+	dialog.setFilterNames (new String [] {"Batch Files", "All Files (*.*)"});
+	dialog.setFilterExtensions (new String [] {"*.bat", "*.*"}); //Windows wild cards
+	dialog.setFilterPath ("c:\\"); //Windows path
+	dialog.setFileName ("fred.bat");
+	salva = dialog.open();
+	System.out.println ("Save to: " + dialog.open ());
+	System.out.println ("Save to: " + salva);
+	while (!shell.isDisposed ()) {
+		if (!display.readAndDispatch ()) display.sleep ();
+	}
+	display.dispose ();
+}
+} 
---
+++ sneer.ui/src/sneer/ui/plugins/pyct/Runner.java
@@ -0,0 +1,15 @@
+package sneer.ui.plugins.pyct;
+
+import java.io.IOException;
+
+
+public class Runner {
+
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	
+		Home obj = new Home();
+		obj.TelaPincipal();
+	}
+
+}

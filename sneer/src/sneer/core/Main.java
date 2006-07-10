a73c800be1a32fc181fb4cbeb11bfcc8309b894b
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-07-10 13:38:48 +0000

    Bootstrap http dropped.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@315 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/src/sneer/core/Main.java
@@ -0,0 +1,13 @@
+package sneer.core;
+
+import java.io.IOException;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {		
+		System.out.println("Start MainApplication!");
+		VirusReplicator.initialize();
+	}
+}
--- sneer/src/sneer/core/VirusReplicator.java
+++ sneer/src/sneer/core/VirusReplicator.java
@@ -1,13 +1,17 @@
-/**
- * 
- */
 package sneer.core;
 
+
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-final class VirusReplicator implements Runnable {
+final public class VirusReplicator implements Runnable {
 	public void run() 	{
 		try {
 			replicate();
@@ -22,11 +26,47 @@ final class VirusReplicator implements Runnable {
 			while (true) {		
 				Socket socket = server.accept();
 				System.out.println("Virus replication sequence started...");
-				SovereignVirus.writeJar(socket);
+				VirusReplicator.writeJar(socket);
 				System.out.println("Virus successfully replicated.");
 			}
 		} finally {
 			server.close();
 		}
+	}
+
+	static File mainAppJarFile() {
+		String path =
+			System.getProperty("user.home") + File.separator +
+			".sneer" + File.separator +
+			"application" + File.separator +
+			"MainApplication.zip";
+		return new File(path);
+	}
+
+	public static void initialize() throws IOException {		
+		startDaemon(new VirusReplicator());
+		System.out.println("Press certain keys to quit");
+		System.in.read();
+	}
+
+	private static void startDaemon(Runnable closure) {
+		Thread thread = new Thread(closure);
+		thread.setDaemon(true);
+		thread.start();
+	}
+
+	private static void writeObject(Socket socket, Object o) throws IOException {
+		OutputStream sos = socket.getOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(sos);		
+		oos.writeObject(o);
+		oos.flush();
+	}
+
+	static void writeJar(Socket socket) throws IOException {		
+		File zip = mainAppJarFile();
+		DataInputStream di = new DataInputStream(new FileInputStream(zip));
+		byte tmp[] =  new byte[di.available()];
+		di.readFully(tmp);
+		writeObject(socket, tmp);
 	}
 }

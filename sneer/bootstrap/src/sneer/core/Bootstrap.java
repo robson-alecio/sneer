45d2e4af792e4722815e2625c5837e8d36f7bdb6
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-07-20 21:59:30 +0000

    Bootstrap - work in progress
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@316 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bootstrap/src/sneer/core/Bootstrap.java
+++ sneer/bootstrap/src/sneer/core/Bootstrap.java
@@ -1,168 +1,290 @@
 package sneer.core;
 
-import static org.eclipse.jdt.internal.compiler.batch.Main.compile;
-
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.swing.JOptionPane;
 
 public class Bootstrap {
 	
 	private static final int DEFAULT_DOWNLOAD_PORT = 42;
+	private static Socket _socket;
+	private static ObjectInputStream _objectInput;
 
 	public static void main(String[] ignored) {
 		try {
 			boot();
-		} catch (Exception e) {			
-			showExceptionDialog(e);
+		} catch (Throwable t) {			
+			showErrorDialog(t);
 		}
 	}
 
 	private static void boot() throws Exception {
-		downloadMainAppIfNecessary();
-		compileSourceIfNecessary();
+		acquireMainAppIfNecessary();
 		executeMainApp();
 	}
 
-
-	private static void downloadMainAppIfNecessary() throws Exception {
-		if (isMainAppAlreadyDownloaded()) return;
-		downloadMainApp();
+	private static void acquireMainAppIfNecessary() throws Exception {
+		valid replacement? replace
+		valid main return
+		download replacement
+		goto top
+		
+		
+		replaceMainAppWithReplacementIfAvailable();
+		acquireMainAppFromPeerIfNecessary();
 	}
 
-	private static boolean isMainAppAlreadyDownloaded() {
-		return mainAppJarFile().exists();
+	private static void replaceMainAppWithReplacementIfAvailable() throws Exception {
+		if (!mainAppReplacementJarFile().exists()) return;
+		
+		if (!checkSignature(mainAppReplacementJarFile())) return;
+		
+		replaceMainAppWithReplacement();
 	}
 
-	private static void compileSourceIfNecessary() throws IOException{
-		File chook = new File(System.getProperty("user.home") + 
-						 File.separator + ".sneer" + 
-						 File.separator + "CompilerHook.java");
-		File dest = new File(chook.getParentFile(),"application/class" );
-//		File jar = new File(chook.getParentFile(),"application/MainApplication.jar");
-		if(dest.exists()) return;
-//		if(jar.exists()) return;
-		
-		dest.mkdirs();
-		FileWriter fw = null;
-		
-		try {
-			fw = new FileWriter(chook);
-			fw.write("public class CompilerHook extends Main {}");
-		} finally{
-			if(fw!=null){
-				fw.close();
-			}			
+	private static void replaceMainAppWithReplacement() throws Exception {
+		mainAppJarFile().delete();
+		boolean done = mainAppReplacementJarFile().renameTo(mainAppJarFile());
+		if (!done) throw new Exception("Unable to replace main application with newer version.");
+	}
+
+	private static void acquireMainAppFromPeer() throws Exception {
+		try{
+			openConnectionToPeer();
+			receiveMainApp();
+		} finally {
+			closeConnectionToPeer();
 		}
-		
-		String cmd = "-source 1.6 -target 1.6 " +
-				" -d \"" + dest.getAbsolutePath() + 
-				"\" -sourcepath \"" + mainAppJarFile().getAbsolutePath() + 
-				"\" \"" + chook.getAbsolutePath() + "\"";
-		System.out.println(cmd);
-		compile(cmd);
-		
-//		FileOutputStream os = new FileOutputStream(jar);
-//		JarOutputStream jos = new JarOutputStream(os, manifest());
-//		addJarEntries(jar, jos);
-//		jos.close();		
 	}
 
-//	private static void addJarEntries(File dir, JarOutputStream jos){
-//		File files[] = dir.listFiles();
-//		
-//		for (File file : files) {
-//			if(file.isDirectory()){
-//				addJarEntries(file, jos);
-//			}else{
-//				ZipEntry entry = new ZipEntry(resourceName(clazz));		
+	private static void compileMainApp() throws Exception {
+		delete(tempDirectory());
+		extractMainAppSource();
+		compileMainAppSource();
+	}
+
+	private static void compileMainAppSource() throws Exception {
+		File dest = new File(tempDirectory(), "classes");
+		dest.mkdir();
+
+		execute(compilerJar(), 
+			"-source", "1.6",
+			"-target", "1.6",
+			"-d", dest.getAbsolutePath(),
+			sourceDirectory().getAbsolutePath()
+		);
+
+		
+//FileOutputStream os = new FileOutputStream(jar);
+//JarOutputStream jos = new JarOutputStream(os, manifest());
+//addJarEntries(jar, jos);
+//jos.close();		
+
+	}
+
+
+
+	private static void execute(File jar, String... args) throws Exception {
+		executeClass(jar, mainClass(jar), args);
+	}
+
+	private static String mainClass(File jar) throws IOException {
+		return new JarFile(jar).getManifest().getMainAttributes().getValue(Attributes.Name.MAIN_CLASS);
+	}
+
+	private static File sourceDirectory() {
+		File result = new File(tempDirectory(), "sources");
+		result.mkdir();
+		return result;
+	}
+
+	private static void delete(File file) throws IOException {
+		if (file.isDirectory()) {
+			for (File subFile : file.listFiles()) delete(subFile);
+			return;
+		}
+		if (!file.delete()) throw new IOException("Unable to delete file " + file);
+	}
+
+	private static void extractMainAppSource() throws Exception {
+		ZipFile sources = new ZipFile(mainAppSourceFile());
+		Enumeration<? extends ZipEntry> entries = sources.entries();
+		while (entries.hasMoreElements()) {
+			extractMainAppSourceEntry(sources, entries.nextElement());
+		}
+	}
+
+	private static void extractMainAppSourceEntry(ZipFile sources, ZipEntry entry) throws IOException {
+		if (entry.isDirectory()) return;
+		
+		InputStream inputStream = sources.getInputStream(entry);
+		byte[] buffer = new byte[1024*4];
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		int read;
+		while (-1 != (read = inputStream.read(buffer))) {
+			bos.write(buffer, 0, read);
+		}
+		inputStream.close();
+		
+		File sourceFile = new File(sourceDirectory(), entry.getName());
+		save(sourceFile, bos.toByteArray());
+	}
+
+	private static File tempDirectory() {
+		File result = new File(sneerDirectory(), "temp");
+		result.mkdir();
+		return result;
+	}
+
+	private static void receiveMainApp() throws Exception {
+		byte[] jarContents = receiveByteArray();
+		byte[] signature = receiveByteArray();
+		
+		saveSignedFile(mainAppReplacementJarFile(), jarContents, signature);
+		replaceMainAppWithReplacementIfAvailable();
+	}
+
+
+
+	private static void saveSignedFile(File file, byte[] contents, byte[] signature) throws IOException {
+		File signatureFile = new File(file.getAbsolutePath() + ".signature");
+		save(file, contents);
+		save(signatureFile, signature);
+	}
+
+	private static void checkSignature(byte[] jarContents, byte[] signature) {
+		System.err.println("Emilio, please provide Public-Key verification code.");
+	}
+
+	private static File mainAppSourceFile() {
+		return new File(sneerDirectory(), "MainApplication.zip");
+	}
+	
+	private static File mainAppJarFile() {
+		return new File(sneerDirectory(), "MainApplication.jar");
+	}
+	
+	private static File mainAppReplacementJarFile() {
+		return new File(sneerDirectory(), "MainApplicationReplacement.jar");
+	}
+
+
+	private static void closeConnectionToPeer() throws IOException {
+		if (_objectInput != null) _objectInput.close();
+		if (_socket != null) _socket.close();
+	}
+
+	private static void receiveCompiler() throws Exception {
+		receiveFileContents(compilerJar());
+	}
+
+	private static File compilerJar() {
+		return new File(sneerDirectory(), "compiler.jar");
+	}
+
+	private static void receiveFileContents(File file) throws Exception {
+		save(file, receiveByteArray());
+	}
+
+	private static byte[] receiveByteArray() throws Exception {
+		return (byte[])_objectInput.readObject();
+	}
+
+	private static File sneerDirectory() {
+		File result = new File(System.getProperty("user.home"), ".sneer");
+		result.mkdir();
+		return result;
+	}
+
+	private static void openConnectionToPeer() throws IOException {
+		String address = promptForHostnameAndPort();
+		_socket = new Socket(hostGiven(address), portGiven(address));
+		_objectInput = new ObjectInputStream(_socket.getInputStream());
+		
+		ObjectOutputStream output = new ObjectOutputStream(_socket.getOutputStream());
+		output.writeObject("Bootstrap");
+	}
+
+
+	private static void acquireMainAppFromPeerIfNecessary() throws Exception {
+		if (mainAppJarFile().exists()) return;
+		
+		acquireMainAppFromPeer();
+	}
+
+
+	private static void addJarEntries(File dir, JarOutputStream jos){
+		File files[] = dir.listFiles();
+		
+		for (File file : files) {
+			if(file.isDirectory()){
+				addJarEntries(file, jos);
+			} else {
+//				ZipEntry entry = new ZipEntry(resourceName(clazz));	
 //				jos.putNextEntry(entry);
 //				jos.write(readClassBytes(clazz));
 //				jos.closeEntry();			
-//			}
-//		}		
-//	}
-//	
-//	
-//	
-//	private static Manifest manifest() {
-//		Manifest m = new Manifest();		
-//		Attributes attributes = m.getMainAttributes();
-//		attributes.put(Attributes.Name.MANIFEST_VERSION, "1.0");
-//		attributes.put(Attributes.Name.MAIN_CLASS, "Main");
-//		return m;
-//	}	
-//	
-	private static File mainAppJarFile() {
-		String path =
-			System.getProperty("user.home") + File.separator +
-			".sneer" + File.separator +
-			"application" + File.separator +
-			"MainApplication.zip";
-		File tmp = new File(path);
-		tmp.getParentFile().mkdirs();
-		return tmp;
+			}
+		}		
 	}
 
+
 	private static void executeMainApp() throws Exception {
-		File jar = mainAppJarFile();
-		Class<?> clazz = new URLClassLoader(new URL[] { jar.toURI().toURL() }).loadClass("Main");
-		clazz.getMethod("main", new Class[] { String[].class }).invoke(null, new Object[] { new String[0] });
+		executeClass(mainAppJarFile(), "Main");
+	}
+
+	private static void executeClass(File jar, String className, String... args) throws ClassNotFoundException, MalformedURLException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		Class<?> clazz = new URLClassLoader(new URL[] { jar.toURI().toURL() }).loadClass(className);
+		clazz.getMethod("main", new Class[] { String[].class }).invoke(null, new Object[] { args });
 	}
 	
-	private static void downloadMainApp() throws Exception {
-		String hostnameAndPort = askHostnameAndPort();
-		
-		Socket socket = new Socket(host(hostnameAndPort), port(hostnameAndPort));
-		byte[] newVersion = null;
-		try {
-			newVersion = (byte[])new ObjectInputStream(socket.getInputStream()).readObject();
-		} finally {
-			socket.close();
-		}
-		saveApp(newVersion);
-	}
-	
-	public static String host(String s) {
+	public static String hostGiven(String s) {
 		String[] addressParts = s.split(":");
 		return addressParts[0];
 	}
 	
-	public static int port(String s) {
+	public static int portGiven(String s) {
 		String[] addressParts = s.split(":");
 		return addressParts.length > 1 
 			? Integer.parseInt(addressParts[1])
 			: DEFAULT_DOWNLOAD_PORT;
 	}
 
-	private static String askHostnameAndPort() {
+	private static String promptForHostnameAndPort() {
 		return JOptionPane.showInputDialog(null, "What is your contact's address? host:port", "localhost:42");
 	}
 
-	private static void saveApp(byte[] newVersion) throws IOException {
-		File directory = mainAppJarFile().getParentFile();
-		directory.mkdirs();
-		FileOutputStream fos = new FileOutputStream(mainAppJarFile());
+	private static void save(File file, byte[] contents) throws IOException {
+		FileOutputStream fos = new FileOutputStream(file);
 		try {
-			fos.write(newVersion);
+			fos.write(contents);
 		} finally {
 			fos.close();
 		}
 	}
 
-	private static void showExceptionDialog(Exception e) {
+	private static void showErrorDialog(Throwable t) {
 		StringWriter message = new StringWriter();
-		message.write(e.toString() + "\n");
-		e.printStackTrace(new PrintWriter(message));
+		t.printStackTrace(new PrintWriter(message));
 		JOptionPane.showMessageDialog(null, message.toString(), "ERROR", JOptionPane.ERROR_MESSAGE);
 	}
 }

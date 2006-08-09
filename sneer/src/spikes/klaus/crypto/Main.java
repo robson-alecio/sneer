eb866a26f079d0b787fe0b08eaf747d43e35aecf
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-08-09 02:40:57 +0000

    Minor work on the crypto spikes.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@331 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/spikes/klaus/crypto/Main.java
+++ sneer/src/spikes/klaus/crypto/Main.java
@@ -7,45 +7,27 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Signature;
-import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-
 		byte[] bytecodeDummy = new SecureRandom().generateSeed(10000000);
 		
 		testSHA512(bytecodeDummy);
-		
 		testRSA(bytecodeDummy);
-
 	}
 
 	private static void testSHA512(byte[] bytecodeDummy) throws Exception {
 		MessageDigest digester = MessageDigest.getInstance("SHA-512", "SUN");
-		byte[] digest1 = digester.digest(bytecodeDummy);
-
-		MessageDigest digester2 = MessageDigest.getInstance("SHA-512", "SUN");
-		byte[] digest2 = digester2.digest(bytecodeDummy);
-		
-		if (!Arrays.equals(digest1, digest2)) throw new Exception("Sorry");
+		byte[] digest = digester.digest(bytecodeDummy);
+		System.out.println("Digest length: " + digest.length * 8 + " bits");
 	}
 
 	private static void testRSA(byte[] bytecodeDummy) throws Exception {
-		long t0;
-		
-		t0 = System.currentTimeMillis();
 		KeyPair keys = generateKeyPair();
-		System.out.println(System.currentTimeMillis() - t0);
-		
-		t0 = System.currentTimeMillis();
 		byte[] signature = generateSignature(keys.getPrivate(), bytecodeDummy);
-		System.out.println(System.currentTimeMillis() - t0);
-		
-		t0 = System.currentTimeMillis();
 		boolean ok = verifySignature(keys.getPublic(), bytecodeDummy, signature);
-		System.out.println("Signature ok: " + ok);
-		System.out.println(System.currentTimeMillis() - t0);
+		System.out.println("RSA Signature ok: " + ok);
 	}
 
 	public static KeyPair generateKeyPair() throws Exception {

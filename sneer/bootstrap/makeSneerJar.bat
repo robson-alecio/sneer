c104434244ff94cc7cf221884151210ba19ff9a5
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-12-14 22:34:06 +0000

    makeSneerJar.bat done
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@412 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bootstrap/makeSneerJar.bat
+++ sneer/bootstrap/makeSneerJar.bat
@@ -1,3 +1,7 @@
 rmdir ..\bin\sneerjartemp /s /q
 mkdir ..\bin\sneerjartemp
-javac -sourcepath .\src;..\src;..\..\wheel\src -d ..\bin\sneerjartemp .\src\sneer\boot\Boot.java
+javac -encoding UTF-8 -sourcepath .\src;..\src;..\..\wheel\src -d ..\bin\sneerjartemp .\src\sneer\boot\Boot.java
+jar cfe ..\bin\Sneer.jar sneer.boot.Boot -C ..\bin\sneerjartemp .
+rmdir ..\bin\sneerjartemp /s /q
+
+

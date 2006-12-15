71301b5804c6ff92bcb2bf763ba412bc3ce65fbf
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-12-15 11:11:15 +0000

    testing server
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@418 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bootstrap/src/sneer/boot/VersionUpdateAgent.java
+++ sneer/bootstrap/src/sneer/boot/VersionUpdateAgent.java
@@ -35,8 +35,6 @@ public class VersionUpdateAgent implements Agent {
 
 	
 	private Command commandToSend() throws IOException {
-		if (1==1) return noNewVersionCommand();
-		
 		File mainApp = newestMainApp();
 		if (mainApp == null) {
 			Log.log("No mainApp files found.");

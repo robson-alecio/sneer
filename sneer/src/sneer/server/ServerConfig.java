e4eebbdc84c3c6a73a975c8bda00d2c4c200ba63
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2006-12-17 06:16:12 +0000

    testing mainapps directory
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@431 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/server/ServerConfig.java
+++ sneer/src/sneer/server/ServerConfig.java
@@ -7,10 +7,6 @@ public class ServerConfig {
 	static public final String HOST = "sovereigncomputing.net";
 	static public final int PORT = 22086;
 	
-	static public final File MAIN_APP_DIRECTORY = new File(sneerProjectDirectory(), "mainapps");
-
-	private static File sneerProjectDirectory() {   //The server runs in .../sneer/bin
-		return new File("whatever").getParentFile().getParentFile();  //Returns .../sneer
-	}
+	static public final File MAIN_APP_DIRECTORY = new File("../mainapps");
 
 }

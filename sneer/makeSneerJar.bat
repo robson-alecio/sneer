ff6104ef55ef6a9cf019d6ace1e20197bd230bc4
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-01-22 22:13:56 +0000

    Strap classes are now garbage collected
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@457 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/makeSneerJar.bat
+++ sneer/makeSneerJar.bat
@@ -1,12 +1,10 @@
 rmdir .\bin\sneerjartemp /s /q
 mkdir .\bin\sneerjartemp
 
-mkdir .\bin\sneerjartemp\strapjartemp
-javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src;..\wheel\src -d .\bin\sneerjartemp\strapjartemp .\src\sneer\strap\Strap.java
-jar cf .\bin\sneerjartemp\strap.jar -C .\bin\sneerjartemp\strapjartemp .
-rmdir .\bin\sneerjartemp\strapjartemp /s /q
-
-javac -target 1.1 -source 1.2 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\Boot.java
+javac -target 1.1 -source 1.2 -encoding UTF-8 -sourcepath .\src              -d .\bin\sneerjartemp .\src\Boot.java
+javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src;..\wheel\src -d .\bin\sneerjartemp .\src\sneer\strap\Strap.java
 jar cfe .\bin\Sneer.jar Boot -C .\bin\sneerjartemp .
+
 rmdir .\bin\sneerjartemp /s /q
 
+java -jar .\bin\Sneer.jar

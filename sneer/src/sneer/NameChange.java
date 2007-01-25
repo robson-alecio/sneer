456864164b612600095b4348f234fbb36ef8cafb
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-01-25 06:36:41 +0000

    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@476 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/src/sneer/NameChange.java
@@ -0,0 +1,54 @@
+package sneer;
+
+import static sneer.strap.SneerDirectories.sneerDirectory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import sneer.strap.Dialog;
+import wheelexperiments.environment.ui.User;
+
+public class NameChange {
+
+	NameChange(User user, boolean forceChange) throws FileNotFoundException {
+		if (hasName() && !forceChange) return;
+		
+		changeOwnName(user);
+	}
+	
+	private void changeOwnName(User user) throws FileNotFoundException {
+		String name = user.answer(" Digite seu nome", name());
+		if (name == null || name.trim().isEmpty()) return;
+		name = name.trim();
+	
+		saveName(name);
+	}
+
+	private boolean hasName() {
+		return name() != null;
+	}
+
+	private String name()  {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(nameFile()));
+			return reader.readLine().trim();
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	private File nameFile() {
+		return new File(sneerDirectory(), "name.txt");
+	}
+
+	private void saveName(String name) throws FileNotFoundException {
+		PrintWriter writer = new PrintWriter(nameFile());
+		writer.println(name);
+		writer.close();
+	}
+
+}

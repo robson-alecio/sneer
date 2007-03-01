d92ee0231607f445d91c8a1b7785a58760d48ce5
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-03-01 03:41:29 +0000

    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@488 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/spikes/klaus/ingredients/Ingredient.java
+++
@@ -1,5 +0,0 @@
-package spikes.klaus.ingredients;
-
-public interface Ingredient {
-
-}
--- sneer/src/spikes/klaus/ingredients/impl/IngredientImpl.java
+++
@@ -1,7 +0,0 @@
-package spikes.klaus.ingredients.impl;
-
-import spikes.klaus.ingredients.Ingredient;
-
-public class IngredientImpl implements Ingredient {
-
-}

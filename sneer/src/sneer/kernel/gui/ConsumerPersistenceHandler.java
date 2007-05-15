61bfd8d2b1ce1055ef7b1cd1c7374e6b0fcf14fe
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-05-15 01:22:41 +0000

    Minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@582 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/kernel/gui/ConsumerPersistenceHandler.java
+++ sneer/src/sneer/kernel/gui/ConsumerPersistenceHandler.java
@@ -5,20 +5,20 @@ import java.lang.reflect.Method;
 
 import org.prevayler.Prevayler;
 
-public class ConsumerPersistenceHandler implements InvocationHandler { //Refactor: consider implementing Consumer to be warned when its definition changes.
+public class ConsumerPersistenceHandler implements InvocationHandler { //Refactor: consider adding "implements Consumer" to be warned by the compiler when the definition of the interface changes.
 
-	private final String _consumerGetter;
+	private final String _consumerGetterMethodName;
 	private final Prevayler _prevayler;
 
-	public ConsumerPersistenceHandler(Prevayler prevayler, String consumerGetter) {
-		_consumerGetter = consumerGetter;
+	public ConsumerPersistenceHandler(Prevayler prevayler, String consumerGetterMethodName) {
+		_consumerGetterMethodName = consumerGetterMethodName;
 		_prevayler = prevayler;
 	}
 
 	public Object invoke(Object proxyImplied, Method methodImplied, Object[] args)	throws Throwable {
 		if(!methodImplied.getName().equals("consume")) throw new UnsupportedOperationException();
 
-		_prevayler.execute(new Consumption(_consumerGetter, args[0]));
+		_prevayler.execute(new Consumption(_consumerGetterMethodName, args[0]));
 		
 		return null;
 	}

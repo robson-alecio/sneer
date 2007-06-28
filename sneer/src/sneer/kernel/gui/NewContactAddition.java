48f718b9a398bba37879b00835cdcd796560a224
gandhi <gandhi@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-06-28 01:56:35 +0000

    i18n translation reverted
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@663 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/src/sneer/kernel/gui/NewContactAddition.java
@@ -0,0 +1,45 @@
+package sneer.kernel.gui;
+
+import java.util.Date;
+
+import org.prevayler.Transaction;
+
+import sneer.kernel.business.BusinessSource;
+import sneer.kernel.business.contacts.ContactInfo;
+
+import wheel.io.network.PortNumberSource;
+import wheel.io.ui.CancelledByUser;
+import wheel.io.ui.User;
+import wheel.io.ui.impl.ValueChangePane;
+import wheel.lang.Consumer;
+import wheel.lang.IntegerParser;
+import wheel.lang.exceptions.IllegalParameter;
+import wheel.lang.exceptions.NotImplementedYet;
+
+public class NewContactAddition {
+
+	public NewContactAddition(User user, Consumer<ContactInfo> contactAdder) throws CancelledByUser {
+		while (true) {
+			String nick = user.answer("New contact's nickname");
+			String host = user.answer("Host Address for " + nick, nick + ".dyndns.org");
+
+			int port = port(user, nick);
+			
+			try {
+				contactAdder.consume(new ContactInfo(nick, host, port));
+				return;
+			} catch (IllegalParameter e) {
+				user.acknowledgeNotification(e.getMessage());
+			}
+		}
+	}
+
+	private int port(User user, String nick) throws CancelledByUser {
+
+		PortNumberSource result = new PortNumberSource(0);
+		new ValueChangePane(string("NEWCONTACT_PORT_PANEL_TITLE"), string("NEWCONTACT_PROMPT_PORT",nick), user, result.output(), new IntegerParser(result.setter())).tryToRun();
+		return result.output().currentValue();
+	}
+
+	private static final long serialVersionUID = 1L;
+}

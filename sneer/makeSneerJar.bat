87eb44904aa62c9dd29243fdba935dc34d285ff0
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-07-10 04:21:09 +0000

    makeSneerJar.bat done
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@727 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/makeSneerJar.bat
+++ sneer/makeSneerJar.bat
@@ -1,23 +1,27 @@
 rmdir .\bin\sneerjartemp /s /q
 mkdir .\bin\sneerjartemp
 
-md .\bin\sneerjartemp\sneer\
-md .\bin\sneerjartemp\sneer\lib
-md .\bin\sneerjartemp\sneer\lib\xstream
-copy .\lib\*.* .\bin\sneerjartemp\sneer\lib
-copy .\lib\xstream\*.* .\bin\sneerjartemp\sneer\lib\xstream
+xcopy .\lib\prevayler-2.3\*.* .\bin\sneerjartemp /S
+xcopy .\lib\xstream\xstream-1.2.2\*.* .\bin\sneerjartemp /S
+xcopy .\lib\xstream\xpp3_min-1.1.3.4.O\*.* .\bin\sneerjartemp /S
 
+javac -target 1.1 -source 1.2 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\sneer\Boot.java
+javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\sneer\SneerJockey.java
+javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp -cp .\bin\sneerjartemp .\src\sneer\Sneer.java
+
+copy .\src\Translation*.* .\bin\sneerjartemp
+
+md .\bin\sneerjartemp\sneer\
 md .\bin\sneerjartemp\sneer\kernel
 md .\bin\sneerjartemp\sneer\kernel\gui
+md .\bin\sneerjartemp\sneer\kernel\gui\traymenu
+copy .\src\sneer\kernel\gui\traymenu\*.png .\bin\sneerjartemp\sneer\kernel\gui\traymenu
+
 md .\bin\sneerjartemp\sneer\kernel\gui\contacts
 md .\bin\sneerjartemp\sneer\kernel\gui\contacts\images
 copy .\src\sneer\kernel\gui\contacts\images\*.gif .\bin\sneerjartemp\sneer\kernel\gui\contacts\images
 
-javac -target 1.1 -source 1.2 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\sneer\Boot.java
-javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\sneer\SneerJockey.java
-javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp -cp .\bin\sneerjartemp\sneer\lib\prevayler-2.3.jar;.\bin\sneerjartemp\sneer\lib\xstream\xstream-1.2.2.jar .\src\sneer\Sneer.java
-
-jar cfem .\bin\Sneer.jar sneer.Boot makeSneerJar.manifest -C .\bin\sneerjartemp .
+jar cfe .\bin\Sneer.jar sneer.Boot -C .\bin\sneerjartemp .
 rmdir .\bin\sneerjartemp /s /q
 
 rmdir c:\sneerteste /s /q

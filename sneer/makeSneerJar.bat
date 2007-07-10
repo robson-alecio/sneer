9dfd5688424330f9661db15f0fcfe1f899badbf8
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-07-10 07:54:30 +0000

    Buildpath changes
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@731 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/makeSneerJar.bat
+++ sneer/makeSneerJar.bat
@@ -1,13 +1,9 @@
 rmdir .\bin\sneerjartemp /s /q
 mkdir .\bin\sneerjartemp
 
-xcopy .\lib\prevayler-2.3\*.* .\bin\sneerjartemp /S
-xcopy .\lib\xstream\xstream-1.2.2\*.* .\bin\sneerjartemp /S
-xcopy .\lib\xstream\xpp3_min-1.1.3.4.O\*.* .\bin\sneerjartemp /S
-
 javac -target 1.1 -source 1.2 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\sneer\Boot.java
 javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp .\src\sneer\SneerJockey.java
-javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp -cp .\bin\sneerjartemp .\src\sneer\Sneer.java
+javac -target 1.6 -source 1.6 -encoding UTF-8 -sourcepath .\src -d .\bin\sneerjartemp -cp .\lib\prevayler-2.3.jar;.\lib\xstream\xstream-1.2.2.jar .\src\sneer\Sneer.java
 
 copy .\src\Translation*.* .\bin\sneerjartemp
 
@@ -21,7 +17,7 @@ md .\bin\sneerjartemp\sneer\kernel\gui\contacts
 md .\bin\sneerjartemp\sneer\kernel\gui\contacts\images
 copy .\src\sneer\kernel\gui\contacts\images\*.gif .\bin\sneerjartemp\sneer\kernel\gui\contacts\images
 
-jar cfe .\bin\Sneer.jar sneer.Boot -C .\bin\sneerjartemp .
+jar cfem .\bin\Sneer.jar sneer.Boot makeSneerJar.manifest -C .\bin\sneerjartemp .
 rmdir .\bin\sneerjartemp /s /q
 
 rmdir c:\sneerteste /s /q

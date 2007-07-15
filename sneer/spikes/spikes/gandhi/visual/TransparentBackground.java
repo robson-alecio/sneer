b08085eed6754fa6a592c8a9045b17d3676a9d39
gandhi <gandhi@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-07-15 00:40:04 +0000

    small test
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@784 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/spikes/spikes/gandhi/visual/TransparentBackground.java
@@ -0,0 +1,68 @@
+package spikes.gandhi.visual;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TransparentBackground extends JComponent {
+
+	private static final long serialVersionUID = 1L;
+
+	private JFrame _frame;
+
+	private Image background;
+
+	public TransparentBackground(JFrame frame) {
+		_frame = frame;
+		updateBackground();
+	}
+
+	public void updateBackground() {
+		try {
+			Robot rbt = new Robot();
+			Toolkit tk = Toolkit.getDefaultToolkit();
+			Dimension dim = tk.getScreenSize();
+			background = rbt.createScreenCapture(new Rectangle(0, 0, (int) dim.getWidth(), (int) dim.getHeight()));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		Point pos = this.getLocationOnScreen();
+		Point offset = new Point(-pos.x, -pos.y);
+		g.drawImage(background, offset.x, offset.y, null);
+	}
+	
+	public static void main(String[] args) {
+	    JFrame frame = new JFrame("Transparent Window");
+	    TransparentBackground bg = new TransparentBackground(frame);
+	    bg.setLayout(new BorderLayout( ));
+	    JButton button = new JButton("This is a button");
+	    bg.add("North",button);
+	JLabel label = new JLabel("This is a label");
+	    bg.add("South",label);
+	    frame.getContentPane( ).add("Center",bg);
+	    frame.pack( );
+	    frame.setSize(150,100);
+	    frame.setVisible(true);
+	}
+}

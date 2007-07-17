dbcb014c3a395fb3b6a0e9d5d50541ccfc87ec90
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-07-17 02:00:04 +0000

    Chat priority 0 Voice priority 1 File Priority 3
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@811 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/Sneer.java
+++ sneer/src/sneer/Sneer.java
@@ -84,10 +84,10 @@ public class Sneer {
 	private List<ContactAction> contactActions() {
 		List<ContactAction> result = new ArrayList<ContactAction>();
 		
-		Channel ConversationsChannel = _communicator.getChannel(ConversationsApp.class.getName(), 1);
+		Channel ConversationsChannel = _communicator.getChannel(ConversationsApp.class.getName(), 0);
 		result.add(new ConversationsApp(ConversationsChannel, _businessSource.output().contacts()).contactAction());
 		
-		Channel TalkChannel = _communicator.getChannel(TalkApp.class.getName(), 0);
+		Channel TalkChannel = _communicator.getChannel(TalkApp.class.getName(), 1);
 		result.add(new TalkApp(TalkChannel, _businessSource.output().contacts()).contactAction());
 		
 		Channel FileTransferChannel = _communicator.getChannel(FileTransferApp.class.getName(), 2);

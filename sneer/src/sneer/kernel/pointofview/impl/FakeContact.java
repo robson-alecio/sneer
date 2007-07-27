a70e5503b88c69520041b0ff324c418f21975676
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-07-27 12:53:29 +0000

    OutOfMemory problem solved: Fake contact tree created lazily.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@905 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/kernel/pointofview/impl/FakeContact.java
+++ sneer/src/sneer/kernel/pointofview/impl/FakeContact.java
@@ -1,18 +1,43 @@
 package sneer.kernel.pointofview.impl;
 
+import java.util.Date;
+import java.util.Random;
+
 import sneer.kernel.business.contacts.ContactId;
 import sneer.kernel.business.contacts.impl.ContactIdImpl;
 import sneer.kernel.pointofview.Contact;
 import sneer.kernel.pointofview.Party;
+import wheel.lang.Threads;
 import wheel.reactive.Signal;
-import wheel.reactive.impl.ConstantSignal;
+import wheel.reactive.Source;
+import wheel.reactive.impl.SourceImpl;
 
 public class FakeContact implements Contact {
 
+	private static final Random RANDOM = new Random();
+	
 	private final String _nickPrefix;
+	private FakeParty _fakeParty;
+	private final Source<String> _nick;
 
+	
 	public FakeContact(String nickPrefix) {
 		_nickPrefix = nickPrefix;
+		_nick = new SourceImpl<String>(_nickPrefix);
+		Threads.startDaemon(randomizer());
+	}
+
+	private Runnable randomizer() {
+		return new Runnable() { @Override public void run() {
+			while (true) {
+				Threads.waitWithoutInterruptions(5000 + RANDOM.nextInt(3000));
+				randomize();
+			}
+		}};
+	}
+
+	private void randomize() {
+		_nick.setter().consume(_nickPrefix + "  " + new Date());
 	}
 
 	@Override
@@ -22,12 +47,15 @@ public class FakeContact implements Contact {
 
 	@Override
 	public Signal<String> nick() {
-		return new ConstantSignal<String>(_nickPrefix);
+		return _nick.output();
 	}
 
 	@Override
 	public Party party() {
-		return new FakeParty(_nickPrefix);
+		if (_fakeParty == null)
+			_fakeParty = new FakeParty(_nickPrefix);
+
+		return _fakeParty;
 	}
 
 }

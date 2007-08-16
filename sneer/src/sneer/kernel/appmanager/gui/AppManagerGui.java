de16b7f49d1cdba0c5f412a83d182675b1cfd961
gandhi <gandhi@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-08-16 01:09:42 +0000

    minor fix gui
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@986 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/kernel/appmanager/gui/AppManagerGui.java
+++ sneer/src/sneer/kernel/appmanager/gui/AppManagerGui.java
@@ -17,7 +17,6 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.event.ListSelectionEvent;
@@ -39,30 +38,7 @@ public class AppManagerGui extends JFrame{
 		setTitle(translate("App Manager"));
 		_appList = new JList(_listModel);
 		updateList();
-		
-		final JButton installButton = new JButton(translate("Install"));
-		installButton.setMaximumSize(new Dimension(100, 30));
-		installButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-		installButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				final JFileChooser fc = new JFileChooser();
-				fc.setDialogTitle(translate("Choose App .src.jar file"));
-				int value = fc.showOpenDialog(null);
-				if (value != JFileChooser.APPROVE_OPTION) return;
-				File jarFile = fc.getSelectedFile();
-				String appName = null;
-				while(appName == null)
-					appName = JOptionPane.showInputDialog(translate("Please choose the App Name")); //Implement: use the manifest file to store the app name and version
-				try{
-					_appManager.install(appName, jarFile);
-				}catch(IOException ioe){
-					Log.log(ioe);
-					ioe.printStackTrace();
-				}
-				updateList();
-			}
-		});
-		
+
 		final JButton removeButton = new JButton(translate("Remove"));
 		removeButton.setMaximumSize(new Dimension(100, 30));
 		removeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -134,9 +110,8 @@ public class AppManagerGui extends JFrame{
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.Y_AXIS));
-		buttonPanel.add(installButton);
-		buttonPanel.add(removeButton);
 		buttonPanel.add(publishButton);
+		buttonPanel.add(removeButton);
 		add(new JScrollPane(_appList),BorderLayout.CENTER);
 		add(buttonPanel,BorderLayout.EAST);
 		setSize(300,400);

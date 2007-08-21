54d86d63153b0d81bf4e7e52e03b390f49cb53f7
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-08-21 00:40:37 +0000

    Refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1007 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/apps/metoo/MeToo.java
+++ sneer/src/sneer/apps/metoo/MeToo.java
@@ -1,20 +1,13 @@
 package sneer.apps.metoo;
 import static wheel.i18n.Language.translate;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
-import sneer.SneerDirectories;
 import sneer.apps.metoo.gui.MeTooFrame;
-import sneer.apps.metoo.packet.AppFilePart;
-import sneer.apps.metoo.packet.AppInstallRequest;
 import sneer.apps.metoo.packet.AppListResponse;
 import sneer.kernel.appmanager.AppManager;
 import sneer.kernel.appmanager.AppTools;
@@ -31,21 +24,14 @@ import wheel.reactive.impl.SourceImpl;
 import wheel.reactive.lists.ListSignal;
 
 //FixUrgent: File transfer is VERY insecure
-public class MeToo implements SovereignApplication{
-	
-	private static final int FILEPART_CHUNK_SIZE = 5000;
+public class MeToo {
 	
 	private final Channel _channel;
 	private final ListSignal<SovereignApplicationUID> _publishedApps;
-	
-	private final File _tempDirectory = AppTools.createTempDirectory("metoo");
 
-	private final AppManager _appManager;
-
-	public MeToo(Channel channel, ListSignal<SovereignApplicationUID> publishedApps, AppManager appManager){
+	public MeToo(Channel channel, ListSignal<SovereignApplicationUID> publishedApps){
 		_channel = channel;
 		_publishedApps = publishedApps;
-		_appManager = appManager;
 		_channel.input().addReceiver(meTooPacketReceiver());
 	}
 
@@ -57,65 +43,19 @@ public class MeToo implements SovereignApplication{
 					case MeTooPacket.APP_LIST_REQUEST:
 						sendAppListResponse(packet._contactId);
 						break;
-					case MeTooPacket.APP_INSTALL_REQUEST:
-						sendAppFile(packet._contactId,(AppInstallRequest)meTooPacket);
-						break;
 				}
 				SourceImpl<MeTooPacket> input = _inputsByContactId.get(packet._contactId);
 				if (input == null) return;
 				input.setter().consume(meTooPacket);
 			}
-
 		};
 	}
 	
-	protected void sendAppFile(ContactId contactId, AppInstallRequest request) {
-		File installDirectory = new File(SneerDirectories.appsDirectory(),request._installName);
-		if (installDirectory.exists())
-			if (installDirectory.listFiles().length>0){
-				File zipFile = installDirectory.listFiles()[0]; 
-				sendFile(contactId,zipFile);
-			}else{
-				System.out.println("file does not exist: "+installDirectory.getAbsolutePath());
-			}
-			else
-				System.out.println("Directory does not exist: "+installDirectory.getAbsolutePath());
-	}
-	
-	private void sendFile(final ContactId contactId, File file) {
-		try {
-			tryToSendFile(contactId, file);
-		} catch(IOException ioe) {
-			ioe.printStackTrace(); //Fix: Treat properly.
-		}
-	}
-
-	private void tryToSendFile(final ContactId contactId, File file) throws IOException {
-		String fileName = file.getName();
-		long fileLength = file.length();
-		byte[] buffer = new byte[FILEPART_CHUNK_SIZE];
-		BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-		int read;
-		long offset = 0;
-		while ((read = in.read(buffer)) != -1) {
-			byte[] contents = new byte[read];
-			System.arraycopy(buffer,0,contents,0,read);
-			final AppFilePart filePart = new AppFilePart(fileName, fileLength, contents, offset);
-			sendPart(contactId, filePart);
-			offset += read;
-		}
-	}
-
-	private void sendPart(ContactId contactId, AppFilePart filePart) {
-		System.out.println("sending:"+filePart);
-		_channel.output().consume(new Packet(contactId, filePart));
-	}
-
 	protected void sendAppListResponse(ContactId contactId) {
-		Map<String,String> installNameAndAppUID = new Hashtable<String,String>();
+		Map<String,String> nameAndAppUID = new Hashtable<String,String>();
 		for(SovereignApplicationUID app:_publishedApps)
-			installNameAndAppUID.put(app._installName, app._appUID);
-		_channel.output().consume(new Packet(contactId,new AppListResponse(installNameAndAppUID)));
+			nameAndAppUID.put(app._sovereignApplication.defaultName(), app._appUID);
+		_channel.output().consume(new Packet(contactId,new AppListResponse(nameAndAppUID)));
 	}
 
 	public List<ContactAction> contactActions() {
@@ -136,13 +76,13 @@ public class MeToo implements SovereignApplication{
 		if (_framesByContactId.get(contact.id()) == null){
 			SourceImpl<MeTooPacket> input = new SourceImpl<MeTooPacket>(null);
 			_inputsByContactId.put(contact.id(), input);
-			_framesByContactId.put(contact.id(), new MeTooFrame(_channel, contact, input.output(), _tempDirectory, _appManager));
+			_framesByContactId.put(contact.id(), new MeTooFrame(_channel, contact, input.output()));
 		} else {
 			_framesByContactId.get(contact.id()).sendAppListRequest();
 		}
 		_framesByContactId.get(contact.id()).setVisible(true);
 	}
-	
+
 	public String defaultName() {
 		return "meetoo";
 	}

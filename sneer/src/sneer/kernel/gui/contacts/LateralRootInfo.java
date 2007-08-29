216ffbf801cb5493d4b81090d9d01b517f5a36ae
gandhi <gandhi@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-08-29 04:10:16 +0000

    minor fix - ownnamesetter
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1047 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/src/sneer/kernel/gui/contacts/LateralRootInfo.java
+++ sneer/src/sneer/kernel/gui/contacts/LateralRootInfo.java
@@ -27,7 +27,7 @@ public class LateralRootInfo extends JPanel{
 		Dimension profileFieldSize = new Dimension(150,100);
 		
 		content.add(new ReactiveJpgImageField(translate("Picture"), _businessSource.output().picture(), _businessSource.pictureSetter(), new Dimension(100,100)));
-		content.add(new LabeledPanel(translate("Own Name"), new ReactiveTextField(_businessSource.output().ownName(), _businessSource.thoughtOfTheDaySetter()),defaultFieldSize));
+		content.add(new LabeledPanel(translate("Own Name"), new ReactiveTextField(_businessSource.output().ownName(), _businessSource.ownNameSetter()),defaultFieldSize));
 		content.add(new LabeledPanel(translate("Thought Of The Day"), new ReactiveTextField(_businessSource.output().thoughtOfTheDay(), _businessSource.thoughtOfTheDaySetter()),defaultFieldSize));
 		content.add(new LabeledPanel(translate("Profile"), new ReactiveMemoField(_businessSource.output().profile(), _businessSource.profileSetter()), profileFieldSize));
 		content.add(new LabeledPanel(translate("Sneer Port"), new ReactiveIntegerField(_businessSource.output().sneerPort(), _businessSource.sneerPortSetter()),defaultFieldSize));

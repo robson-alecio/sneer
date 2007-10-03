55eb9d3a8da049292d36425835610cb5fbfe8e1a
gandhi <gandhi@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-10-03 00:00:11 +0000

    mp3sushi being fixed
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1149 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/apps/mp3sushi/sneer/games/mediawars/mp3sushi/ID3Summary.java
+++ sneer/apps/mp3sushi/sneer/games/mediawars/mp3sushi/ID3Summary.java
@@ -1,6 +1,12 @@
 package sneer.games.mediawars.mp3sushi;
 
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 
 import org.farng.mp3.MP3File;
 import org.farng.mp3.id3.AbstractID3v2;
@@ -32,12 +38,10 @@ public class ID3Summary implements Serializable{
 		AbstractID3v2 aid3V2 = mp3file.getID3v2Tag();
 		
 		if (aid3V2 != null) {
-			title = aid3V2.getSongTitle(); //Fix Try and recognize different charsets. Use strangeCharsetName.mp3 in the tests package.
-			if (aid3V2 != null) {
-				if (title != null) title = title.trim();
-				singer = aid3V2.getLeadArtist();
-				if (singer != null) singer = singer.trim();
-			}	
+			title = convertCharset(aid3V2.getSongTitle()); //Fix Try and recognize different charsets. Use strangeCharsetName.mp3 in the tests package.
+			if (title != null) title = title.trim();
+				singer = convertCharset(aid3V2.getLeadArtist());
+			if (singer != null) singer = singer.trim();	
 		}
 		
 		if ((title == null) || (title.length() == 0) || (singer == null) || (singer.length() == 0)) {
@@ -71,6 +75,21 @@ public class ID3Summary implements Serializable{
 		if ((title == null) || (title.length() == 0) || (singer == null) || (singer.length() == 0)) return null;
 		return new ID3Summary(fileName, singer, title);
 	}
+	
+	private static String convertCharset(String text) {
+		Charset charset = Charset.forName("ISO-8859-1");
+		CharsetDecoder decoder = charset.newDecoder();
+		CharsetEncoder encoder = charset.newEncoder();
+		try {
+		    ByteBuffer bbuf = encoder.encode(CharBuffer.wrap(text));
+		    CharBuffer cbuf = decoder.decode(bbuf);
+		    return cbuf.toString();
+		} catch (CharacterCodingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	public String getSinger() {
 		return _singer;
 	}

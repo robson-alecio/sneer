19e8c0adbce21cd462eade136e58ab5963f7546f
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2007-10-08 09:54:13 +0000

    Go development
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1158 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/spikes/priscila/Main.java
+++ sneer/spikes/spikes/priscila/Main.java
@@ -1,102 +1,143 @@
 package spikes.priscila;
 
 import java.awt.Color;
+import static java.awt.Color.BLACK;
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-import wheel.lang.Pair;
 import wheel.lang.Threads;
 
 
 public class Main extends JFrame{
 	
+	private static final int _SCREEN_SIZE = 500;
+	private static final int _CELL_SIZE = 30;
+
 	public class Scroller implements Runnable {
 
 		public void run() {
-			while(true){
-				if (!_shouldScrollY) continue;
-				_scroll = (_scroll + 1) % 9;
-				repaint();
-				Threads.sleepWithoutInterruptions(300);
+			while (true) {
+				scrollX();
+				scrollY();
+				if (_scrollXDelta != 0 || _scrollYDelta != 0) repaint();
+				Threads.sleepWithoutInterruptions(150);
 			}
 		}
 
+		private void scrollX() {
+			_scrollX = (_scrollX + _scrollXDelta + 9) % 9;
+		}
+		
+		private void scrollY() {
+			_scrollY = (_scrollY + _scrollYDelta + 9) % 9;
+		}
+
 	}
 
-	private static final int _OFFSET = 60;
+	private static final int _MARGIN = 100;
 	private static final int _STONE_DIAMETER = 28;
 	private static final long serialVersionUID = 1L;
-	public boolean _isBlacksTurn = true;
-	private List<Pair<Integer,Integer>> _blackMoves= new ArrayList<Pair<Integer,Integer>>();
-	private List<Pair<Integer,Integer>> _whiteMoves= new ArrayList<Pair<Integer,Integer>>();
-	private int _scroll;
-    private BufferedImage _bufferImage;
-	private volatile boolean _shouldScrollY;
-    
+
+	private final GoBoard _board = new ToroidalGoBoard(9);
+
+	private BufferedImage _bufferImage;
+
+	private volatile int _scrollY;
+	private volatile int _scrollX;
+	private volatile int _scrollYDelta;
+	private volatile int _scrollXDelta;
 	
-	public Main(){		
+	
+	public Main() {		
 		setTitle("Go");
-	    setSize(400,400);
+	    setSize(_SCREEN_SIZE, _SCREEN_SIZE);
 	    setResizable(false);
 	    setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    addMouseListener(new MouseListener());
-	    addMouseMotionListener(new MouseListener());
+		
+	    addMouseListener();
 	    
 	    Threads.startDaemon(new Scroller());
 	}
+
+	private void addMouseListener() {
+		MouseListener listener = new MouseListener();
+		addMouseListener(listener);
+	    addMouseMotionListener(listener);
+	}
 	
 	@Override
-	public void paint(Graphics g1){
+	public void paint(Graphics graphics){
 		Graphics2D buffer = getBuffer();
-
-	    buffer.setColor(Color.white);
-		buffer.fillRect(0, 0, 400, 400);
-
+		
+	    buffer.setColor(new Color(228,205,152,90));
+		buffer.fillRect(0, 0, _SCREEN_SIZE, _SCREEN_SIZE);
+		
 		buffer.setColor(new Color(228,205,152));
-		buffer.fillRect(0+ _OFFSET, 0+_OFFSET, 240, 240);
+		buffer.fillRect(_MARGIN, _MARGIN, _CELL_SIZE * 8, _CELL_SIZE * 8);
 	    
 		buffer.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
 		buffer.setColor(Color.black);
-		for(int i=0; i< 270; i+=30){
-			buffer.drawLine(i+_OFFSET, 0+_OFFSET, i+_OFFSET, 240+_OFFSET);
-			buffer.drawLine(0+_OFFSET, i+_OFFSET, 240+_OFFSET, i+_OFFSET);
+	
+		for(int i = 0; i < 270; i += _CELL_SIZE){
+			buffer.drawLine(i+_MARGIN, 0+_MARGIN, i+_MARGIN, 240+_MARGIN);
+			buffer.drawLine(0+_MARGIN, i+_MARGIN, 240+_MARGIN, i+_MARGIN);
 		}	
 		
-		buffer.setColor(Color.black);
-		paintMoves(buffer, _blackMoves);
-		buffer.setColor(Color.white);
-		paintMoves(buffer, _whiteMoves);		
+		paintStones(buffer);
 
-		g1.drawImage(_bufferImage, 0, 0, this);
+		graphics.drawImage(_bufferImage, 0, 0, this);
 	}
 
-	private Graphics2D getBuffer() {
-		if (_bufferImage == null) {
-			_bufferImage = (BufferedImage)createImage(400, 400);
-	    	return _bufferImage.createGraphics();
-	    }
-		return (Graphics2D)_bufferImage.getGraphics();
-	}
-
-	private void paintMoves(Graphics2D g, List<Pair<Integer, Integer>> moves) {
+	private void paintStones(Graphics2D graphics) {
+		int size = _board.size();
 		
-		for(Pair<Integer, Integer> move : moves) {
-			int x = move._a;
-			int y = move._b + ((_scroll * 30) % (9 * 30));
-			g.fillOval(x, y, _STONE_DIAMETER, _STONE_DIAMETER);	
-		}
+		for (int x = 0; x < size; x++)
+			for (int y = 0; y < size; y++)
+				paintStoneOnPosition(graphics, x, y);
+	}
+
+	private void paintStoneOnPosition(Graphics2D graphics, int x, int y) {
+		Color color = _board.stoneAt(x, y);
+		if (color == null) return;
+		
+		int cx = toCoordinate(scrollX(x));		
+		int cy = toCoordinate(scrollY(y));		
+		graphics.setColor(color);
+		paintStoneOnCoordinates(graphics, cx, cy);
+	}
+
+	private int scrollX(int x) { return (x + _scrollX) % 9; }
+	private int unscrollX(int x) { return (9 + x - _scrollX) % 9; }
+
+	private int scrollY(int y) { return (y + _scrollY) % 9; }
+	private int unscrollY(int y) { return (9 + y - _scrollY) % 9; }
+	
+	private Graphics2D getBuffer() {
+		_bufferImage = (BufferedImage)createImage(_SCREEN_SIZE, _SCREEN_SIZE);
+	    return _bufferImage.createGraphics();
+	}
+
+
+	private int toCoordinate(int position) {
+		return position * _CELL_SIZE + _MARGIN;
 	}
 	
+	private int toPosition(int coordinate) {
+		int result = (coordinate - _MARGIN + (_CELL_SIZE / 2)) / _CELL_SIZE;
+		if (result < 0) return 0;
+		if (result > 8) return 8;
+		return result;
+	}
+
 	public static void main(String[] args){
     	new Main();
 	}
@@ -104,23 +145,48 @@ public class Main extends JFrame{
 	private class MouseListener extends MouseAdapter{
 		
 		@Override
-		public void mouseMoved(MouseEvent e) {
-			_shouldScrollY = e.getY() > 9*30 + _OFFSET;
+		public void mouseMoved(final MouseEvent e) {
+			_scrollXDelta = scrollDeltaFor(e.getX());
+			_scrollYDelta = scrollDeltaFor(e.getY());
+
+			repaint();
+			SwingUtilities.invokeLater(new Runnable() { @Override public void run() {
+				drawTransparentStone(toPosition(e.getX()), toPosition(e.getY()));
+			}});
 		}
-		
+
+		private int scrollDeltaFor(int coordinate) {
+			if (coordinate > 9 * _CELL_SIZE + _MARGIN) return -1;
+			if (coordinate < _MARGIN) return 1;
+			return 0;
+		}
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			
-			int x = ((e.getX()+15)/30)*30 - (_STONE_DIAMETER / 2);
-			int y = ((e.getY()+15)/30)*30 - (_STONE_DIAMETER / 2);
-			
-			(_isBlacksTurn ? _blackMoves : _whiteMoves).add(new Pair<Integer, Integer>(x,y));
-			_isBlacksTurn = !_isBlacksTurn;
-			
+			int x = unscrollX(toPosition(e.getX()));
+			int y = unscrollY(toPosition(e.getY()));
+			if (!_board.canPlayStone(x, y)) return;
+
+			_board.playStone(x, y);
 			repaint();		
 		}
+
 	}
 
+	private void drawTransparentStone(int x, int y) {
+		Graphics2D graphics = (Graphics2D) getGraphics();
+		
+		if(_board.nextToPlay() == BLACK)
+			graphics.setColor(new Color(0, 0, 0, 50));
+		else
+			graphics.setColor(new Color(255, 255, 255, 90));
+		
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		paintStoneOnCoordinates(graphics, toCoordinate(x), toCoordinate(y));
+	}
+
+	private void paintStoneOnCoordinates(Graphics2D graphics, int x, int y) {
+		graphics.fillOval(x - (_STONE_DIAMETER / 2), y - (_STONE_DIAMETER / 2), _STONE_DIAMETER, _STONE_DIAMETER);
+	}
+	
 }
-
-

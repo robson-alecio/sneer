745f6effe83b70a31b25a60ac4de59e5c494743c
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-03-17 18:05:49 +0000

    SmokeTest moved to spikes/leandro
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1313 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ spikes/src/spikes/leandro/SmokeTest.java
@@ -0,0 +1,18 @@
+package spikes.leandro;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import sneer.compiler.tests.CompilerTest;
+import sneer.deployer.test.DeployerTest;
+import sneer.lego.tests.ContainerTest;
+ 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	ContainerTest.class,
+	CompilerTest.class,
+	DeployerTest.class
+})
+public class SmokeTest {
+	
+}

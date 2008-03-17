4d37c5cfe4b45778b4216628220ed54dd3cd702a
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-03-17 23:40:04 +0000

    removing old bricks. updating classpath
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1320 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- kernel/src/sneer/lego/tests/ContainerTest.java
+++ kernel/src/sneer/lego/tests/ContainerTest.java
@@ -2,11 +2,7 @@ package sneer.lego.tests;
 
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-import im.IM;
 
-import java.security.Policy;
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.lego.Binder;
@@ -15,8 +11,6 @@ import sneer.lego.Container;
 import sneer.lego.impl.SimpleBinder;
 import sneer.lego.impl.SimpleContainer;
 import sneer.lego.tests.impl.MySample;
-import spikes.vitor.security.PolicySpike;
-import topten.TopTen;
 
 public class ContainerTest extends BrickTestSupport {
 
@@ -59,17 +53,4 @@ public class ContainerTest extends BrickTestSupport {
         assertTrue(lifecycle.startCalled());
 
 	}
-	
-	@Ignore
-	public void testX() throws Exception {
-		
-		Policy.setPolicy(new PolicySpike());
-		System.setSecurityManager(new SecurityManager());
-
-		IM im = container.produce(IM.class);
-		im.sendMessage("leandro", "Eu te amo. PS: Klaus");
-		TopTen topTen = container.produce(TopTen.class);
-		topTen.toString();
-	}
-	
 }

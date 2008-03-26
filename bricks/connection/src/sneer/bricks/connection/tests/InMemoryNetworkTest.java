816cf7a601a351cbd1b79e640fc77da6628fa90f
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-03-26 20:56:58 +0000

    testing InMemoryNetwork
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1370 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ bricks/connection/src/sneer/bricks/connection/tests/InMemoryNetworkTest.java
@@ -0,0 +1,46 @@
+package sneer.bricks.connection.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import sneer.bricks.connection.impl.mock.InMemoryNetwork;
+import sneer.bricks.network.ByteArrayServerSocket;
+import sneer.bricks.network.ByteArraySocket;
+import sneer.bricks.network.Network;
+import sneer.lego.Binder;
+import sneer.lego.Brick;
+import sneer.lego.impl.SimpleBinder;
+import sneer.lego.tests.BrickTestSupport;
+import wheel.lang.Threads;
+
+
+public class InMemoryNetworkTest extends BrickTestSupport {
+
+	@Brick
+	private Network _network;
+	
+	@Override
+	protected Binder getBinder() {
+		return new SimpleBinder().bind(Network.class).toInstance(new InMemoryNetwork());
+	}
+
+	@Test
+	public void testNetworkMessages() throws Exception {
+		final ByteArrayServerSocket server = _network.openServerSocket(9090);
+		Threads.startDaemon(new Runnable(){ @Override public void run() {
+			try {
+				ByteArraySocket request = server.accept();
+				request.write(new String(request.read()).toUpperCase().getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}});
+		ByteArraySocket client = _network.openSocket("localhost", 9090);
+		client.write("hello".getBytes());
+		byte[] reply = client.read();
+		assertEquals("HELLO", new String(reply));
+	}
+}

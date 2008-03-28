79b8c73597a034a2249dba790032acd869931ab4
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-03-28 20:42:14 +0000

    implementing KeyManager (in memory) + tests
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1398 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/keymanager/src/sneer/bricks/keymanager/impl/KeyManagerImpl.java
+++ bricks/keymanager/src/sneer/bricks/keymanager/impl/KeyManagerImpl.java
@@ -1,22 +1,70 @@
 package sneer.bricks.keymanager.impl;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import sneer.bricks.keymanager.KeyManager;
 import sneer.contacts.Contact;
 
 public class KeyManagerImpl implements KeyManager {
 
+	//Fix: read read from local file
+	private Key _ownKey = new Key("1234");
+	
+	private Map<Contact,Key> _keyByContact = new HashMap<Contact, Key>(); 
+	
 	@Override
 	public void addKey(Contact contact, byte[] peersPublicKey) {
+		Key key = _keyByContact.get(contact);
+		if(key == null) {
+			//Fix: store key file
+			_keyByContact.put(contact, new Key(peersPublicKey));
+		} else {
+			//Fix: use a better exception
+			throw new IllegalStateException("duplicate key for "+contact.nickname());
+		}
 	}
 
 	@Override
 	public Contact contactGiven(byte[] peersPublicKey) {
-		throw new wheel.lang.exceptions.NotImplementedYet();
+		Key wanted = new Key(peersPublicKey);
+		for (Contact contact : _keyByContact.keySet()) {
+			Key key = _keyByContact.get(contact);
+			if(key.equals(wanted))
+				return contact;
+		}
+		return null;
 	}
 
 	@Override
 	public byte[] ownPublicKey() {
-		throw new wheel.lang.exceptions.NotImplementedYet();
+		return _ownKey.data();
+	}
+}
+
+class Key {
+
+	private byte[] _data;
+	
+	public Key(String s) {
+		_data = s.getBytes();
 	}
 
+	public Key(byte[] bytes) {
+		_data = bytes;
+	}
+
+	public byte[] data() {
+		return _data;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) return false;
+		if(!(obj instanceof Key)) return false;
+		
+		Key other = (Key) obj;
+		return Arrays.equals(_data, other.data());
+	}
 }
---
+++ bricks/keymanager/src/sneer/bricks/keymanager/tests/KeyManagerTest.java
@@ -0,0 +1,59 @@
+package sneer.bricks.keymanager.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sneer.bricks.keymanager.KeyManager;
+import sneer.contacts.Contact;
+import sneer.lego.Brick;
+import sneer.lego.tests.BrickTestSupport;
+import wheel.reactive.Register;
+import wheel.reactive.Signal;
+import wheel.reactive.impl.RegisterImpl;
+
+public class KeyManagerTest extends BrickTestSupport {
+
+	@Brick
+	private KeyManager _keyManager;
+	
+	@Test
+	public void testOwnKey() throws Exception {
+		byte[] pk = _keyManager.ownPublicKey();
+		assertEquals("1234", new String(pk));
+	}
+
+	@Test
+	public void testAddKey() throws Exception {
+		Contact contact = newContact();
+		byte[] key = "some key".getBytes();
+		
+		assertNull(_keyManager.contactGiven(key));
+		
+		_keyManager.addKey(contact, key);
+		assertSame(contact, _keyManager.contactGiven(key));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testAddKeyTwiceForSameContact() throws Exception {
+		Contact contact = newContact();
+		byte[] key = "some key".getBytes();
+		
+		assertNull(_keyManager.contactGiven(key));
+
+		_keyManager.addKey(contact, key);
+		assertSame(contact, _keyManager.contactGiven(key));
+		
+		_keyManager.addKey(contact, key);
+		fail("Should have thrown exception");
+	}
+
+	private Contact newContact() {
+		Contact contact = new Contact() { @Override public Signal<String> nickname() {
+			Register<String> r = new RegisterImpl<String>("Klaus");
+			return r.output(); 
+		}};
+		return contact;
+	}
+	
+}

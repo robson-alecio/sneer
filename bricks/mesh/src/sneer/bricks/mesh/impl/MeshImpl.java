185fd158ed84e01c83bacaed4c65497b324eb86b
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-01 22:43:29 +0000

    Mesh is a receiver for new connections
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1426 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/mesh/src/sneer/bricks/mesh/impl/MeshImpl.java
+++ bricks/mesh/src/sneer/bricks/mesh/impl/MeshImpl.java
@@ -3,14 +3,26 @@ package sneer.bricks.mesh.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import sneer.bricks.connection.Connection;
+import sneer.bricks.connection.ConnectionManager;
 import sneer.bricks.mesh.Mesh;
 import sneer.contacts.Contact;
 import sneer.contacts.ContactManager;
 import sneer.lego.Brick;
 import sneer.lego.Injector;
+import sneer.lego.Startable;
 import wheel.reactive.Signal;
+import wheel.reactive.maps.impl.SimpleMapReceiver;
 
-public class MeshImpl implements Mesh {
+public class MeshImpl implements Mesh, Startable {
+
+	private final Map<Contact, ContactProxy> _proxiesByContact = new HashMap<Contact, ContactProxy>();
+	
+	@SuppressWarnings("unused")
+	private SimpleMapReceiver<Contact, Connection> _connectionReceiverToAvoidGC;
+
+	@Brick
+	private ConnectionManager _connectionManager;
 
 	@Brick
 	private ContactManager _contactManager;
@@ -18,8 +30,29 @@ public class MeshImpl implements Mesh {
 	@Brick
 	private Injector _injector;
 
-	private final Map<Contact, ContactProxy> _proxiesByContact = new HashMap<Contact, ContactProxy>();
+	@Override
+	public void start() throws Exception {
+		_connectionReceiverToAvoidGC = new SimpleMapReceiver<Contact, Connection>(_connectionManager.connections()){
 
+			@Override
+			public void elementAdded(Contact contact, Connection connection) {
+				System.out.println("New connection with: "+contact.nickname());
+				//TODO: start new Thread to read bytes from connection
+				throw new wheel.lang.exceptions.NotImplementedYet();
+			}
+
+			@Override
+			public void elementRemoved(Contact contact, Connection connection) {
+				System.out.println("removed connection with: "+contact.nickname());
+				throw new wheel.lang.exceptions.NotImplementedYet();
+			}
+
+			@Override
+			public void elementReplaced(Contact contact, Connection connection) {
+				System.out.println("replaced connection with: "+contact.nickname());
+				throw new wheel.lang.exceptions.NotImplementedYet();
+			}};
+	}
 
 	@Override
 	public <T> Signal<T> findSignal(String nicknamePath, String signalPath) {
@@ -45,5 +78,4 @@ public class MeshImpl implements Mesh {
 			return proxy;
 		}
 	}
-
 }
--- wheel/src/wheel/reactive/maps/impl/SimpleMapReceiver.java
+++ wheel/src/wheel/reactive/maps/impl/SimpleMapReceiver.java
@@ -2,43 +2,19 @@ package wheel.reactive.maps.impl;
 
 import wheel.reactive.maps.MapSignal;
 
-public class SimpleMapReceiver<K, V> extends VisitingMapReceiver<K, V> {
+public abstract class SimpleMapReceiver<K, V> extends VisitingMapReceiver<K, V> {
 
 	private final MapSignal<K, V> _mapSignal;
 	
 	public SimpleMapReceiver(MapSignal<K, V> mapSignal) {
 		_mapSignal = mapSignal;
 	}
-	
-	@Override
-	public void elementAdded(K key, V value) {
-		// Implement Auto-generated method stub
-		throw new wheel.lang.exceptions.NotImplementedYet();
-	}
-
-	@Override
-	public void elementRemoved(K key, V value) {
-		// Implement Auto-generated method stub
-		throw new wheel.lang.exceptions.NotImplementedYet();
-	}
-
-	@Override
-	public void elementReplaced(K key, V value) {
-		// Implement Auto-generated method stub
-		throw new wheel.lang.exceptions.NotImplementedYet();
-	}
 
 	@Override
 	public void elementToBeRemoved(K key, V value) {
-		// Implement Auto-generated method stub
-		throw new wheel.lang.exceptions.NotImplementedYet();
 	}
 
 	@Override
 	public void elementToBeReplaced(K key, V value) {
-		// Implement Auto-generated method stub
-		throw new wheel.lang.exceptions.NotImplementedYet();
 	}
-
-
 }

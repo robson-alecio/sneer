7703cbeccf32c297a133201c45bce41bde703f69
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-03 19:26:51 +0000

    delegating method call to SovereignParty when possible
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1443 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- tests/functional/freedom7/adapter/SneerFreedom7Test.java
+++ tests/functional/freedom7/adapter/SneerFreedom7Test.java
@@ -1,8 +1,11 @@
 package functional.freedom7.adapter;
 
+import java.io.File;
+
 import functional.SovereignCommunity;
 import functional.SovereignParty;
 import functional.adapters.SneerCommunity;
+import functional.freedom7.BrickPublished;
 import functional.freedom7.BrickPublisher;
 import functional.freedom7.Freedom7Test;
 
@@ -10,8 +13,44 @@ import functional.freedom7.Freedom7Test;
 public class SneerFreedom7Test extends Freedom7Test {
 
 	@Override
-	protected BrickPublisher wrapParty(SovereignParty party) {
-		throw new wheel.lang.exceptions.NotImplementedYet();
+	protected BrickPublisher wrapParty(final SovereignParty party) {
+		
+		return new BrickPublisher() {
+			
+			@Override
+			public void meToo(String interface1) {
+				throw new wheel.lang.exceptions.NotImplementedYet();
+			}
+
+			@Override
+			public BrickPublished publishBrick(File brickFile) {
+				throw new wheel.lang.exceptions.NotImplementedYet();
+			}
+
+			@Override
+			public void bidirectionalConnectTo(SovereignParty peer) {
+				party.bidirectionalConnectTo(peer);
+			}
+
+			@Override
+			public void giveNicknameTo(SovereignParty peer, String nickname) {
+				party.giveNicknameTo(peer, nickname);
+			}
+
+			@Override
+			public String navigateAndGetName(String nicknamePath) {
+				return party.navigateAndGetName(nicknamePath);
+			}
+
+			@Override
+			public String ownName() {
+				return party.ownName();
+			}
+
+			@Override
+			public void setOwnName(String newName) {
+				party.setOwnName(newName);
+			}};
 	}
 
 	@Override

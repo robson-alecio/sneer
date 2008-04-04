38411b5c54044854a99d18e220c1b3991fb2053d
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-04 18:59:50 +0000

    - loading brick file from classpath - using SimpleBrickPublisher
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1450 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- tests/functional/freedom7/adapter/SneerFreedom7Test.java
+++ tests/functional/freedom7/adapter/SneerFreedom7Test.java
@@ -5,7 +5,6 @@ import java.io.File;
 import functional.SovereignCommunity;
 import functional.SovereignParty;
 import functional.adapters.SneerCommunity;
-import functional.freedom7.BrickPublished;
 import functional.freedom7.BrickPublisher;
 import functional.freedom7.Freedom7Test;
 
@@ -14,43 +13,7 @@ public class SneerFreedom7Test extends Freedom7Test {
 
 	@Override
 	protected BrickPublisher wrapParty(final SovereignParty party) {
-		
-		return new BrickPublisher() {
-			
-			@Override
-			public void meToo(String interface1) {
-				throw new wheel.lang.exceptions.NotImplementedYet();
-			}
-
-			@Override
-			public BrickPublished publishBrick(File brickFile) {
-				throw new wheel.lang.exceptions.NotImplementedYet();
-			}
-
-			@Override
-			public void bidirectionalConnectTo(SovereignParty peer) {
-				party.bidirectionalConnectTo(peer);
-			}
-
-			@Override
-			public void giveNicknameTo(SovereignParty peer, String nickname) {
-				party.giveNicknameTo(peer, nickname);
-			}
-
-			@Override
-			public String navigateAndGetName(String nicknamePath) {
-				return party.navigateAndGetName(nicknamePath);
-			}
-
-			@Override
-			public String ownName() {
-				return party.ownName();
-			}
-
-			@Override
-			public void setOwnName(String newName) {
-				party.setOwnName(newName);
-			}};
+		return new SimpleBrickPublisher(party);
 	}
 
 	@Override
@@ -60,7 +23,8 @@ public class SneerFreedom7Test extends Freedom7Test {
 
 	@Override
 	protected File askBrickFile() {
-		// Implement Auto-generated method stub
-		throw new wheel.lang.exceptions.NotImplementedYet();
+		String filename = System.getProperty("user.dir") + "/tests/functional/freedom7/test-resources/bricks/sample"; //sneer/bricks/sample/Sample.java
+		File result = new File(filename);
+		return result;
 	}
 }

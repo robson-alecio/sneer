fed62244f7c2ce1a65c718d7c1cc691f92783fd5
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-22 21:37:33 +0000

    closing inputstream (too many open files)
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1506 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- kernel/src/sneer/lego/utils/asm/ClassUtils.java
+++ kernel/src/sneer/lego/utils/asm/ClassUtils.java
@@ -3,15 +3,18 @@ package sneer.lego.utils.asm;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.objectweb.asm.ClassReader;
 
 public class ClassUtils {
 
 	public static MetaClass metaClass(File classFile) throws IOException {
-		ClassReader reader = new ClassReader(new FileInputStream(classFile));
+		InputStream is = new FileInputStream(classFile);
+		ClassReader reader = new ClassReader(is);
 		MetaClass visitor = new MetaClass(classFile);
 		reader.accept(visitor, 0);
+		is.close();
 		return visitor;
 	}
 }

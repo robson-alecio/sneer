bf239b1aa2fc5fca96764834defa90fd42f967cb
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-25 21:36:57 +0000

    fixed path names on windows
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1537 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- kernel/src/sneer/lego/utils/metaclass/SimpleMetaClass.java
+++ kernel/src/sneer/lego/utils/metaclass/SimpleMetaClass.java
@@ -2,6 +2,8 @@ package sneer.lego.utils.metaclass;
 
 import java.io.File;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class SimpleMetaClass extends MetaClassSupport {
 
 	public SimpleMetaClass(File rootDirectory, File classFile) {
@@ -15,8 +17,8 @@ public class SimpleMetaClass extends MetaClassSupport {
 		if (!path.startsWith(rootPath))
 			throw new MetaClassException("Class file " + path + " on wrong directory");
 
-		_className = path.substring(rootPath.length() + 1);
-		_className = _className.substring(0, _className.indexOf("."));
+		_className = FilenameUtils.separatorsToUnix(path.substring(rootPath.length() + 1));
+		_className = _className.substring(0, _className.indexOf(".class"));
 		_className = _className.replaceAll("/", ".");
 		_packageName = _className.substring(0, _className.lastIndexOf("."));
 		_isInterface = _packageName.indexOf("impl") < 0;

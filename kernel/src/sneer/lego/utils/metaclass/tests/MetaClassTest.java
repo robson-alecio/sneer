59c02a9f0ffff3d4edbccd900aca35735e3c9f96
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-25 21:38:04 +0000

    filenameutils test
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1538 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- kernel/src/sneer/lego/utils/metaclass/tests/MetaClassTest.java
+++ kernel/src/sneer/lego/utils/metaclass/tests/MetaClassTest.java
@@ -3,6 +3,8 @@ package sneer.lego.utils.metaclass.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.io.FilenameUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.lego.utils.metaclass.ClassUtils;
@@ -15,8 +17,17 @@ public class MetaClassTest {
 	public void testMetaClass() throws Exception {
 		MetaClass metaClass = ClassUtils.metaClass(Bean.class);
 		assertTrue(metaClass.isInterface());
-		assertEquals("sneer.lego.utils.asm.tests.bean.Bean", metaClass.getName());
-		assertEquals("sneer.lego.utils.asm.tests.bean", metaClass.getPackageName());
+		assertEquals("sneer.lego.utils.metaclass.tests.bean.Bean", metaClass.getName());
+		assertEquals("sneer.lego.utils.metaclass.tests.bean", metaClass.getPackageName());
 		//assertTrue(metaClass.isAssignanbleTo(Object.class));
+	}
+
+	@Test
+	@Ignore
+	public void testPathNames() {
+		String filename = "c:\\a\\b\\c.txt";
+		System.out.println(FilenameUtils.separatorsToUnix(filename));
+		System.out.println(FilenameUtils.normalize(filename));
+		System.out.println(FilenameUtils.getPath(filename));
 	}
 }

82a85bf213c8dd94c5a448a7874b518d94807b93
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-04-29 19:39:40 +0000

    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1551 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ bricks/src/sneer/bricks/deployer/impl/filters/ImplFinder.java
@@ -0,0 +1,48 @@
+package sneer.bricks.deployer.impl.filters;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import sneer.bricks.deployer.impl.parser.JavaSource;
+import sneer.bricks.deployer.impl.parser.JavaSourceParser;
+import sneer.lego.utils.io.SimpleFilter;
+
+public class ImplFinder extends SimpleFilter {
+
+	public ImplFinder(File root) {
+		super(root, JAVA_SOURCE_FILE_FILTER);
+	}
+
+	@SuppressWarnings({ "unchecked", "unused" })
+	@Override
+	protected void handleFile(File file, int depth, Collection results) throws IOException {
+		File implDir = findImplDir(file);
+		if(implDir == null)
+			return;
+
+		JavaSource source = new JavaSourceParser(file).parse();
+		if(/* !source.isInterface() && */ !source.isAccessPublic())
+			results.add(file);
+
+	}
+	
+	private File findImplDir(File file) {
+		File parent = file.getParentFile();
+		
+		if(parent == null) 
+			return null;
+		
+		if("impl".equals(parent.getName())) {
+			return parent;
+		}
+		return findImplDir(parent);
+		
+	}
+
+	@Override
+	protected String[] ignoreDirectoryNames() {
+		return new String[]{"."};
+	}
+
+}

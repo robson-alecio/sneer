8b230a939b2cc373b22f3dfa190b9eaa63a631bc
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-05-06 21:49:56 +0000

    adding dependency on SampleImpl (used for testing)
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1579 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- tests/functional/freedom7/test-resources/bricks/source/sneer/bricks/sample/impl/SampleImpl.java
+++ tests/functional/freedom7/test-resources/bricks/source/sneer/bricks/sample/impl/SampleImpl.java
@@ -1,12 +1,18 @@
 package sneer.bricks.sample.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import sneer.bricks.sample.Helper;
 import sneer.bricks.sample.Sample;
 
 class SampleImpl implements Sample {
 
+	private Log _log = LogFactory.getLog("category");
+	
 	@Override
 	public String doSomething() {
+		_log.trace("doSomething() called"); 
 		return "SampleImpl";
 	}
 

962b80e783750fa094e0af0c80844c3753c74b2c
leandrosaad <leandrosaad@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-05-08 18:48:57 +0000

    better test assertions
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1604 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- tests/functional/freedom7/Freedom7Test.java
+++ tests/functional/freedom7/Freedom7Test.java
@@ -1,9 +1,11 @@
 package functional.freedom7;
 
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import functional.SovereignFunctionalTest;
 import functional.SovereignParty;
@@ -14,7 +16,7 @@ public abstract class Freedom7Test extends SovereignFunctionalTest {
 	private static final String BRICK_NAME = "sneer.bricks.sample.Sample";
 	
 	@Test
-	public void testPublish() {
+	public void testPublish() throws Exception {
 		
 		if (!TestDashboard.newTestsShouldRun()) return;
 		
@@ -26,18 +28,18 @@ public abstract class Freedom7Test extends SovereignFunctionalTest {
 		 * compiles all bricks found under _sourceFolder_ and installs them locally
 		 */
 		publisher.publishBrick(sourceFolder);
-		Object orig = publisher.produce(BRICK_NAME);
-		print("sneer+AnaAlmeida",orig);
+
+		Object brick1 = publisher.produce(BRICK_NAME);
+		ClassLoader cl1 = brick1.getClass().getClassLoader();
+		assertTrue("wrong directory for brick class loader: "+cl1.toString(),cl1.toString().indexOf("sneer+AnaAlmeida") > 0);
 		
 		receiver.meToo(publisher, BRICK_NAME);
-		Object copied = receiver.produce(BRICK_NAME);
-		print("sneer+BrunoBarros",copied);
-	}
-
-	private void print(String expected, Object obj) {
-		String toString = obj.getClass().getClassLoader().toString();
-		//System.out.println(toString);
-		assertTrue("wrong directory for brick class loader: "+toString,toString.indexOf(expected) > 0);
+		Object brick2 = receiver.produce(BRICK_NAME);
+		ClassLoader cl2 = brick2.getClass().getClassLoader();
+		assertTrue("wrong directory for brick class loader: "+cl2.toString(),cl2.toString().indexOf("sneer+BrunoBarros") > 0);
+		
+		assertNotSame(brick1, brick2);
+		assertNotSame(cl1, cl2);
 	}
 
 	protected abstract  File askSourceFolder();

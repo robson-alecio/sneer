4e776026f3473dc1222f444d81e3bb1d7321ec81
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-05-26 21:55:53 +0000

    Minor change
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1665 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneerapps/gis/ui/tests/SampleApplication.java
+++ bricks/src/sneerapps/gis/ui/tests/SampleApplication.java
@@ -10,6 +10,6 @@ public class SampleApplication  {
 	public static void main(String[] args) throws Exception {
 		Container container = ContainerUtils.getContainer();
 		container.produce(LocationGui.class);
-		Threads.sleepWithoutInterruptions(10000);
+		Threads.sleepWithoutInterruptions(20000);
 	}
 }

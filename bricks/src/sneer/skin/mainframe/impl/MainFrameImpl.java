f3eae816400eca454574a742c4ad5e9ba4dd615b
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-06-17 14:14:57 +0000

    add a sneer gui bar
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1730 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/skin/mainframe/impl/MainFrameImpl.java
+++ bricks/src/sneer/skin/mainframe/impl/MainFrameImpl.java
@@ -1,37 +1,66 @@
 package sneer.skin.mainframe.impl;
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-import sneer.bricks.mesh.Me;
-import sneer.bricks.mesh.Party;
+import net.sourceforge.napkinlaf.NapkinLookAndFeel;
+import sneer.bricks.threadpool.ThreadPool;
 import sneer.lego.Inject;
-import sneer.lego.Startable;
 import sneer.skin.mainframe.MainFrame;
-import sneer.skin.viewmanager.PartyView;
-import sneer.skin.viewmanager.ViewManager;
-import wheel.reactive.Signal;
-import wheel.reactive.impl.RegisterImpl;
 
-public class MainFrameImpl implements MainFrame, Startable {
+public class MainFrameImpl implements MainFrame, Runnable {
+	
+	private static transient final int _WIDTH = 250;
+	private static transient final int _HOFFSET = 30;
 	
 	@Inject
-	static private ViewManager _viewManager;
-
-	@Inject
-	static private Me _me;
+	static private ThreadPool threadPool;
 	
-	@Override
-	public void start() {
-		JFrame container = new JFrame();
-		container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		container.setSize(200, 300);
-		container.setVisible(true);
-		
-		PartyView view = _viewManager.getOnlyOnePartyViewForNow();
-		
-		Signal<Party> partySignal = new RegisterImpl<Party>(_me).output();
-		view.init(container, partySignal);
+	private Dimension screenSize;
+	private Rectangle bounds;
+	
+	private transient JFrame window = new JFrame();
+
+	public MainFrameImpl() {
+		threadPool.registerActor(this);
 	}
 
+	private void initWindow() {
+		try {
+			UIManager.setLookAndFeel(new NapkinLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			throw new wheel.lang.exceptions.NotImplementedYet(e);
+		}
+
+		window.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				bounds = window.getBounds();
+			}
+		});
+		resize();
+		window.setVisible(true);	
+	}
+
+	private void resize() {
+		Dimension newSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		if(screenSize==null || !screenSize.equals(newSize)){
+			//set a new size
+			screenSize  = newSize;
+			bounds = new Rectangle((int) screenSize.getWidth() - _WIDTH, 0, _WIDTH,	
+								   (int) screenSize.getHeight() - _HOFFSET);
+		}
+		window.setBounds(bounds);
+	}
+
+	@Override
+	public void run() {
+		initWindow();
+	}
 }

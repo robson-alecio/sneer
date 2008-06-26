b7325cae4eb4b9d48067def9504c2ab081672fc9
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-06-26 18:08:10 +0000

    GiveNTake brick started
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1795 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ bricks/src/sneerapps/giventake/GiveNTake.java
@@ -0,0 +1,9 @@
+package sneerapps.giventake;
+
+import sneer.bricks.things.Thing;
+
+public interface GiveNTake {
+
+	void advertise(Thing thing);
+
+}
---
+++ bricks/src/sneerapps/giventake/tests/GiveNTakeUser.java
@@ -0,0 +1,27 @@
+package sneerapps.giventake.tests;
+
+import java.util.Collection;
+
+import sneer.bricks.things.Thing;
+import sneer.bricks.things.ThingHome;
+import sneer.lego.Container;
+import sneer.lego.ContainerUtils;
+import sneerapps.giventake.GiveNTake;
+
+class GiveNTakeUser {
+
+	private final Container _container = ContainerUtils.newContainer();
+	
+	public void advertise(String title, String description) {
+		GiveNTake gnt = _container.produce(GiveNTake.class);
+		ThingHome thingHome = _container.produce(ThingHome.class);
+		
+		gnt.advertise(thingHome.create(title, description));
+	}
+
+	public Collection<Thing> search(String tags) {
+		tags.toString();
+		throw new wheel.lang.exceptions.NotImplementedYet(); // Implement
+	}
+
+}

f25df25a35c3ac9cba682127990aa05239ed3bbe
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-06-27 19:58:30 +0000

    Workaround for the Eclipse 3.4.0 (Ganymede) compiler bug.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1818 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- wheel/src/wheel/lang/Consumer.java
+++ wheel/src/wheel/lang/Consumer.java
@@ -1,9 +1,8 @@
 package wheel.lang;
 
-import wheel.lang.exceptions.IllegalParameter;
 
 public interface Consumer<VO> {
-	
-	void consume(VO valueObject) throws IllegalParameter;
+
+	void consume(VO valueObject); //throws IllegalParameter; 	// For now, throwing of the exception has been removed because of a bug in the Eclipse compiler . Refactor: See wheel.lang.exceptions.IllegalParameter and throw the Exception again. 
 
 }

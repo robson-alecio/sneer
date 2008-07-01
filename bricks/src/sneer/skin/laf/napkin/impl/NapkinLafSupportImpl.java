3e6d1f11b5d70fbc96a231a8855b40dd966a8ef6
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-07-01 13:56:08 +0000

    remove JTaskPane
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1824 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/skin/laf/napkin/impl/NapkinLafSupportImpl.java
+++ bricks/src/sneer/skin/laf/napkin/impl/NapkinLafSupportImpl.java
@@ -1,8 +1,6 @@
 package sneer.skin.laf.napkin.impl;
 
-import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics2D;
 
 import javax.swing.LookAndFeel;
 import javax.swing.UIDefaults;
@@ -11,9 +9,6 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.FontUIResource;
 
 import net.sourceforge.napkinlaf.NapkinLookAndFeel;
-
-import org.jdesktop.swingx.painter.Painter;
-
 import sneer.lego.Inject;
 import sneer.skin.laf.LafManager;
 import sneer.skin.laf.napkin.NapkinLafSupport;
@@ -53,16 +48,7 @@ public class NapkinLafSupportImpl implements NapkinLafSupport{
 		UIDefaults defaults = laf.getDefaults();
 		FontUIResource font = new  FontUIResource(new Font("Arial", Font.PLAIN,13));
 		defaults.put("TextArea.font", font);
-		defaults.put("TextField.font", font);
-		
-	    Painter painter = new Painter(){
-			@Override
-			public void paint(Graphics2D g, Object object, int width, int height) {
-				//ignore
-			}
-		};
-		defaults.put("TaskPaneContainer.backgroundPainter", painter);	
-		defaults.put("TaskPane.borderColor", Color.LIGHT_GRAY);	    
+		defaults.put("TextField.font", font); 
 	}
 
 	@Override

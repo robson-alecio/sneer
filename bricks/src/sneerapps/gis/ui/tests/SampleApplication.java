f833780ae2d179b88e127363fec4d044542aa394
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-07-08 14:28:07 +0000

    back to sneerapps
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1873 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ bricks/src/sneerapps/gis/ui/tests/SampleApplication.java
@@ -0,0 +1,15 @@
+package sneerapps.gis.ui.tests;
+
+import sneer.lego.Container;
+import sneer.lego.ContainerUtils;
+import sneerapps.gis.ui.LocationGui;
+import wheel.lang.Threads;
+
+public class SampleApplication  {
+
+	public static void main(String[] args) throws Exception {
+		Container container = ContainerUtils.getContainer();
+		container.produce(LocationGui.class);
+		Threads.sleepWithoutInterruptions(40000);
+	}
+}

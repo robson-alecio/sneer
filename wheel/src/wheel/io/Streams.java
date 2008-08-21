f8dedc85bc739e835fefd4bb1f3ef7beb98a7765
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-08-21 12:11:11 +0000

    Add a Streams.crash(object). Implemented with reflection, without Closeable interface.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@1983 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- wheel/src/wheel/io/Streams.java
+++ wheel/src/wheel/io/Streams.java
@@ -19,8 +19,20 @@ public class Streams {
 
 	public static void crash(Closeable closeable) {
 		try {
-			closeable.close();
+			if(closeable!=null) closeable.close();
 		} catch (IOException ignored) {}
+	}
+
+	public static void crash(Object closeable){
+		if(closeable==null)	return;
+		Class<?> clazz = closeable.getClass();
+		try {
+			clazz.getMethod("close", new Class[0]).invoke(closeable, new Object[0]);
+		} catch (Exception e) {
+			Throwable cause = e.getCause();
+			if(cause==null)	Log.log(e);
+			else Log.log(cause);
+		}
 	}
 
 }

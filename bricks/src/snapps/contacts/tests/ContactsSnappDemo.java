449dbd69f12b54a119b62cc6e5f51f1c6b884e3f
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-08-28 22:38:19 +0000

    add nickname test
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2081 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/snapps/contacts/tests/ContactsSnappDemo.java
+++ bricks/src/snapps/contacts/tests/ContactsSnappDemo.java
@@ -1,16 +1,22 @@
 package snapps.contacts.tests;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JComponent;
+import javax.swing.border.TitledBorder;
+
 import snapps.contacts.ContactsSnapp;
 import sneer.kernel.container.Container;
 import sneer.kernel.container.ContainerUtils;
-import sneer.kernel.container.Inject;
+import sneer.pulp.contacts.Contact;
 import sneer.pulp.contacts.ContactManager;
 import sneer.skin.dashboard.Dashboard;
 import sneer.skin.snappmanager.SnappManager;
 import sneer.skin.viewmanager.Snapp;
 import sneer.skin.widgets.reactive.RFactory;
-import wheel.lang.Omnivore;
+import wheel.reactive.Register;
 import wheel.reactive.Signal;
+import wheel.reactive.impl.RegisterImpl;
 
 public class ContactsSnappDemo  {
 	
@@ -23,42 +29,55 @@ public class ContactsSnappDemo  {
 		container.produce(Dashboard.class);
 		container.produce(ContactsSnapp.class);
 		
-//		RFactory rfactory = container.produce(RFactory.class);
+		RFactory rfactory = container.produce(RFactory.class);
 
 		ContactManager manager = container.produce(ContactManager.class);
 		
+
 		manager.addContact("Sandro");
-		manager.addContact("Sandro1");
-		manager.addContact("Sandro2");
-		manager.addContact("Sandro3");
-//		Contact contact = manager.contactGiven("Sandro");
-//		Register<Contact> register = new RegisterImpl<Contact>(contact);
-//		SnappTest test = new SnappTest(rfactory, register.output(), register.setter());
+		manager.addContact("Klaus");
+		manager.addContact("Bamboo");
+		manager.addContact("Nell");
+		
+		ContactTest test = new ContactTest();
+		manager.getContacts().add(test);
+
+		SnappManager snapps = container.produce(SnappManager.class);
+		new NicknameTest(rfactory, snapps, test);
 		
 		
 	}
 }
-class SnappTest implements Snapp{
+class ContactTest implements Contact{
+	
+	Register<String> nickname = new RegisterImpl<String>("Teste!");
+
+	@Override
+	public Signal<String> nickname() {
+		return nickname.output();
+	}
+
+}
+
+class NicknameTest implements Snapp{
 
 	private final RFactory _rfactory;
-	private final Signal<String> _output;
-	private final Omnivore<String> _setter;
-	
-	@Inject
-	static private SnappManager _snapps;
+	private final SnappManager _snapps;
+	private final ContactTest _test;
 
-	public SnappTest(RFactory rfactory, Signal<String> output,	Omnivore<String> setter) {
+	public NicknameTest(RFactory rfactory, SnappManager snapps, ContactTest test) {
 		_rfactory = rfactory;
-		_output = output;
-		_setter = setter;
+		_snapps = snapps;
+		_test = test;
 		_snapps.registerSnapp(this);
-	} 
+	}
 
 	@Override
 	public void init(java.awt.Container container) {
-		container.add(
-			_rfactory.newEditableLabel(_output, _setter, true).getComponent()
-		);
+		container.setLayout(new BorderLayout());
+		JComponent field = _rfactory.newEditableLabel(_test.nickname(), _test.nickname.setter(), true).getComponent();
+		container.add(field);
+		field.setBorder(new TitledBorder("Change Nickname Here:"));
 	}
 	@Override
 	public String getName() {

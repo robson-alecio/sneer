92393251fc4d8c1901e815c4a8728f069712e5d3
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-02 03:04:53 +0000

    ConnectionImpl debugging info removed.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2114 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/pulp/connection/impl/ConnectionImpl.java
+++ bricks/src/sneer/pulp/connection/impl/ConnectionImpl.java
@@ -142,8 +142,7 @@ class ConnectionImpl implements ByteConnection {
 		} catch (IOException e) {
 			_logger.info(e.getMessage(), e);
 			_socketHolder.crash(mySocket);
-			throw new RuntimeException(e);
-			//return null;
+			return null;
 		} 
 
 	}

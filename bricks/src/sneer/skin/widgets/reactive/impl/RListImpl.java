fe8f64c73a9050e1f7fc223e1bc0d115c3f0eb8a
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-03 03:57:03 +0000

    Bug Fix RListImpl - remove reactive components from DefaultListRendere - renderer without list line cache - using SignalChoser
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2129 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/skin/widgets/reactive/impl/RListImpl.java
+++ bricks/src/sneer/skin/widgets/reactive/impl/RListImpl.java
@@ -2,28 +2,18 @@ package sneer.skin.widgets.reactive.impl;
 
 import static wheel.lang.Types.cast;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Image;
-import java.util.HashMap;
-import java.util.Map;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
-import javax.swing.UIManager;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
+import javax.swing.SwingUtilities;
 
-import sneer.kernel.container.Container;
-import sneer.kernel.container.ContainerUtils;
-import sneer.skin.widgets.reactive.ImageWidget;
 import sneer.skin.widgets.reactive.LabelProvider;
 import sneer.skin.widgets.reactive.ListWidget;
-import sneer.skin.widgets.reactive.RFactory;
-import sneer.skin.widgets.reactive.TextWidget;
 import wheel.io.ui.impl.ListSignalModel;
 import wheel.lang.Omnivore;
 import wheel.reactive.Signal;
@@ -39,10 +29,6 @@ public class RListImpl<ELEMENT> extends JList implements ListWidget<ELEMENT> {
 
 	private final ListSignal<ELEMENT> _source;
 
-	private static final Border noFocusBorder = new EmptyBorder(1, 1, 1, 1);
-	private static final Border SAFE_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1,
-			1);
-
 	private final Omnivore<ListValueChange> _listReceiver = new Omnivore<ListValueChange>() {
 		@Override
 		public void consume(ListValueChange valueObject) {
@@ -51,8 +37,15 @@ public class RListImpl<ELEMENT> extends JList implements ListWidget<ELEMENT> {
 	};
 
 	private void repaintList() {
-		revalidate();
-		repaint();
+		SwingUtilities.invokeLater(
+				new Runnable() {
+					@Override
+					public void run() {
+						revalidate();
+						repaint();
+					}
+				}
+		);	
 	}
 
 	RListImpl(ListSignal<ELEMENT> source, LabelProvider<ELEMENT> labelProvider) {
@@ -63,71 +56,31 @@ public class RListImpl<ELEMENT> extends JList implements ListWidget<ELEMENT> {
 
 		class DefaultListCellRenderer implements ListCellRenderer {
 
-			Map<Object, JPanel> cacheLines = new HashMap<Object, JPanel>(); //Fix this might be a leak
-
-			@SuppressWarnings("unused")
-			Receiver<Object> _listRepainter = new Receiver<Object>() {
-				@Override
-				public void consume(Object ignore) {
-					repaintList();
-				}
-			};
-
-			private Border getNoFocusBorder() {
-				if (System.getSecurityManager() != null) {
-					return SAFE_NO_FOCUS_BORDER;
-				}
-				return noFocusBorder;
-			}
+			private javax.swing.DefaultListCellRenderer renderer = new javax.swing.DefaultListCellRenderer();
 
 			@Override
-			public Component getListCellRendererComponent(JList ignored,
-					Object value, int ignored2, boolean isSelected,
-					boolean cellHasFocus) {
-				JPanel panel;
-				if (cacheLines.containsKey(value)) {
-					panel = cacheLines.get(value);
-				} else {
-					Container container = ContainerUtils.getContainer();
-					RFactory rfactory = container.produce(RFactory.class);
-
-					panel = new JPanel();
-					panel.setOpaque(false);
-					panel.setLayout(new BorderLayout(5, 5));
-
-					Signal<String> slabel = _labelProvider
-							.labelFor(getElement(value));
-					Signal<Image> sicon = _labelProvider
-							.imageFor(getElement(value));
-
-					TextWidget<JLabel> label = rfactory.newLabel(slabel);
-					panel.add(label.getComponent(), BorderLayout.CENTER);
-
-					ImageWidget image = rfactory.newImage(sicon);
-					panel.add(image.getComponent(), BorderLayout.WEST);
-					panel.setOpaque(false);
-
-					_listRepainter.addToSignal(label.output());
-					_listRepainter.addToSignal(image.output());
-
-					cacheLines.put(value, panel);
-				}
-
-				Border border = null;
-				if (cellHasFocus) {
-					if (isSelected) {
-						border = UIManager
-								.getBorder("List.focusSelectedCellHighlightBorder");
+			public Component getListCellRendererComponent(JList ignored, Object value, int ignored2, boolean isSelected, boolean cellHasFocus) {
+				
+//				System.out.println(Thread.currentThread());
+				
+				Signal<String> slabel = _labelProvider.labelFor(getElement(value));
+				Signal<Image> sicon = _labelProvider.imageFor(getElement(value));
+				
+				ImageIcon icon = new ImageIcon(sicon.currentValue());
+				JLabel label = (JLabel) renderer.getListCellRendererComponent(ignored, value, ignored2, isSelected, cellHasFocus);
+				label.setIcon(icon);
+				label.setText(slabel.currentValue());
+				
+				
+				@SuppressWarnings("unused")
+				Receiver<Object> _listRepainter = new Receiver<Object>() {
+					@Override
+					public void consume(Object ignore) {
+						repaintList();
 					}
-					if (border == null) {
-						border = UIManager
-								.getBorder("List.focusCellHighlightBorder");
-					}
-				} else {
-					border = getNoFocusBorder();
-				}
-				panel.setBorder(border);
-				return panel;
+				};
+
+				return label;
 			}
 
 			private ELEMENT getElement(Object value) {
@@ -143,14 +96,13 @@ public class RListImpl<ELEMENT> extends JList implements ListWidget<ELEMENT> {
 	}
 
 	private void initModel() {
-
-		//		setModel(new ListSignalModel<ELEMENT>(_source, new ListSignalModel.SignalChooser<ELEMENT>(){
-		//			@Override
-		//			public Signal<?>[] signalsToReceiveFrom(ELEMENT element) {
-		//				return new Signal<?>[]{_labelProvider.imageFor(element), 
-		//									   _labelProvider.labelFor(element)};
-		//			}}));
-		setModel(new ListSignalModel<ELEMENT>(_source));
+//		setModel(new ListSignalModel<ELEMENT>(_source));
+		setModel(new ListSignalModel<ELEMENT>(_source, new ListSignalModel.SignalChooser<ELEMENT>(){
+		@Override
+		public Signal<?>[] signalsToReceiveFrom(ELEMENT element) {
+			return new Signal<?>[]{_labelProvider.imageFor(element), 
+								   _labelProvider.labelFor(element)};
+		}}));
 	}
 
 	@Override

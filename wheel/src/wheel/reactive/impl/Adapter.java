5a67ed8a5f56859a876396d5e0e5f45b10d2cfd5
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-03 20:02:00 +0000

    - Using new Receiver(signal)" instead of signal.addReceiver(Omnivore) - Removing SignalOwnerReference
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2137 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- wheel/src/wheel/reactive/impl/Adapter.java
+++ wheel/src/wheel/reactive/impl/Adapter.java
@@ -1,24 +1,23 @@
 package wheel.reactive.impl;
 
 import wheel.lang.Functor;
-import wheel.lang.Omnivore;
 import wheel.reactive.Register;
 import wheel.reactive.Signal;
 
 public class Adapter<T1, T2> {
 
-	private Omnivore<T1> _receiver;
+	@SuppressWarnings("unused")
+	private Receiver<T1> _receiver;
+	
 	private Register<T2> _register = new RegisterImpl<T2>(null);
 
 	public Adapter(Signal<T1> input, final Functor<T1, T2> functor) {
-		_receiver = new Omnivore<T1>() { @Override public void consume(T1 inputValue) {
+		_receiver = new Receiver<T1>(input) { @Override public void consume(T1 inputValue) {
 			_register.setter().consume(functor.evaluate(inputValue));
 		}};
-		input.addReceiver(_receiver);
 	}
 
 	public Signal<T2> output() {
-		return new SignalOwnerReference<T2>(_register.output(), this);
+		return _register.output();
 	}
-
 }

896ba5aea73bbf43a1e770e0de361dbbddc91653
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-03 20:41:14 +0000

    - Using new Receiver(signal)" instead of signal.addReceiver(Omnivore) - Using SwingUtilities.invokeLater now
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2139 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/pulp/own/name/tests/OwnNameKeeperDemo.java
+++ bricks/src/sneer/pulp/own/name/tests/OwnNameKeeperDemo.java
@@ -13,9 +13,13 @@ import sneer.kernel.container.tests.BrickTestSupport;
 import sneer.pulp.own.name.OwnNameKeeper;
 import sneer.skin.widgets.reactive.RFactory;
 import sneer.skin.widgets.reactive.TextWidget;
-import wheel.lang.Omnivore;
+import wheel.reactive.impl.Receiver;
+
 
 public class OwnNameKeeperDemo extends BrickTestSupport {
+
+	@SuppressWarnings("unused")
+	private static Receiver<String> _receiver;
 
 	@Inject
 	private RFactory rfactory;
@@ -27,14 +31,10 @@ public class OwnNameKeeperDemo extends BrickTestSupport {
 		OwnNameKeeperDemo demo = initializeDemo();
 		createWidgets(demo);
 		
-		demo.ownNameKeeper.name().addReceiver(
-				new Omnivore<String>(){
-					@Override
-					public void consume(String valueObject) {
-						System.out.println(valueObject);
-					}
-				}
-			);
+		_receiver = 
+			new Receiver<String>(demo.ownNameKeeper.name()){@Override public void consume(String valueObject) {
+				System.out.println(valueObject);
+			}};
 	}
 	
 	private static OwnNameKeeperDemo initializeDemo() {
@@ -49,30 +49,22 @@ public class OwnNameKeeperDemo extends BrickTestSupport {
 	private static void createWidgets(OwnNameKeeperDemo demo) {
 		
 		TextWidget<JTextField> newTextField1 = demo.rfactory.newTextField(demo.ownNameKeeper.name(), demo.ownNameKeeper.nameSetter());
-		final JFrame frm1 =createTestFrame(newTextField1);
+		createTestFrame(newTextField1, 10, 10, 300, 100);
 		
 		TextWidget<JTextField> newTextField2 = demo.rfactory.newEditableLabel(demo.ownNameKeeper.name(), demo.ownNameKeeper.nameSetter());
-		final JFrame frm2 =createTestFrame(newTextField2);
-		
-		frm1.setBounds(10, 10, 300, 100);
-		frm2.setBounds(10, 120, 300, 100);
-		
-		SwingUtilities.invokeLater(
-			new Runnable(){
-				@Override
-				public void run() {
-					frm1.setVisible(true);
-					frm2.setVisible(true);
-				}
-			}
-		);
+		createTestFrame(newTextField2, 10, 120, 300, 100);
 	}
 
-	private static JFrame createTestFrame(final TextWidget<?> textWidget) {
-		JFrame frm = new JFrame(textWidget.getClass().getSimpleName());
-		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frm.getContentPane().setLayout(new FlowLayout());
-		frm.getContentPane().add(textWidget.getComponent());
-		return frm;
+	private static void createTestFrame(final TextWidget<?> textWidget, final int x, final int y, final int w, final int h) {
+
+		SwingUtilities.invokeLater(	new Runnable(){@Override
+			public void run() {
+				JFrame frm = new JFrame(textWidget.getClass().getSimpleName());
+				frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frm.getContentPane().setLayout(new FlowLayout());
+				frm.getContentPane().add(textWidget.getComponent());
+				frm.setBounds(x, y, w, h);
+				frm.setVisible(true);
+			}});
 	}
 }

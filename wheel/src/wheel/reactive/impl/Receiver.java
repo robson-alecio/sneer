8ecd4756d73c1274dad00193db9b6dcee6b5058a
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-03 22:41:13 +0000

    Using EventSource instead Signal
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2151 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- wheel/src/wheel/reactive/impl/Receiver.java
+++ wheel/src/wheel/reactive/impl/Receiver.java
@@ -4,37 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 import wheel.lang.Omnivore;
-import wheel.reactive.Signal;
+import wheel.reactive.EventSource;
 
 public abstract class Receiver<T> implements Omnivore<T> {
 	
-	private final List<Signal<? extends T>> _signals = new ArrayList<Signal<? extends T>>();
+	private final List<EventSource<? extends T>> _eventSources = new ArrayList<EventSource<? extends T>>();
 
-	public Receiver(Signal<T> signal) {
+	public Receiver(EventSource<T> signal) {
 		addToSignal(signal);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public Receiver(Signal... signals) {
-		for (Signal<T> signal : signals) {
+	public Receiver(EventSource... signals) {
+		for (EventSource<T> signal : signals) {
 			addToSignal(signal);
 		}
 	}
 
 	@SuppressWarnings("deprecation")
-	public void addToSignal(Signal<? extends T> signal) {
+	public void addToSignal(EventSource<? extends T> signal) {
 		if (null == signal)
 			throw new IllegalArgumentException();
-		if (_signals.add(signal)) {
+		if (_eventSources.add(signal)) {
 			signal.addReceiver(this);
 		}
 	}
 	
 	@SuppressWarnings("deprecation")
 	public void removeFromSignals() {
-		for (Signal<? extends T> signal : _signals) {
+		for (EventSource<? extends T> signal : _eventSources) {
 			signal.removeReceiver(this);
 		}
-		_signals.clear();
+		_eventSources.clear();
 	}
 }

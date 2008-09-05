5ed7b1b885613714285c9c94782ad39c7e65ee98
rodrigob <rodrigob@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-05 21:33:56 +0000

    minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2194 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/pulp/httpclient/impl/HttpClientImpl.java
+++ bricks/src/sneer/pulp/httpclient/impl/HttpClientImpl.java
@@ -1,6 +1,7 @@
 package sneer.pulp.httpclient.impl;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -14,11 +15,22 @@ class HttpClientImpl implements HttpClient {
 	@Override
 	public String get(String url, Pair<String, String>... headers) throws IOException {
 		final URLConnection connection = new URL(url).openConnection();
+		setRequestProperties(connection, headers);
+		return readString(connection);
+	}
+
+	private void setRequestProperties(final URLConnection connection, Pair<String, String>... headers) {
 		for (Pair<String, String> header : headers) {
 			connection.setRequestProperty(header._a, header._b);
 		}
-		connection.connect();
-		return IOUtils.toString(connection.getInputStream());
 	}
 
+	private String readString(final URLConnection connection) throws IOException {
+		final InputStream is = connection.getInputStream();
+		try {
+			return IOUtils.toString(is);
+		} finally {
+			is.close();
+		}
+	}
 }

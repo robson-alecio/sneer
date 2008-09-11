f6f3569ea28e1eb655d7f8706e2fe9097892c1f4
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-11 03:43:42 +0000

    change tests using triggerAlarm
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2246 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/pulp/blinkinglights/tests/BlinkingLightsTest.java
+++ bricks/src/sneer/pulp/blinkinglights/tests/BlinkingLightsTest.java
@@ -5,11 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import org.jmock.Expectations;
-import org.jmock.Mockery;
-import org.jmock.api.Invocation;
-import org.jmock.integration.junit4.JUnit4Mockery;
-import org.jmock.lib.action.CustomAction;
 import org.junit.Test;
 
 import sneer.kernel.container.Inject;
@@ -17,16 +12,14 @@ import sneer.kernel.container.tests.TestThatIsInjected;
 import sneer.pulp.blinkinglights.BlinkingLights;
 import sneer.pulp.blinkinglights.Light;
 import sneer.pulp.blinkinglights.impl.LightImpl;
-import sneer.pulp.clock.Clock;
+import sneer.pulp.clock.mocks.ClockMock;
 
 public class BlinkingLightsTest extends TestThatIsInjected {
 
 	@Inject
 	private BlinkingLights _lights;
 
-	final Mockery context = new JUnit4Mockery();
-	final BlinkingLights blinkingLights = context.mock(BlinkingLights.class);
-	final Clock clock = context.mock(Clock.class);
+	final ClockMock clock = new ClockMock();
 	
 	@Test
 	public void testLights() throws Exception {
@@ -53,30 +46,12 @@ public class BlinkingLightsTest extends TestThatIsInjected {
 		final String message = "some error";
 		final NullPointerException exception = new NullPointerException();
 		final int timeout = 1000;
-		final Runnable clockTriger[] = new Runnable[1];
-		
-		context.checking(new Expectations() {{
-			
-			exactly(1).of(clock).addAlarm(with(timeout), with(any(Runnable.class)));
-				will(new CustomAction("call clock.addAlarm()"){@Override public Object invoke(Invocation invocation) {
-					clockTriger[0] = (Runnable)invocation.getParameter(1);
-					return null;
-				}});
-			
-			exactly(1).of(blinkingLights).turnOff(with(any(Light.class)));
-				will(new CustomAction("call blinkingLights.turnOff()"){@Override public Object invoke(Invocation invocation) {
-					((Light)invocation.getParameter(0)).turnOff();
-					return null;
-				}});
-			
-			
-		}});
 
-		final Light light = new LightImpl(message, exception, timeout, clock, blinkingLights);
+		final Light light = new LightImpl(message, exception, timeout, clock, _lights);
 		
 		assertTrue(light.isOn());
 		
-		clockTriger[0].run();
+		clock.triggerAlarm(0);
 		
 		assertFalse(light.isOn());
 	}

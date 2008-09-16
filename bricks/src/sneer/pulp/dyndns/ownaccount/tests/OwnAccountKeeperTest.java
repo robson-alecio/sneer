0e27b44d535551bf729df460d39388461165dc9f
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-16 05:59:43 +0000

    calling socket.setTcpNoDelay(true) and dyndns.OwnAccountKeeperImpl done
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2331 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/pulp/dyndns/ownaccount/tests/OwnAccountKeeperTest.java
+++ bricks/src/sneer/pulp/dyndns/ownaccount/tests/OwnAccountKeeperTest.java
@@ -1,11 +1,13 @@
 package sneer.pulp.dyndns.ownaccount.tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 import sneer.kernel.container.Inject;
 import sneer.kernel.container.tests.TestThatIsInjected;
+import sneer.pulp.dyndns.ownaccount.Account;
 import sneer.pulp.dyndns.ownaccount.OwnAccountKeeper;
 
 public class OwnAccountKeeperTest extends TestThatIsInjected {
@@ -15,9 +17,16 @@ public class OwnAccountKeeperTest extends TestThatIsInjected {
 	
 	@Test
 	public void testAccountKeeper() {
-		assertNull(_subject.ownAccount().currentValue());
+		assertNull(ownAccount());
 		
-		//_subject.accountSetter().consume(new Account());
+		_subject.accountSetter().consume(new Account("neide.dyndns.org", "neide", "abc123"));
+		assertEquals("neide.dyndns.org", ownAccount().host);
+		assertEquals("neide", ownAccount().dynDnsUser);
+		assertEquals("abc123", ownAccount().password);
+	}
+
+	private Account ownAccount() {
+		return _subject.ownAccount().currentValue();
 	} 
 	
 }

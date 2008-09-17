cb09a2d530b03c22bd1347513a727989578cc9de
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-17 02:09:45 +0000

    commit real implementation
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2353 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/sneer/pulp/clock/impl/ClockImpl.java
+++ bricks/src/sneer/pulp/clock/impl/ClockImpl.java
@@ -1,6 +1,8 @@
 package sneer.pulp.clock.impl;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -55,47 +57,45 @@ class ClockImpl implements Clock {
 		_currentTimeMillis = absoluteTimeMillis;
 		checkTime();
 	}
-
 	
 	private void checkTime() {
-		boolean tryAgain = true;
 
-		while(tryAgain) {
-			System.err.println("Sandro please fix this bug. " + System.currentTimeMillis());
-			Iterator<Alarm> iterator = _alarms.iterator();
+		List<Alarm> tmpPeriodics = new ArrayList<Alarm>();
+		Iterator<Alarm> iterator = _alarms.iterator();
 			
-			while(iterator.hasNext()) {
-				Alarm alarm = iterator.next();
-				
-				tryAgain = alarm.tryRunAndRemove(iterator);
-				
-				if(!tryAgain) //Break Last Timeout
-					break;				
-
-				if(alarm._increment>0){ //Break Periodic
-					break;
-				}
-				
-				tryAgain=false;
-			}
+		while(iterator.hasNext()) {
+			Alarm alarm = iterator.next();
+			
+			if(alarm.isPeriodic()) //Store Periodic
+				tmpPeriodics.add(alarm);
+			
+			if(!alarm.tryRunAndRemove(iterator)) //Break Last Timeout
+				break;				
 		}
+		
+		_alarms.addAll(tmpPeriodics);
+		
 	}
 
 	private class Alarm implements Comparable<Alarm>{
 		
 		final int _increment;
 		
-		long _nextAlarmAbsoluteTimeMillies;
+		long _nextAlarmAbsoluteTimeMillis;
 		final Runnable _runnable;
 
 		Alarm(Runnable runnable, int millisFromCurrentTime, boolean isPeriodic) {
 			_increment = isPeriodic ? millisFromCurrentTime : 0;
-			_nextAlarmAbsoluteTimeMillies = millisFromCurrentTime + _currentTimeMillis;
+			_nextAlarmAbsoluteTimeMillis = millisFromCurrentTime + _currentTimeMillis;
 			_runnable = runnable;
 		}
 		
+		public boolean isPeriodic() {
+			return _increment>0;
+		}
+		
 		boolean tryRunAndRemove(Iterator<Alarm> iterator){
-			if(_currentTimeMillis <= _nextAlarmAbsoluteTimeMillies )
+			if(_currentTimeMillis <= _nextAlarmAbsoluteTimeMillis )
 				return false;
 			
 			_runnable.run();
@@ -104,19 +104,18 @@ class ClockImpl implements Clock {
 			if(_increment==0)  
 				return true; //NotPeriodic
 				
-			_nextAlarmAbsoluteTimeMillies = _nextAlarmAbsoluteTimeMillies+_increment;   //Periodic.incrementTime 
-			_alarms.add(this);
+			_nextAlarmAbsoluteTimeMillis = _nextAlarmAbsoluteTimeMillis+_increment;   //Periodic.incrementTime 
 			return true;
 		}
 
 		@Override
 		public int compareTo(Alarm alarm) {
-			return (int) (_nextAlarmAbsoluteTimeMillies-alarm._nextAlarmAbsoluteTimeMillies);
+			return (int) (_nextAlarmAbsoluteTimeMillis-alarm._nextAlarmAbsoluteTimeMillis);
 		}
 		
 		@Override
 		public String toString() {
-			return "Alarm: " + _nextAlarmAbsoluteTimeMillies;
+			return "Alarm: " + _nextAlarmAbsoluteTimeMillis;
 		}
 	}
 

473de59ba8585843d1eb903972b8d8845c0c6cae
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-23 15:53:11 +0000

    Fix comment added
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2434 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- wheel/src/wheel/lang/Consumer.java
+++ wheel/src/wheel/lang/Consumer.java
@@ -3,6 +3,6 @@ package wheel.lang;
 
 public interface Consumer<T> {
 
-	void consume(T value); //throws IllegalParameter; 	// For now, throwing of the exception has been removed because of a bug in the Eclipse compiler . Refactor: See wheel.lang.exceptions.IllegalParameter and throw the Exception again. 
+	void consume(T value); //throws IllegalParameter; 	// Fix For now, throwing of the exception has been removed because of a bug in the Eclipse compiler . Refactor: See wheel.lang.exceptions.IllegalParameter and throw the Exception again. 
 
 }
--- wheel/src/wheel/lang/exceptions/IllegalParameter.java
+++ wheel/src/wheel/lang/exceptions/IllegalParameter.java
@@ -3,7 +3,7 @@ package wheel.lang.exceptions;
 /** The checked counterpart of java.lang.IllegalArgumentException
  * It is not called simply IllegalArgument because it is easy to confuse them and throw the unchecked IllegalArgumentException causing bugs.
  * 
- * For now, it has been made a RuntimeException because of a bug in the Eclipse compiler . Refactor: Make this a checked Exception again and run all tests once this bug is fixed: https://bugs.eclipse.org/bugs/show_bug.cgi?id=238484 See wheel.lang.Consumer . Eclipse 3.4.0 still has the bug. 
+ * Fix For now, it has been made a RuntimeException because of a bug in the Eclipse compiler . Refactor: Make this a checked Exception again and run all tests once this bug is fixed: https://bugs.eclipse.org/bugs/show_bug.cgi?id=238484 See wheel.lang.Consumer . Eclipse 3.4.0 still has the bug. 
  */
 public class IllegalParameter extends RuntimeException {
 

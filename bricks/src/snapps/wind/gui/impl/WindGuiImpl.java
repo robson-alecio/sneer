eb23ec6fee890160cc3c77545c1743a5ad4bcb93
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-09-25 21:58:47 +0000

    Minor cleanup
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2438 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- bricks/src/snapps/wind/gui/impl/WindGuiImpl.java
+++ bricks/src/snapps/wind/gui/impl/WindGuiImpl.java
@@ -4,13 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
-import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -20,8 +14,6 @@ import snapps.wind.Shout;
 import snapps.wind.Wind;
 import snapps.wind.gui.WindGui;
 import sneer.kernel.container.Inject;
-import sneer.pulp.blinkinglights.Light;
-import sneer.pulp.blinkinglights.LightType;
 import sneer.pulp.keymanager.KeyManager;
 import sneer.skin.snappmanager.InstrumentManager;
 import sneer.skin.widgets.reactive.LabelProvider;
@@ -73,33 +65,6 @@ class WindGuiImpl implements WindGui {
 			: _otherImage;
 	}	
 
-	private void showMessage(Light light){
-		LightType type = light.type();
-		String title = type.name();
-		int optType = type==LightType.ERROR?JOptionPane.ERROR_MESSAGE: 
-					  type==LightType.WARN? JOptionPane.WARNING_MESSAGE:
-						  				 JOptionPane.INFORMATION_MESSAGE;
-		
-		JOptionPane.showMessageDialog(_container, createMessage(light), title, optType);
-	}
-
-	private String createMessage(Light light) {
-		String stack = "";
-		String msg = "";
-		
-		if(light.error()!=null){
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			PrintStream ps = new PrintStream(out);
-			light.error().printStackTrace(ps);
-			stack = "\n " + new String(out.toByteArray());
-			ps.close();
-			msg = light.error().getMessage();
-		}
-		
-		msg = msg==null?"":msg;
-		
-		return light.message() + "\n " + msg + stack;
-	}	
 	@Override
 	public void init(Container container) {
 		_container = container;
@@ -107,7 +72,6 @@ class WindGuiImpl implements WindGui {
 		_myShout = _rfactory.newTextField(new Constant<String>(""), _wind.megaphone(), true);
 		
 		iniGui();
-		initMouseListener();
 	}
 
 	private void iniGui() {
@@ -122,22 +86,6 @@ class WindGuiImpl implements WindGui {
 		scrollPane.setBorder(new TitledBorder(new EmptyBorder(5,5,5,5), getName()));
 		_shoutsList.getComponent().setBorder(new EmptyBorder(0,0,0,0));
 		scrollPane.setBackground(_shoutsList.getComponent().getBackground());
-		
-		
-	}
-	
-	private void initMouseListener() {
-		_shoutsList.getComponent().addMouseListener(new MouseAdapter(){ @Override public void mouseReleased(final MouseEvent event) {
-			Light light = getClickedLight(event);
-			showMessage(light);
-		}
-		
-		private Light getClickedLight(final MouseEvent event) {
-			JList list = (JList)event.getSource();
-			list.setSelectedIndex(list.locationToIndex(event.getPoint()));
-			Light light = (Light)list.getSelectedValue();
-			return light;
-		}});
 	}
 
 	private Dimension size(Container container) {

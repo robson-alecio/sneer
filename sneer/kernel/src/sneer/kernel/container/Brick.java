6fe7bfd975693ab57b819a76bf655494c580bb3d
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-01 19:24:02 +0000

    Brick defaults to "Synchronized Brick"
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2480 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/kernel/src/sneer/kernel/container/Brick.java
+++ sneer/kernel/src/sneer/kernel/container/Brick.java
@@ -1,9 +1,14 @@
 package sneer.kernel.container;
 
 /**
- * Marker interface for Bricks. All implementations of all interfaces referenced
- * by a brick interface must either declare "throws Hiccup" or return in under
- * one microsecond.
+ * Marker interface for Bricks.
+ * 
+ * All implementations of all interfaces referenced by a Brick interface must
+ * either declare "throws Hiccup" or return in under one microsecond.
+ * 
+ * All method calls to all classes in a Brick, including callbacks, will be
+ * synchronized by the container.
  */
 public interface Brick {
+
 }
--- sneer/kernel/src/sneer/kernel/container/SynchronizedBrick.java
+++
@@ -1,6 +0,0 @@
-package sneer.kernel.container;
-
-/** All method calls to all classes in this brick, including callbacks, will be synchronized by the container. The brick itself will be used as the synchronization monitor. */
-public interface SynchronizedBrick extends Brick {
-
-}

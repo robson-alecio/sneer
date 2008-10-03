bb97878a458a2109a71eb59ee50e9ae08281aff0
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-03 14:19:12 +0000

    minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2502 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/widgets/reactive/impl/RListHtmlCellRenderer.java
+++ sneer/bricks/src/sneer/skin/widgets/reactive/impl/RListHtmlCellRenderer.java
@@ -8,13 +8,13 @@ import java.awt.Image;
 import java.awt.Insets;
 
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import sneer.skin.widgets.resizer.Resizer;
-
 import wheel.reactive.Signal;
 import wheel.reactive.impl.Receiver;
 
@@ -32,29 +32,9 @@ class RListHtmlCellRenderer<ELEMENT> extends RListSimpleCellRenderer<ELEMENT>  {
 	public Component getListCellRendererComponent(JList ignored, Object value,
 			int ignored2, boolean isSelected, boolean cellHasFocus) {
 
-		Signal<String> signalText = _rlist._labelProvider.labelFor(getElement(value));
-		Signal<Image> signalImage = _rlist._labelProvider.imageFor(getElement(value));
-
-		JPanel root = new JPanel();
-		root.setLayout(new GridBagLayout());
-		root.setOpaque(false);
-
-		JLabel icon = new JLabel(new ImageIcon(signalImage.currentValue()));
-		icon.setOpaque(false);
-
-		JTextArea area = new JTextArea();
-		area.setWrapStyleWord(true);
-		area.setLineWrap(true);
-		area.setText(signalText.currentValue());
-
-		root.add(icon, new GridBagConstraints(0, 0, 1, 1, 0, 0,
-				GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
-				new Insets(0, 0, 0, 0), 0, 0));
-
-		root.add(area, new GridBagConstraints(1, 0, 1, 1, 1., 1.,
-				GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH,
-				new Insets(0, 0, 0, 0), 0, 0));
-		;
+		JComponent icon = createIcon(value);
+		JComponent area = createText(value);
+		JComponent root = createRootPanel(icon, area);
 
 		new Receiver<Object>() {
 			@Override
@@ -67,8 +47,39 @@ class RListHtmlCellRenderer<ELEMENT> extends RListSimpleCellRenderer<ELEMENT>  {
 		addLineSpace(root);
 		return root;
 	}
+	
+	private JComponent createIcon(Object value) {
+		Signal<Image> signalImage = _rlist._labelProvider.imageFor(getElement(value));
+		JLabel icon = new JLabel(new ImageIcon(signalImage.currentValue()));
+		icon.setOpaque(false);
+		return icon;
+	}
+	
+	private JComponent createText(Object value) {
+		Signal<String> signalText = _rlist._labelProvider.labelFor(getElement(value));
+		JTextArea area = new JTextArea();
+		area.setWrapStyleWord(true);
+		area.setLineWrap(true);
+		area.setText(signalText.currentValue());
+		return area;
+	}
 
-	private void addLineSpace(JPanel root) {
+	private JComponent createRootPanel(JComponent icon, JComponent area) {
+		JPanel root = new JPanel();
+		root.setLayout(new GridBagLayout());
+		root.setOpaque(false);
+
+		root.add(icon, new GridBagConstraints(0, 0, 1, 1, 0, 0,
+				GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+				new Insets(0, 0, 0, 0), 0, 0));
+
+		root.add(area, new GridBagConstraints(1, 0, 1, 1, 1., 1.,
+				GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 0), 0, 0));
+		return root;
+	}
+
+	private void addLineSpace(JComponent root) {
 		Dimension psize = root.getPreferredSize();
 		root.setPreferredSize(new Dimension(psize.width, psize.height + _rlist.getLineSpace()));
 	}

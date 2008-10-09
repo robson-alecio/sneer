c9ca579b548d072ab1a7133dd379345bf68742f8
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-09 18:10:20 +0000

    Priscila's contact added
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2569 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/tests/src/main/impl/MainDemoBrickImpl.java
+++ sneer/tests/src/main/impl/MainDemoBrickImpl.java
@@ -127,8 +127,9 @@ class MainDemoBrickImpl implements MainDemoBrick {
 			new ContactInfo("Douglas", "dtgiacomini.dyndns.com", 5923),
 			new ContactInfo("Duno", "duno.dyndns.org", 5923),
 			new ContactInfo("Klaus", "klausw.selfip.net", 5923),
-			new ContactInfo("Nell", "anelisedaux.dyndns.org", 5924),
 			new ContactInfo("Localhost", "localhost", 7777),
+			new ContactInfo("Nell", "anelisedaux.dyndns.org", 5924),
+			new ContactInfo("Priscila", "priscilavriesman.dyndns.org", 7770),
 		};
 	}
 

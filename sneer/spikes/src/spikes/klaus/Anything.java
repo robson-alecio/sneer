50904d3d42677cf1af0720de13af4f29585151c8
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-15 14:03:54 +0000

    Spiking with png format
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2616 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/src/spikes/klaus/Anything.java
+++ sneer/spikes/src/spikes/klaus/Anything.java
@@ -1,14 +1,19 @@
 package spikes.klaus;
 
-import javax.swing.JWindow;
+
+
+
 
 public class Anything {
 
 	public static void main(String[] args) {
-		JWindow window = new JWindow();
-		window.setBounds(0, 0, 300, 300);
-		window.setName("Banana");
-		window.setVisible(true);
+		
+//		ImageIO.write(image, "png", output);
+		
+//		JWindow window = new JWindow();
+//		window.setBounds(0, 0, 300, 300);
+//		window.setName("Banana");
+//		window.setVisible(true);
 		
 //		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 //		DisplayMode mode = device.getDisplayMode();

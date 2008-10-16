35052e398b92c5961af58207805403738ce644ae
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-16 00:18:07 +0000

    Spiking with nanoTime
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2638 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/src/spikes/klaus/Anything.java
+++ sneer/spikes/src/spikes/klaus/Anything.java
@@ -6,7 +6,17 @@ package spikes.klaus;
 
 public class Anything {
 
+	private static long _lastTime;
+
 	public static void main(String[] args) {
+		
+		while (true) {
+			long now = now();
+			if ((now - _lastTime) < (1000L * 1000 * 1000 * 5)) continue;
+			System.out.println("" + now + "  " + (now - _lastTime));
+			_lastTime = now;
+		}
+
 		
 //		ImageIO.write(image, "png", output);
 		
@@ -19,5 +29,10 @@ public class Anything {
 //		DisplayMode mode = device.getDisplayMode();
 //		System.out.println("" + mode.getWidth() + " x " + mode.getHeight());
 	}
+	
+	static private long now() {
+		return System.nanoTime();
+	}
+
 
 }

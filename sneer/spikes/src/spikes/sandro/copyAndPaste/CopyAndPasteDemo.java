21b446b2182b7297f466de132a7a8218c03e1d8b
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-17 15:07:46 +0000

    - Added Key Listeners (Ctrl-C and Ctrl-V) - Minor Refactoring (now using swing widgets)
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2662 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/src/spikes/sandro/copyAndPaste/CopyAndPasteDemo.java
+++ sneer/spikes/src/spikes/sandro/copyAndPaste/CopyAndPasteDemo.java
@@ -1,8 +1,7 @@
 package spikes.sandro.copyAndPaste;
 
-import java.awt.Button;
 import java.awt.FlowLayout;
-import java.awt.TextField;
+import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.DataFlavor;
@@ -11,13 +10,18 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
 public class CopyAndPasteDemo extends JFrame implements ClipboardOwner {
 
-	private TextField sourceText = new TextField(40);
+	private JTextField textField = new JTextField(20);
 	private Clipboard clipboard;
 
 	public CopyAndPasteDemo() {
@@ -26,29 +30,48 @@ public class CopyAndPasteDemo extends JFrame implements ClipboardOwner {
 	}
 
 	public void initGui() {
-		Button cmdCopy = new Button("Copy All Text");
-		Button cmdPaste = new Button("Paste in Console");
+		JButton cmdCopy = new JButton("Copy All Text");
+		JButton cmdPaste = new JButton("Paste in Console");
 		setLayout(new FlowLayout());
-		getContentPane().add(sourceText);
+		getContentPane().add(textField);
 		getContentPane().add(cmdCopy);
 		getContentPane().add(cmdPaste);
+		
 		createCopyListener(cmdCopy);
 		createPasteListener(cmdPaste);
+		
+		createKeyMaps();
 	}
 
-	private void createCopyListener(Button cmdCopy) {
-		cmdCopy.addActionListener(new ActionListener(){ @Override public void actionPerformed(ActionEvent arg0) {
-			String text = sourceText.getText();
-			StringSelection fieldContent = new StringSelection(text);
-			clipboard.setContents(fieldContent, CopyAndPasteDemo.this);	
-			logClipboardData("Copy: ");
+	private void createKeyMaps() {
+		int modifiers = getPortableSoModifiers();
+		final KeyStroke ctrlc = KeyStroke.getKeyStroke(KeyEvent.VK_C, modifiers);
+		final KeyStroke ctrlv = KeyStroke.getKeyStroke(KeyEvent.VK_V, modifiers);
+		
+		textField.getInputMap().put(ctrlc,  "ctrlc");
+		textField.getActionMap().put("ctrlc",  new AbstractAction(){@Override public void actionPerformed(ActionEvent e) {
+			copy();
+		}});
+
+		textField.getInputMap().put(ctrlv,  "ctrlv");
+		textField.getActionMap().put("ctrlv",  new AbstractAction(){@Override public void actionPerformed(ActionEvent e) {
+			paste();
 		}});
 	}
 
-	private void createPasteListener(Button cmdPaste) {
-		cmdPaste.addActionListener(new ActionListener(){ @Override public void actionPerformed(ActionEvent e) {
-			logClipboardData("Paste: ");
-		}});
+	private int getPortableSoModifiers() {
+		return Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+	}
+	
+	private void copy() {
+		String text = textField.getText();
+		StringSelection fieldContent = new StringSelection(text);
+		clipboard.setContents(fieldContent, CopyAndPasteDemo.this);	
+		logClipboardData("Copy: ");
+	}
+	
+	private void paste() {
+		logClipboardData("Paste: ");
 	}
 	
 	private void logClipboardData(String label) {
@@ -69,6 +92,18 @@ public class CopyAndPasteDemo extends JFrame implements ClipboardOwner {
 		//do nothing
 	}
 
+	private void createCopyListener(JButton cmdCopy) {
+		cmdCopy.addActionListener(new ActionListener(){ @Override public void actionPerformed(ActionEvent arg0) {
+			copy();
+		}});
+	}
+	
+	private void createPasteListener(JButton cmdPaste) {
+		cmdPaste.addActionListener(new ActionListener(){ @Override public void actionPerformed(ActionEvent e) {
+			paste();
+		}});
+	}
+	
 	public static void main(String[] args) {
 		CopyAndPasteDemo demo = new CopyAndPasteDemo();
 		demo.setBounds(10,10,300,200);

7074756d08f32ab9daaf6197ae91ef2a7f2700f6
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-20 23:55:04 +0000

    Xml file missing for PlayerDemo, so I simplified it - runs with mic now.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2705 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/mic/tests/PlayerDemo.java
+++ sneer/bricks/src/sneer/skin/sound/mic/tests/PlayerDemo.java
@@ -1,40 +1,25 @@
 package sneer.skin.sound.mic.tests;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-
 import sneer.kernel.container.Container;
 import sneer.kernel.container.ContainerUtils;
 import sneer.pulp.tuples.TupleSpace;
-import sneer.skin.sound.PcmSoundPacket;
+import sneer.skin.sound.mic.Mic;
 import sneer.skin.sound.speaker.Speaker;
 import wheel.io.Logger;
-
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
+import wheel.lang.Threads;
 
 public class PlayerDemo {
 
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
+	public static void main(String[] args) {
 		Logger.redirectTo(System.out);
 	
-		final Container container = ContainerUtils.newContainer();
-		final TupleSpace tuples = container.produce(TupleSpace.class);
-		final Speaker speaker = container.produce(Speaker.class);
-		speaker.open();
+		final Container container1 = ContainerUtils.newContainer();
+		final TupleSpace tuples = container1.produce(TupleSpace.class);
+		final Container container2 = ContainerUtils.newContainer(tuples);
+
+		container2.produce(Mic.class).open();
+		container1.produce(Speaker.class).open();
 		
-		final XStream stream = new XStream(new DomDriver());
-		final ObjectInputStream input = stream.createObjectInputStream(new FileReader("micdemo.xml"));
-		try {
-			while (true) {
-				final PcmSoundPacket packet = (PcmSoundPacket) input.readObject();
-				if (null == packet)
-					break;
-				tuples.publish(packet);
-			}
-		} finally {
-			input.close();
-		}
+		Threads.sleepWithoutInterruptions(10000);
 	}
 }

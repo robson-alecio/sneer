da1f20d24274ed953a19e61f03cd52abe8711cf1
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-20 17:25:17 +0000

    Images.isSameImage() accelerated
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2686 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/wheel/src/wheel/io/ui/graphics/Images.java
+++ sneer/wheel/src/wheel/io/ui/graphics/Images.java
@@ -3,8 +3,11 @@ package wheel.io.ui.graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
 import java.net.URL;
+import java.util.Arrays;
 
+import wheel.lang.exceptions.Hiccup;
 import wheel.testutil.Profiler;
 
 public class Images {
@@ -25,17 +28,24 @@ public class Images {
 	}
 
 	private static boolean doIsSameImage(BufferedImage image1, BufferedImage image2) {
-		if (image1.getWidth() != image2.getWidth()) return false;
-		if (image1.getHeight() != image2.getHeight()) return false;
+		if (image2.getWidth() != image1.getWidth()) return false;
+		if (image2.getHeight() != image1.getHeight()) return false;
 		
-		for (int x = 0; x < image1.getWidth(); x++)
-			for (int y = 0; y < image1.getHeight(); y++)
-				if (image1.getRGB(x, y) != image2.getRGB(x, y))
-					return false;
-
-		return true;
+		return Arrays.equals(pixels(image1), pixels(image2));
 	}
 	
+	/** @throws Hiccup */
+	private static int[] pixels(BufferedImage image) {
+		PixelGrabber result = new PixelGrabber(image, 0, 0, image.getWidth(), image.getHeight(), true);
+		try {
+			result.grabPixels();
+		} catch (InterruptedException e) {
+			throw new IllegalStateException();
+		}
+
+		return (int[])result.getPixels();
+	}
+
 	static public BufferedImage copy(BufferedImage original) {
 		return new BufferedImage(
 			original.getColorModel(),

01d5d869244813f6cc46525605126cf87e45d018
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-21 16:34:53 +0000

    deleting
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2714 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/dashboard/InstrumentState.java
+++
@@ -1,9 +0,0 @@
-package sneer.skin.dashboard;
-
-public enum InstrumentState {//Implement dashboard support for this states
-	iconified, 		//Just a Icon in the Dashboard (one icon)
-	minimized,		//A Icon and a Title in the Dashboard (one roll)
-	attached,		//A Normal Instrument Gui in the Dashboard (one pane)
-	detached,		//Detached Instrument Window (one window)
-	maximized;	//Showing the Additional Instrument Window (one * and additional window)
-}

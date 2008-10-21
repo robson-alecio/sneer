230c5077329f709a557dbeb91d39d6ac1f8e90f4
douglas <douglas@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-21 19:04:36 +0000

    Winds history using prevayler
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2716 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/wind/impl/WindImpl.java
+++ sneer/bricks/src/snapps/wind/impl/WindImpl.java
@@ -1,8 +1,16 @@
 package snapps.wind.impl;
 
+import java.io.IOException;
+
+import org.prevayler.Prevayler;
+import org.prevayler.PrevaylerFactory;
+import org.prevayler.foundation.serialization.XStreamSerializer;
+
 import snapps.wind.Shout;
 import snapps.wind.Wind;
+import snapps.wind.impl.bubble.Bubble;
 import sneer.kernel.container.Inject;
+import sneer.pulp.config.persistence.PersistenceConfig;
 import sneer.pulp.tuples.TupleSpace;
 import wheel.lang.Omnivore;
 import wheel.reactive.lists.ListRegister;
@@ -13,11 +21,32 @@ class WindImpl implements Wind, Omnivore<Shout> {
 
 	@Inject
 	static private TupleSpace _environment;
+	
+	@Inject
+	static private PersistenceConfig _persistence;
 
-
-	private ListRegister<Shout> _shoutsHeard = new ListRegisterImpl<Shout>();
+	private final ListRegister<Shout> _shoutsHeard;
 
 	{
+		Prevayler prevayler;
+		try {
+			PrevaylerFactory factory = new PrevaylerFactory();
+			factory.configureJournalSerializer(new XStreamSerializer());
+			factory.configurePrevalenceDirectory(_persistence.persistenceDirectory().getAbsolutePath() + "/wind");
+			factory.configurePrevalentSystem(new ListRegisterImpl<Shout>());
+			factory.configureTransactionFiltering(false);
+			prevayler = factory.create();
+
+		} catch (IOException e) {
+			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+		} catch (ClassNotFoundException e) {
+			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+		}
+		_shoutsHeard = Bubble.wrapStateMachine(prevayler);
+		
+		for (Shout shout: _shoutsHeard.output())
+			_environment.publish(shout);
+		
 		_environment.addSubscription(Shout.class, this);
 	}
 
@@ -28,7 +57,7 @@ class WindImpl implements Wind, Omnivore<Shout> {
 
 	@Override
 	public void consume(Shout shout) {
-		_shoutsHeard.add(shout);
+		_shoutsHeard.adder().consume(shout);
 	}
 
 	@Override
@@ -41,6 +70,5 @@ class WindImpl implements Wind, Omnivore<Shout> {
 	private void shout(String phrase) {
 		_environment.publish(new Shout(phrase));
 	}
-	
 
 }
---
+++ sneer/bricks/src/snapps/wind/impl/bubble/Bubble.java
@@ -0,0 +1,83 @@
+package snapps.wind.impl.bubble;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.prevayler.Prevayler;
+
+import wheel.lang.Consumer;
+import wheel.lang.Omnivore;
+
+public class Bubble {
+
+	public static <STATE_MACHINE> STATE_MACHINE wrapStateMachine(Prevayler prevayler) {
+		Object stateMachine = prevayler.prevalentSystem();
+		return (STATE_MACHINE)wrap(stateMachine, prevayler, new ArrayList<String>());  //Refactor Remove this cast and use Casts.uncheckedCast() instead, when the Sun compiler can handle it (bug fixed in JDK7). Remove the @SuppressWarnings for this method.
+	}
+
+	private static <T> T wrap(Object object, Prevayler prevayler, List<String> getterMethodPath) {
+		InvocationHandler handler = new Bubble(object, prevayler, getterMethodPath).handler();
+		Object proxy = Proxy.newProxyInstance(object.getClass().getClassLoader(), object.getClass().getInterfaces(), handler);
+		return (T)proxy;  //Refactor Remove this cast and use Casts.uncheckedCast() instead, when the Sun compiler can handle it (bug fixed in JDK7). Remove the @SuppressWarnings for this method.
+	}
+
+	private Bubble(Object stateMachine, Prevayler prevayler, List<String> getterMethodPath) {
+		_stateMachine = stateMachine;
+		_prevayler = prevayler;
+		_getterMethodPath = getterMethodPath;
+	}
+	
+	private final Object _stateMachine;
+	private final Prevayler _prevayler;
+	private final List<String> _getterMethodPath;
+	
+	
+	private InvocationHandler handler() {
+		return new InvocationHandler() {
+			@Override
+			public Object invoke(Object proxyImplied, Method method, Object[] args) throws Throwable {
+				Object result = invokeOnStateMachine(method, args);
+				return wrapIfNecessary(result, method);
+			}
+		};
+	}
+	
+	private Object invokeOnStateMachine(Method method, Object[] args) throws Throwable {
+		Object result;
+		try {
+			result = method.invoke(_stateMachine, args);
+		} catch (InvocationTargetException e) {
+			throw e.getCause();
+		} catch (IllegalArgumentException e) {
+			throw new IllegalStateException(e);
+		} catch (IllegalAccessException e) {
+			throw new IllegalStateException(e);
+		}
+		return result;
+	}
+
+	
+	private Object wrapIfNecessary(Object object, Method method) {
+		String methodName = method.getName();
+		if (methodName.equals("output")) return object;
+		
+		List<String> pathToObject = new ArrayList<String>(_getterMethodPath.size() + 1);
+		pathToObject.addAll(_getterMethodPath);
+		pathToObject.add(methodName);
+
+		Class<?> type = method.getReturnType();
+		if (Omnivore.class.isAssignableFrom(type))
+			return new OmnivoreBubble(_prevayler, pathToObject);
+		if (Consumer.class.isAssignableFrom(type))
+			return new ConsumerBubble(_prevayler, pathToObject);
+		
+		if (type == Void.class) throw new UnsupportedOperationException("Void methods such as " + method + " are not supported.");
+		
+		return wrap(object, _prevayler, pathToObject);
+	}
+
+}

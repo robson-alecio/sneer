e98b6e8271ba6e0cb7ab0e8b86c1bf05d902d18a
priscila <priscila@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-22 17:08:43 +0000

    Removing unused code
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2728 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/pulp/tuples/TupleSpace.java
+++ sneer/bricks/src/sneer/pulp/tuples/TupleSpace.java
@@ -1,10 +1,7 @@
 package sneer.pulp.tuples;
 
 import sneer.kernel.container.Brick;
-import sneer.pulp.keymanager.PublicKey;
-import wheel.lang.Functor;
 import wheel.lang.Omnivore;
-import wheel.lang.Predicate;
 
 public interface TupleSpace extends Brick {
 
@@ -12,11 +9,6 @@ public interface TupleSpace extends Brick {
 
 	<T extends Tuple> void addSubscription(Class<T> tupleType, Omnivore<T> subscriber);
 	<T extends Tuple> void removeSubscription(Class<T> tupleType, Omnivore<T> subscriber);
-
-	<T extends Tuple> Iterable<T> tuples(Class<T> tupleType);
-	<T extends Tuple> T mostRecentTuple(Class<T> tupleType, PublicKey publisher, Predicate<? super T> filter);
-	<T extends Tuple> Iterable<T> mostRecentTupleByGroup(Class<T> tupleType, PublicKey publisher, Functor<? super T, Object> grouping);
-	<T extends Tuple> Iterable<T> mostRecentTupleByGroup(Class<T> tupleType, PublicKey publisher,  Predicate<? super T> filter, Functor<? super T, Object> grouping);
 
 	long tupleCount();
 	Tuple tuple(long index);
--- sneer/bricks/src/sneer/pulp/tuples/impl/TupleSpaceImpl.java
+++ sneer/bricks/src/sneer/pulp/tuples/impl/TupleSpaceImpl.java
@@ -3,21 +3,14 @@ package sneer.pulp.tuples.impl;
 import static wheel.lang.Types.cast;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import sneer.pulp.keymanager.PublicKey;
 import sneer.pulp.tuples.Tuple;
 import sneer.pulp.tuples.TupleSpace;
-import wheel.lang.Functor;
-import wheel.lang.Nulls;
 import wheel.lang.Omnivore;
-import wheel.lang.Predicate;
 import wheel.lang.Types;
 
 public class TupleSpaceImpl implements TupleSpace {
@@ -89,50 +82,6 @@ public class TupleSpaceImpl implements TupleSpace {
 		}
 	}
 
-
-	@Override
-	public synchronized <T extends Tuple> Iterable<T> tuples(Class<T> tupleType) {
-		Collection<T> result = new ArrayList<T>();
-		
-		for (Tuple candidate : _tuples)
-			if (Types.instanceOf(candidate, tupleType)) {
-				T casted = (T)(candidate);
-				result.add(casted);
-			}
-		
-		return result;
-	}
-
-
-	@Override
-	public <T extends Tuple> T mostRecentTuple(Class<T> tupleType, PublicKey publisher, Predicate<? super T> filter) {
-		Iterable<T> qq = mostRecentTupleByGroup(tupleType, publisher, filter, Functor.SINGLETON_FUNCTOR);
-		return Nulls.firstOrNull(qq);
-	}
-
-
-	@Override
-	public <T extends Tuple> Iterable<T> mostRecentTupleByGroup(Class<T> tupleType, PublicKey publisher, Functor<? super T, Object> grouping) {
-		return mostRecentTupleByGroup(tupleType, publisher, Predicate.TRUE, grouping);
-	}
-
-	@Override
-	public <T extends Tuple> Iterable<T> mostRecentTupleByGroup(Class<T> tupleType, PublicKey publisher, Predicate<? super T> filter, Functor<? super T, Object> grouping) {
-		Map<Object, T> result = new HashMap<Object, T>();
-		
-		for (T candidate : tuples(tupleType)) {
-			if (!candidate.publisher.equals(publisher)) continue;
-			if (!filter.evaluate(candidate)) continue;
-
-			Object group = grouping.evaluate(candidate);
-			
-			T mostRecentSoFar = 	result.get(group);
-			if (mostRecentSoFar == null || candidate.publicationTime > mostRecentSoFar.publicationTime)
-				result.put(group, candidate);
-		}
-		
-		return result.values();
-	}
 
 	@Override
 	public synchronized Tuple tuple(long index) {

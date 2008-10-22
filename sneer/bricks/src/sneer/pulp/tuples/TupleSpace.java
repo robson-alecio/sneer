0c07d1ac4c5a0bcccdb8f3e43b7ff125b3669c66
priscila <priscila@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-22 17:06:36 +0000

    Minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2727 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/pulp/tuples/TupleSpace.java
+++ sneer/bricks/src/sneer/pulp/tuples/TupleSpace.java
@@ -11,19 +11,15 @@ public interface TupleSpace extends Brick {
 	void publish(Tuple tuple);
 
 	<T extends Tuple> void addSubscription(Class<T> tupleType, Omnivore<T> subscriber);
-	
 	<T extends Tuple> void removeSubscription(Class<T> tupleType, Omnivore<T> subscriber);
 
 	<T extends Tuple> Iterable<T> tuples(Class<T> tupleType);
-
 	<T extends Tuple> T mostRecentTuple(Class<T> tupleType, PublicKey publisher, Predicate<? super T> filter);
-
 	<T extends Tuple> Iterable<T> mostRecentTupleByGroup(Class<T> tupleType, PublicKey publisher, Functor<? super T, Object> grouping);
-
 	<T extends Tuple> Iterable<T> mostRecentTupleByGroup(Class<T> tupleType, PublicKey publisher,  Predicate<? super T> filter, Functor<? super T, Object> grouping);
 
+	long tupleCount();
 	Tuple tuple(long index);
 
-	long tupleCount();
 
 }

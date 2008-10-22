aa7e3835d19532c5e98a60e0ad6945e31dbc689e
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-22 21:08:51 +0000

    added some cellrenderer spikes
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2735 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/spikes/src/spikes/sandro/cellrenderer/ComplexRenderingSample.java
@@ -0,0 +1,105 @@
+package spikes.sandro.cellrenderer;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListCellRenderer;
+
+import wheel.io.Logger;
+import wheel.io.ui.graphics.Images;
+import wheel.testutil.MemorySentinel;
+
+public class ComplexRenderingSample {
+
+	static final Image _me = getImage();
+	
+	static Object elements[][] = {
+			{ new Font("Helvetica", Font.PLAIN, 20), Color.red, new ImageIcon(_me), "Help" },
+			{ new Font("TimesRoman", Font.BOLD, 14), Color.blue, new ImageIcon(_me), "Me" },
+			{ new Font("Courier", Font.ITALIC, 18), Color.green,	new ImageIcon(_me), "I'm" },
+			{ new Font("Helvetica", Font.BOLD | Font.ITALIC, 12), Color.gray,	new ImageIcon(_me), "Trapped" },
+			{ new Font("TimesRoman", Font.PLAIN, 32), Color.pink, new ImageIcon(_me), "Inside" },
+			{ new Font("Courier", Font.BOLD, 16), Color.yellow, new ImageIcon(_me), "This" },
+			{ new Font("Helvetica", Font.ITALIC, 8), Color.darkGray,	new ImageIcon(_me), "Computer" } };
+
+	private static Image getImage(){
+		return Images.getImage(ComplexRenderingSample.class.getResource("me.png"));
+	}	
+	
+	public static void main(String args[]) {
+		Logger.redirectTo(System.out);
+		MemorySentinel.startLoggingSignificantMemoryUsageChanges();
+		initGui();
+	}
+
+	private static void initGui() {
+		JFrame frame = new JFrame("Complex Renderer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container contentPane = frame.getContentPane();
+
+		final DefaultListModel model = new DefaultListModel();
+		JList jlist = new JList(model);
+		ListCellRenderer renderer = new ComplexCellRenderer();
+		jlist.setCellRenderer(renderer);
+		JScrollPane scrollPane = new JScrollPane(jlist);
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+
+		JButton btn = new JButton("Add...");
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				for (Object[] element : elements) {
+					model.addElement(element);
+				}
+			}
+		});
+
+		contentPane.add(btn, BorderLayout.SOUTH);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+}
+
+class ComplexCellRenderer implements ListCellRenderer {
+	protected DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+		Font theFont = null;
+		Color theForeground = null;
+		Icon theIcon = null;
+		String theText = null;
+
+		JLabel renderer = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index, isSelected,	cellHasFocus);
+
+		Object values[] = (Object[]) value;
+		theFont = (Font) values[0];
+		theForeground = (Color) values[1];
+		theIcon = (Icon) values[2];
+		theText = (String) values[3];
+		
+		if (!isSelected) {
+			renderer.setForeground(theForeground);
+		}
+		if (theIcon != null) {
+			renderer.setIcon(theIcon);
+		}
+		renderer.setText(theText);
+		renderer.setFont(theFont);
+		return renderer;
+	}
+}

d78a6db30e174f4cf275c58a0446933166538202
priscila <priscila@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-24 21:03:20 +0000

    Unsuccesful persistence attempt reverted
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2783 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/wind/impl/WindImpl.java
+++ sneer/bricks/src/snapps/wind/impl/WindImpl.java
@@ -1,16 +1,8 @@
 package snapps.wind.impl;
 
-import java.io.IOException;
-
-import org.prevayler.Prevayler;
-import org.prevayler.PrevaylerFactory;
-import org.prevayler.foundation.serialization.XStreamSerializer;
-
 import snapps.wind.Shout;
 import snapps.wind.Wind;
-import snapps.wind.impl.bubble.Bubble;
 import sneer.kernel.container.Inject;
-import sneer.pulp.config.persistence.PersistenceConfig;
 import sneer.pulp.tuples.TupleSpace;
 import wheel.lang.Omnivore;
 import wheel.reactive.lists.ListRegister;
@@ -22,31 +14,9 @@ class WindImpl implements Wind, Omnivore<Shout> {
 	@Inject
 	static private TupleSpace _environment;
 	
-	@Inject
-	static private PersistenceConfig _persistence;
-
-	private final ListRegister<Shout> _shoutsHeard;
+	private final ListRegister<Shout> _shoutsHeard = new ListRegisterImpl<Shout>();
 
 	{
-		Prevayler prevayler;
-		try {
-			PrevaylerFactory factory = new PrevaylerFactory();
-			factory.configureJournalSerializer(new XStreamSerializer());
-			factory.configurePrevalenceDirectory(_persistence.persistenceDirectory().getAbsolutePath() + "/wind");
-			factory.configurePrevalentSystem(new ListRegisterImpl<Shout>());
-			factory.configureTransactionFiltering(false);
-			prevayler = factory.create();
-
-		} catch (IOException e) {
-			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
-		} catch (ClassNotFoundException e) {
-			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
-		}
-		_shoutsHeard = Bubble.wrapStateMachine(prevayler);
-		
-		for (Shout shout: _shoutsHeard.output())
-			_environment.acquire(shout);
-		
 		_environment.addSubscription(Shout.class, this);
 	}
 
@@ -59,6 +29,7 @@ class WindImpl implements Wind, Omnivore<Shout> {
 	public void consume(Shout shout) {
 		_shoutsHeard.adder().consume(shout);
 	}
+
 
 	@Override
 	public Omnivore<String> megaphone() {

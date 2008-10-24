4065a923ae3da59fc21c239b6b13239d74051fc0
priscila <priscila@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-10-24 03:34:12 +0000

    Spiking CPU impact and precision of a thread that wakes up every 1ms and asks for nanoTime. Result: Precision very near to 1ms and 0,05% CPU impact per thread on a Core 2 Duo T5550.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2774 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/src/spikes/klaus/Anything.java
+++ sneer/spikes/src/spikes/klaus/Anything.java
@@ -1,21 +1,27 @@
 package spikes.klaus;
 
+import wheel.lang.Threads;
+
 
 
 
 
 public class Anything {
 
-	private static long _lastTime;
-
 	public static void main(String[] args) {
+
+		for (int i = 0; i <= 100; i++)
+			new Thread("Test " + i) { @Override public void run() {
+				testTime();
+			}}.start();
+
 		
-		while (true) {
-			long now = now();
-			if ((now - _lastTime) < (1000L * 1000 * 1000 * 5)) continue;
-			System.out.println("" + now + "  " + (now - _lastTime));
-			_lastTime = now;
-		}
+//		while (true) {
+//			long now = now();
+//			if ((now - _lastTime) < (1000L * 1000 * 1000 * 5)) continue;
+//			System.out.println("" + now + "  " + (now - _lastTime));
+//			_lastTime = now;
+//		}
 
 		
 //		ImageIO.write(image, "png", output);
@@ -28,6 +34,21 @@ public class Anything {
 //		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 //		DisplayMode mode = device.getDisplayMode();
 //		System.out.println("" + mode.getWidth() + " x " + mode.getHeight());
+	}
+
+	private static void testTime() {
+		long t0 = now();
+		int counter = 0;
+
+		while (true) {
+			Threads.sleepWithoutInterruptions(1);
+			long now = now();
+			
+			if (now - t0 > 10L * 1000 * 1000 * 1000) break;
+			counter++;
+		}
+		
+		System.out.println(counter / 10);
 	}
 	
 	static private long now() {

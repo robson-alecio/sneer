7a0efbeb0acc2c3c825858fab3f40df06d976e59
rodrigob <rodrigob@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-05 16:55:04 +0000

    Unnecessary @SuppressWarnings
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2916 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/wheel/src/wheel/reactive/impl/AbstractOwnerReference.java
+++ sneer/wheel/src/wheel/reactive/impl/AbstractOwnerReference.java
@@ -3,7 +3,6 @@ package wheel.reactive.impl;
 /** Avoid garbage collection of another object while the wrapped signal has references. Useful for implementing gates. */
 public class AbstractOwnerReference<T> {
 
-	@SuppressWarnings("unused")
 	protected final Object _referenceToAvoidGC;
 
 	public AbstractOwnerReference(Object objectToSaveFromGC) {

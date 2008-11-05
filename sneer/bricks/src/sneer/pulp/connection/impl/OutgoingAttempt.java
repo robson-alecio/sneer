647c3e4384fd41e16aaa8fecb9b34f429c4782ea
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-05 20:19:00 +0000

    Refactored and logs failed outgoing attempts
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2924 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/pulp/connection/impl/OutgoingAttempt.java
+++ sneer/bricks/src/sneer/pulp/connection/impl/OutgoingAttempt.java
@@ -9,54 +9,54 @@ import sneer.pulp.internetaddresskeeper.InternetAddress;
 import sneer.pulp.network.ByteArraySocket;
 import sneer.pulp.network.Network;
 import sneer.pulp.threadpool.ThreadPool;
+import wheel.io.Logger;
 
 class OutgoingAttempt implements Runnable {
 
-	@Inject
-	static private Network _network;
+	@Inject	static private Network _network;
+	@Inject	static private ConnectionManager _connectionManager;
+	@Inject	static private ThreadPool _threadPool; { _threadPool.registerActor(this); }
+	@Inject	static private Clock _clock;
 
-	@Inject
-	static private ConnectionManager _connectionManager;
-
-	@Inject
-	static private ThreadPool _threadPool;
 	
-	@Inject
-	static private Clock _clock;
-
 	private final InternetAddress _address;
-
 	private boolean _isRunning = true;
 
+	
 	OutgoingAttempt(InternetAddress address) {
 		_address = address;
-//		_clock.wakeUpNowAndEvery(20 * 1000, this);
-		_threadPool.registerActor(this);
 	}
 
+	
+	public synchronized void crash() {
+		_isRunning = false;
+	}
+
+	
 	public void run() {
 		while (isRunning()) {
 			tryToOpen();
 			_clock.sleepAtLeast(20 * 1000);
+
 		}
 	}
 
+	
 	private synchronized boolean isRunning() {
 		return _isRunning;
 	}
 
+	
 	private void tryToOpen() {
 		ByteArraySocket socket;
 		try {
 			socket = _network.openSocket(_address.host(), _address.port());
 		} catch (IOException e) {
+			Logger.logShort(e, "Unable to open socket to: ", _address);
 			return;
 		}
 		
 		_connectionManager.manageOutgoingSocket(_address.contact(), socket);
 	}
 	
-	public synchronized void crash() {
-		_isRunning = false;
-	}
 }

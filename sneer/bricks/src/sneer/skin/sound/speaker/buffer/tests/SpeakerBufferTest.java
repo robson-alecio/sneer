c73e68adaf3669af2e904cad7230ae75f0c10f73
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-11 23:58:51 +0000

    SpeakerBufferTest expecting passive buffer, not active with thread.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2969 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/speaker/buffer/tests/SpeakerBufferTest.java
+++ sneer/bricks/src/sneer/skin/sound/speaker/buffer/tests/SpeakerBufferTest.java
@@ -27,15 +27,13 @@ import wheel.lang.Omnivore;
 @RunWith(JMock.class)
 public class SpeakerBufferTest extends TestThatIsInjected {
 	
-	private static final int FIRT_GROUP_TRIGGER = 51;
-
 	@Inject private static SpeakerBuffers _subject;
 	
 	@Inject private static Clock _clock;
 	
 	@Inject private static KeyManager _keyManager;
 
-	private final List<Integer> _sequenceRecorder = new ArrayList<Integer>();
+	private final List<Integer> _recordedSequence = new ArrayList<Integer>();
 
 	private final ThreadPoolMock _threads = new ThreadPoolMock();
 
@@ -66,8 +64,8 @@ public class SpeakerBufferTest extends TestThatIsInjected {
 		}});
 		
 		SpeakerBuffer buffer = _subject.createBufferFor(_consumer);
-		PcmSoundPacket toOldPacket = p0();
-		buffer.consume(toOldPacket);
+		PcmSoundPacket tooOldPacket = p0();
+		buffer.consume(tooOldPacket);
 		buffer.consume(first);
 		buffer.consume(second);
 		
@@ -86,53 +84,33 @@ public class SpeakerBufferTest extends TestThatIsInjected {
 			51, // More than 30 gap since last played (11): will cause 20, 21 to be played.
 			600, 601, 602, // More than 500 gap: will cause buffer to drain (22, 51 will not be played)
 			604, //Gap
-			-700, -701 // More than 500 gap in the other direction will cause buffer to drain (604 will not be played)
+			-700, -699 // More than 500 gap in the other direction will also cause buffer to drain (604 will not be played)
 		};
 
+		feedInputSequence(input);
+		expectOutputSequence(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 21, 600, 601, 602, -700, -699);
+	}
+
+	private void feedInputSequence(int[] input) {
 		SpeakerBuffer buffer = _subject.createBufferFor(sequenceRecorder());
 		
-		expectOutputSequence( input, buffer, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20, 21, 600, 601, 602);// , -700, -701);
+		for (int sequence : input)
+			buffer.consume(packet(sequence));
 	}
 	
 	private Omnivore<PcmSoundPacket> sequenceRecorder() {
 		return new Omnivore<PcmSoundPacket>(){ @Override public void consume(PcmSoundPacket value) {
-			_sequenceRecorder.add(value.sequence);
+			_recordedSequence.add(value.sequence);
 		}};
 	}
 	
-	private void expectOutputSequence(int input[], SpeakerBuffer buffer, long... sequences) {
-		
-		firstFeedInput(buffer, input);
-		_threads.stepper(0).step();		// play [0-21]
-		
-		consumeSequence(buffer, 600);  
-		consumeSequence(buffer, 601);
-		consumeSequence(buffer, 602); 
-		_threads.stepper(0).step();   // drain 22, 51
-		
-		consumeSequence(buffer, 604);
-		_threads.stepper(0).step();   // gap
-		
-		consumeSequence(buffer, -700);
-		consumeSequence(buffer, -701);
-		_threads.stepper(0).step(); // drain 604, sort and play others
-		
-		for (int i = 0; i < sequences.length; i++) {
-			assertEquals(sequences[i], _sequenceRecorder.get(i).longValue()); 
-		}
+	private void expectOutputSequence(int... sequences) {
+		for (int i = 0; i < sequences.length; i++)
+			assertEquals(_recordedSequence.get(i), (Object)sequences[i]); 
 	}
 	
-	private void firstFeedInput(SpeakerBuffer buffer, int[] input) {
-		for (int i = 0; i < input.length; i++) {
-			short sequence = (short)input[i];
-			consumeSequence(buffer, sequence);
-			if(sequence == FIRT_GROUP_TRIGGER)
-				return;
-		}
-	}
-	
-	private void consumeSequence(SpeakerBuffer buffer, int sequence) {
-		buffer.consume(contactPacket(new byte[] { (byte) 7, 11, 13, 17 }, (short)sequence));
+	private PcmSoundPacket packet(int sequence) {
+		return contactPacket(new byte[] { (byte) 7, 11, 13, 17 }, (short)sequence);
 	}
 
 	@SuppressWarnings("deprecation")

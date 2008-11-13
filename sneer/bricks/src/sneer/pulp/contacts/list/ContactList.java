e26271960a1a558951ca563330f6f468fd85154a
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-13 23:13:06 +0000

    Refactor comment added to code: Eliminate ContactList
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@2982 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/pulp/contacts/list/ContactList.java
+++ sneer/bricks/src/sneer/pulp/contacts/list/ContactList.java
@@ -4,6 +4,7 @@ import sneer.kernel.container.Brick;
 import sneer.pulp.contacts.Contact;
 import wheel.reactive.lists.ListRegister;
 
+//Refactor Eliminate this intermediate list. Use ContactManager.contacts() directly. ContactInfo is a non-tuple class, which is not allowed in the brick package (only interfaces and tuples are allowed). This list is serving only as a shortcut for ConnectionManager.connectionFor(contact).isOnline() - it is not worth it. 
 public interface ContactList extends ListRegister<ContactInfo>, Brick{
 
 	ContactInfo contactInfo(Contact contact);

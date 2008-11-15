228df2954de71d5a9761c7a65c9d40d48043fcde
douglas <douglas@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-15 19:46:20 +0000

    Now classes in sneer.skin.sound.kernel.impl package are local instead of public.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3007 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/kernel/impl/AudioImpl.java
+++ sneer/bricks/src/sneer/skin/sound/kernel/impl/AudioImpl.java
@@ -1,11 +1,12 @@
 package sneer.skin.sound.kernel.impl;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
 
 import sneer.skin.sound.kernel.Audio;
 
-public class AudioImpl implements Audio {
+class AudioImpl implements Audio {
 
 	@Override
 	public SourceDataLine bestAvailableSourceDataLine() {
@@ -15,6 +16,41 @@ public class AudioImpl implements Audio {
 	@Override
 	public TargetDataLine bestAvailableTargetDataLine() {
 		return AudioCommon.bestAvailableTargetDataLine();
+	}
+
+	@Override
+	public AudioFormat audioFormat() {
+		return AudioUtil.AUDIO_FORMAT;
+	}
+
+	@Override
+	public int framesPerAudioPacket() {
+		return AudioUtil.FRAMES_PER_AUDIO_PACKET;
+	}
+
+	@Override
+	public int narrowbandEncoding() {
+		return AudioUtil.NARROWBAND_ENCODING; 
+	}
+
+	@Override
+	public int sampleRate() {
+		return AudioUtil.SAMPLE_RATE;
+	}
+
+	@Override
+	public int sampleSizeInBits() {
+		return AudioUtil.SAMPLE_SIZE_IN_BITS;
+	}
+
+	@Override
+	public boolean signed() {
+		return AudioUtil.SIGNED;
+	}
+
+	@Override
+	public int soundQuality() {
+		return AudioUtil.SOUND_QUALITY;
 	}
 
 }

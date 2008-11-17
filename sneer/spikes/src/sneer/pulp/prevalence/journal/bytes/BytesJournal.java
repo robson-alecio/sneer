b7ccce97ef4dbdbae2cc0da7c385123d8f6db942
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-17 18:03:54 +0000

    Minor refactoring to eliminate JournalFactory
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3021 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/src/sneer/pulp/prevalence/journal/bytes/BytesJournal.java
+++ sneer/spikes/src/sneer/pulp/prevalence/journal/bytes/BytesJournal.java
@@ -8,6 +8,6 @@ public interface BytesJournal {
 	 * @param previousEntryConsumer to which all existing entries in the journal will be passed before this method returns.
 	 * @return a consumer for appending entries to the journal.
 	 */
-	Consumer<byte[]> open(Consumer<byte[]> previousEntryConsumer);
+	Consumer<byte[]> open(String directory, Consumer<byte[]> previousEntryConsumer);
 	
 }
--- sneer/spikes/src/sneer/pulp/prevalence/journal/bytes/BytesJournalFactory.java
+++
@@ -1,9 +0,0 @@
-package sneer.pulp.prevalence.journal.bytes;
-
-import wheel.io.files.Directory;
-
-public interface BytesJournalFactory {
-
-	BytesJournal createJournal(Directory directory);
-	
-}

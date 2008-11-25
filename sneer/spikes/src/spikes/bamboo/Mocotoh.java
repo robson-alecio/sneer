f16d860c5b430d579b5bf7c1e3514e086a12cf3a
rodrigob <rodrigob@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-11-25 22:45:36 +0000

    mocotó spike
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3076 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/spikes/src/spikes/bamboo/Mocotoh.java
@@ -0,0 +1,42 @@
+package spikes.bamboo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.junit.internal.runners.InitializationError;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.internal.runners.TestMethod;
+
+import sneer.kernel.container.ContainerUtils;
+
+public class Mocotoh extends JUnit4ClassRunner {
+	
+	public Mocotoh(Class<?> testClass) throws InitializationError {
+		super(testClass);
+	}
+	
+	@Override
+	protected TestMethod wrapMethod(Method method) {
+		return new TestMethod(method, this.getTestClass()) {
+			@Override
+			public void invoke(final Object test) {
+				Environment.with(ContainerUtils.newContainer(), new Runnable() { @Override public void run() {
+					try {
+						superInvoke(test);
+					} catch (IllegalArgumentException e) {
+						throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+					} catch (IllegalAccessException e) {
+						throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+					} catch (InvocationTargetException e) {
+						throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+					}
+				}});
+			}
+			
+			private void superInvoke(Object test) throws IllegalArgumentException,
+			IllegalAccessException, InvocationTargetException {
+				super.invoke(test);
+			}
+		};
+	}
+}
---
+++ sneer/spikes/src/spikes/bamboo/MocotohTest.java
@@ -0,0 +1,17 @@
+package spikes.bamboo;
+
+import static spikes.bamboo.Environment.my;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import sneer.pulp.clock.Clock;
+
+@RunWith(Mocotoh.class)
+public class MocotohTest extends Assert {
+	@Test
+	public void test() {
+		assertEquals(0, my(Clock.class).time());
+	}
+}

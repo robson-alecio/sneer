0720c365e1a9840e52a4aff38f277b294decd2d7
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-12-02 19:11:36 +0000

    Wind sound player fixed.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3127 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/player/impl/SoundPlayerImpl.java
+++ sneer/bricks/src/sneer/skin/sound/player/impl/SoundPlayerImpl.java
@@ -3,9 +3,8 @@ package sneer.skin.sound.player.impl;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.List;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -18,61 +17,70 @@ import sneer.pulp.threadpool.Stepper;
 import sneer.pulp.threadpool.ThreadPool;
 import sneer.skin.sound.kernel.Audio;
 import sneer.skin.sound.player.SoundPlayer;
+import wheel.io.Logger;
+import wheel.io.Streams;
 import wheel.lang.Threads;
 import wheel.lang.exceptions.NotImplementedYet;
 
 class SoundPlayerImpl implements SoundPlayer, Stepper {
 
 	@Inject static private Audio _audio;
-	
-	@Inject
-	static private ThreadPool _threads; {
+	@Inject static private ThreadPool _threads; {
 		_threads.registerStepper(this);
 	}
 	
-	final static private Collection<URL> urls = Collections.synchronizedCollection(new ArrayList<URL>());
+	final static private List<URL> urls = Collections.synchronizedList(new ArrayList<URL>());
 	
-	public void playNext() {
-		if (1 == 1) throw new NotImplementedYet();
-		URL url = nextUrlToPlay();
-		AudioInputStream audioInputStream = tryInitAudioInputStream(url);
-		AudioFormat audioFormat = audioInputStream.getFormat();
-
-		int bufferSize = (int) audioFormat.getSampleRate() * audioFormat.getFrameSize();
-		byte[] buffer = new byte[bufferSize];
-
-		SourceDataLine dataLine = null;
+	private void playNext() {
+		URL url = urls.remove(0);
+		
+		AudioInputStream audioInputStream = null;
 		try {
-			dataLine = _audio.tryToOpenPlaybackLine(audioFormat);
-			int bytesRead = 0;
-			while (bytesRead >= 0) {
-				bytesRead = audioInputStream.read(buffer, 0, buffer.length);
-				if (bytesRead >= 0) {
-					dataLine.write(buffer, 0, bytesRead);
-				}
-			} 
+			audioInputStream = tryInitAudioInputStream(url);
+			AudioFormat audioFormat = audioInputStream.getFormat();
+	
+			int bufferSize = (int) audioFormat.getSampleRate() * audioFormat.getFrameSize();
+			byte[] buffer = new byte[bufferSize];
+	
+			SourceDataLine dataLine = null;
+			try {
+				dataLine = _audio.tryToOpenPlaybackLine(audioFormat);
+				playStream(dataLine, audioInputStream, buffer); 
+			} finally {
+				if (dataLine == null) return;
+				dataLine.close();
+			}
 		} catch (IOException e) {
-			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Use BL
 		} finally {
-			if (dataLine == null) return;
-			dataLine.close();
+			if (audioInputStream != null) Streams.crash(audioInputStream);
+		} 
+	}
+
+	private void playStream(SourceDataLine dataLine, AudioInputStream audioInputStream, byte[] buffer) {
+		int bytesRead = 0;
+		while (true) {
+			bytesRead = read(audioInputStream, buffer);
+			if (bytesRead == -1) return;
+			
+			dataLine.write(buffer, 0, bytesRead);
 		}
 	}
 
-	private URL nextUrlToPlay() {
-		Iterator<URL> iterator = urls.iterator();
-		URL url = iterator.next();
-		iterator.remove();
-		return url;
+	private int read(AudioInputStream audioInputStream, byte[] buffer) {
+		try {
+			return audioInputStream.read(buffer, 0, buffer.length);
+		} catch (IOException e) {
+			Logger.log(e); //Implement Make this a blinking light.
+			return -1;
+		}
 	}
 
-	private AudioInputStream tryInitAudioInputStream(URL url) {
+	private AudioInputStream tryInitAudioInputStream(URL url) throws IOException {
 		try {
 			return AudioSystem.getAudioInputStream(url);
 		} catch (UnsupportedAudioFileException e) {
-			throw new IllegalArgumentException("Unsupported Audio: " + url.toString()); //Implement
-		} catch (IOException e) {
-			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
+			throw new NotImplementedYet(); //Implement BL
 		}
 	} 
 

048b0952742a207f137d44469e205cd0c9ce9529
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-12-04 04:16:18 +0000

    Audio working with mono instead of stereo
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3139 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/kernel/impl/AudioImpl.java
+++ sneer/bricks/src/sneer/skin/sound/kernel/impl/AudioImpl.java
@@ -16,7 +16,7 @@ class AudioImpl implements Audio {
 
 	static private final int SAMPLE_RATE = 8000;
 	static private final int SAMPLE_SIZE_IN_BITS = 16;
-	static private final int CHANNELS = 2; //Fix: Back to 1 and run tests.  //If this doesnt work on some linux machines, try 2 channels (stereo).
+	static private final int CHANNELS = 1; //If this doesnt work on some linux machines, try 2 channels (stereo).
 	static private final boolean SIGNED = true;
 	static private final boolean BIG_ENDIAN = false;
 

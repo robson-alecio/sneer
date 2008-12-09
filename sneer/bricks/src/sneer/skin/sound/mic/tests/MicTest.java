06494400b5950d485275b7c0a9b66478e1a81b7a
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-12-09 19:10:56 +0000

    MicTest adapted for "my()" bug in JMockContainerEnvironment
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3189 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/mic/tests/MicTest.java
+++ sneer/bricks/src/sneer/skin/sound/mic/tests/MicTest.java
@@ -11,35 +11,56 @@ import org.jmock.integration.junit4.JUnit4Mockery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import sneer.pulp.tuples.TupleSpace;
+import sneer.skin.sound.PcmSoundPacket;
 import sneer.skin.sound.kernel.Audio;
 import sneer.skin.sound.mic.Mic;
 import tests.Contribute;
 import tests.JMockContainerEnvironment;
+import tests.TestThatIsInjected;
+import wheel.lang.Consumer;
+import wheel.lang.Threads;
 import wheel.testutil.SignalUtils;
 
-public class MicTest extends MicTestBase {
+@RunWith(JMockContainerEnvironment.class)
+public class MicTest extends TestThatIsInjected {
 
-	private static Mic _subject = my(Mic.class);
+	protected final Mockery _mockery = new JUnit4Mockery();
 
-	private final Mockery _mockery = new JUnit4Mockery();
-	@Contribute private final Audio _audio = _mockery.mock(Audio.class);
-	private final TargetDataLine _line = _mockery.mock(TargetDataLine.class);
-	private final AudioFormat _format = new AudioFormat(8000, 16, 1, true, false);
+	protected final TargetDataLine _line = _mockery.mock(TargetDataLine.class);
+	protected final AudioFormat _format = new AudioFormat(8000, 16, 1, true, false);
+
+	@Contribute	protected final Audio _audio = _mockery.mock(Audio.class);
+
 	
-	@Test
-	public void testIsRunningSignal() {
+	@Test(timeout = 3000)
+	public void testChannels() {
+		Mic subject = my(Mic.class);
+		
 		_mockery.checking(soundExpectations());
-
-		SignalUtils.waitForValue(false, _subject.isRunning());
-		
-		_subject.open();
-		SignalUtils.waitForValue(true, _subject.isRunning());
-		
-		_subject.close();
-		SignalUtils.waitForValue(false, _subject.isRunning());
+	
+		int defaultChannel = 0;
+	
+		subject.open();
+		waitForPcmPacketChannel(defaultChannel);
+	
+		subject.setChannel(42);
+		waitForPcmPacketChannel(42);
 	}
 
+	private void waitForPcmPacketChannel(final int channel) {
+		final Object notifier = new Object();
 	
+		synchronized (notifier) { //Refactor Try to extract this "wait for notify" logic.
+			my(TupleSpace.class).addSubscription(PcmSoundPacket.class, new Consumer<PcmSoundPacket>() { @Override public void consume(PcmSoundPacket packet) {
+				if (packet.channel != channel) return;
+				synchronized (notifier) { notifier.notify(); }
+			}});
+			
+			Threads.waitWithoutInterruptions(notifier);
+		}
+	}
+
 	private Expectations soundExpectations() {
 		return new Expectations() {{
 			//Sequence main = _mockery.sequence("main");
@@ -48,6 +69,21 @@ public class MicTest extends MicTestBase {
 			allowing(_line).read(with(aNonNull(byte[].class)), with(0), with(320)); will(returnValue(320));
 			allowing(_line).close();// inSequence(main);
 		}};
+	}
+
+	@Test
+	public void testIsRunningSignal() {
+		Mic subject = my(Mic.class);
+		
+		_mockery.checking(soundExpectations());
+	
+		SignalUtils.waitForValue(false, subject.isRunning());
+		
+		subject.open();
+		SignalUtils.waitForValue(true, subject.isRunning());
+		
+		subject.close();
+		SignalUtils.waitForValue(false, subject.isRunning());
 	}
 
 }

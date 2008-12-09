546f20596e4886089eafef83fae3fd8dc496b4b5
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-12-09 16:32:33 +0000

    Minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3179 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/wheel/src/wheel/testutil/TestUtils.java
+++ sneer/wheel/src/wheel/testutil/TestUtils.java
@@ -2,20 +2,19 @@ package wheel.testutil;
 
 import junit.framework.Assert;
 
-public class TestUtils {
+public class TestUtils extends Assert {
 
 	public static void assertFloat(float expected, float actual) {
 		Assert.assertEquals(expected, actual, 0.001f);
 	}
 
 	public static <T> void assertSameContents(Iterable<T> actual, T... expected) {
-		
-		int i=0;
+		int i = 0;
 		for (T actualItem : actual) {
-			Assert.assertEquals("different values at index " + i, actualItem, expected[i]);
-			++i;
+			assertEquals("different values at index " + i, actualItem, expected[i]);
+			i++;
 		}
-		Assert.assertEquals(expected.length, i);
+		assertEquals(expected.length, i);
 	}
 
 }

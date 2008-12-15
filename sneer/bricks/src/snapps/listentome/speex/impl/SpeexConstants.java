0567451017250430af3bbb8d625abc94ed44f36e
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-12-15 21:40:29 +0000

    Speex sound quality increased to 3. Lets see
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3236 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/listentome/speex/impl/SpeexConstants.java
+++ sneer/bricks/src/snapps/listentome/speex/impl/SpeexConstants.java
@@ -2,7 +2,7 @@ package snapps.listentome.speex.impl;
 
 class SpeexConstants {
 
-	static final int SOUND_QUALITY = 2; //From 0 (bad) to 10 (good);
+	static final int SOUND_QUALITY = 3; //From 0 (bad) to 10 (good); 2 was causing Sandro's sound to burst, like loud sound on a weak speaker.
 
 	static final int NARROWBAND_ENCODING = 0;
 

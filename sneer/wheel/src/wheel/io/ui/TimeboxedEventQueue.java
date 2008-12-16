5b4d3c3eec010ec932bcda7f120b399767e6f02c
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2008-12-16 14:31:18 +0000

    If EventQueues are ever discarded (popped), take care because pending events will be automatically passed on to the previous queue and if that is the AWT queue it will start dispatching events immediately. This has to be avoided.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3241 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/wheel/src/wheel/io/ui/TimeboxedEventQueue.java
+++ sneer/wheel/src/wheel/io/ui/TimeboxedEventQueue.java
@@ -72,7 +72,7 @@ public class TimeboxedEventQueue extends EventQueue {
 			super.threadBlockedNotification(thread);
 			Logger.log("Starting new Gui Thread");
 
-			startNewQueue(); //This is an EventQueue leak. Not serious compared to the thread leak of the blocked threads that make these new EventQueues necessary.
+			startNewQueue(); //This is an EventQueue leak. Not serious compared to the thread leak of the blocked threads that make these new EventQueues necessary. If these EventQueues are ever discarded (popped), take care because pending events will be automatically passed on to the previous queue and if that is the AWT queue it will start dispatching events immediately. This has to be avoided.
 		}
 		
 	}

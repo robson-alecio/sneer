090b6b4c6fd99342d9bc45c7fe6c1291e95ce1fc
priscila <priscila@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-01-06 05:17:33 +0000

    Add Comment
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3321 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/spikes/src/spikes/priscila/go/gui/GoScorePanel.java
+++ sneer/spikes/src/spikes/priscila/go/gui/GoScorePanel.java
@@ -8,7 +8,6 @@ import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 
 import sneer.kernel.container.Container;
-import sneer.kernel.container.ContainerUtils;
 import sneer.skin.widgets.reactive.ReactiveWidgetFactory;
 import sneer.skin.widgets.reactive.TextWidget;
 import wheel.lang.Functor;
@@ -18,21 +17,21 @@ import wheel.reactive.impl.Adapter;
 public class GoScorePanel extends JPanel {
 
 	public GoScorePanel(Signal<Integer> countCapturedBlack, Signal<Integer> countCapturedWhite) {
-		
-		Container container = ContainerUtils.getContainer();
-		ReactiveWidgetFactory rfactory = container.produce(ReactiveWidgetFactory.class);
+		//Fix Replace getContainer() from ContainerUtils
+		//Container container = ContainerUtils.getContainer(); 
+		//ReactiveWidgetFactory rfactory = container.produce(ReactiveWidgetFactory.class);
 
-		TextWidget<?> newLabelBlack = rfactory.newLabel(adaptToString(countCapturedBlack));
-		TextWidget<?> newLabelWhite = rfactory.newLabel(adaptToString(countCapturedWhite));
+		//TextWidget<?> newLabelBlack = rfactory.newLabel(adaptToString(countCapturedBlack));
+		//TextWidget<?> newLabelWhite = rfactory.newLabel(adaptToString(countCapturedWhite));
 		
 		JSeparator space= new JSeparator(SwingConstants.VERTICAL);
 		space.setPreferredSize(new Dimension(8,0));
 		add(new JLabel("Points:"));
 		add(space);
 		add(new JLabel("Black"));
-		add(newLabelWhite.getComponent());
+		//add(newLabelWhite.getComponent());
 		add(new JLabel("White"));
-		add(newLabelBlack.getComponent());
+		//add(newLabelBlack.getComponent());
 
 		setVisible(true);
 	}

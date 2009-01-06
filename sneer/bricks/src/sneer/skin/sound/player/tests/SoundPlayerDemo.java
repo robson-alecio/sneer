b4b9447b42df7ff9c60c29f6a4baa4d1a644fd15
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-01-06 19:55:50 +0000

    minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3329 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/skin/sound/player/tests/SoundPlayerDemo.java
+++ sneer/bricks/src/sneer/skin/sound/player/tests/SoundPlayerDemo.java
@@ -5,22 +5,17 @@ import sneer.kernel.container.Containers;
 import sneer.skin.sound.player.SoundPlayer;
 import wheel.io.Logger;
 import wheel.lang.Environments;
-import wheel.lang.Threads;
 
 public class SoundPlayerDemo {
 	
 	private final SoundPlayer _player = my(SoundPlayer.class);
 	
 	SoundPlayerDemo(){
-		System.out.println("1");
 		_player.play(SoundPlayerDemo.class.getResource("alert1.wav"));
-		System.out.println("2");
 		_player.play(SoundPlayerDemo.class.getResource("alert2.wav"));
-		System.out.println("3");
 		
 		try {
 			Thread.sleep(5000);
-			System.out.println("4");
 		} catch (InterruptedException e) {
 			throw new wheel.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
 		}

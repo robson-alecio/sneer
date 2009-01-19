3f2d49d9751ecb21368881f9389a98ccf3aeae79
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-01-19 22:03:50 +0000

    Naming standard applied
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3342 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/wind/Shout.java
+++ sneer/bricks/src/snapps/wind/Shout.java
@@ -6,8 +6,8 @@ public class Shout extends Tuple {
 
 	public final String phrase;
 
-	public Shout(String pPhrase) {
-		phrase = pPhrase;
+	public Shout(String phrase_) {
+		phrase = phrase_;
 	}
 
 	@Override

df35e145ed5c11f3f7bb7aa8f358b366c3857135
douglas <douglas@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-02-08 14:59:08 +0000

    Roll back to previous revision.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3371 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/sneer/pulp/logging/Logger.java
+++ sneer/bricks/src/sneer/pulp/logging/Logger.java
@@ -4,14 +4,12 @@ import sneer.kernel.container.Brick;
 import wheel.io.logging.WheelLogger;
 import wheel.lang.Consumer;
 import wheel.reactive.EventSource;
-import wheel.reactive.lists.ListSignal;
+import wheel.reactive.lists.ListRegister;
 
 public interface Logger extends WheelLogger, Brick {
 
-	Consumer<String> whiteListEntry();	
-	void whiteListEntryAdder(String phrase);
-	void whiteListEntryRemover(String phrase);
-	ListSignal<String> phrases();
+	Consumer<String> whiteListEntry();
+	public ListRegister<LogWhiteListEntry> whiteListEntries();
 	EventSource<String> loggedMessages();
 
 }

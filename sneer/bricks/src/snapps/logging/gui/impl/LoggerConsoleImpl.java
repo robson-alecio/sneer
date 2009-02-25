e41385adecf00412e68c89c28bf6078d2244c0bd
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-02-25 20:10:11 +0000

    Brick needs impl or Sneer won't compile it.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3403 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/bricks/src/snapps/logging/gui/impl/LoggerConsoleImpl.java
@@ -0,0 +1,12 @@
+package snapps.logging.gui.impl;
+
+import snapps.logging.gui.LoggerConsole;
+
+class LoggerConsoleImpl implements LoggerConsole {
+
+	@Override
+	public void show() {
+		throw new wheel.lang.exceptions.NotImplementedYet(); // Implement
+	}
+
+}

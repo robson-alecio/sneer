6f9d8571b388d8003669116cc9fcd338b0d0a522
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-03-06 09:50:13 +0000

    Minor refactoring
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3462 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/brickness/src/sneer/brickness/environment/Brickness.java
+++ sneer/brickness/src/sneer/brickness/environment/Brickness.java
@@ -1,6 +1,7 @@
 package sneer.brickness.environment;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 import sneer.commons.environments.Environment;
 
@@ -9,30 +10,28 @@ public class Brickness implements Environment {
 
 	@Override
 	public <T> T provide(Class<T> intrface) {
-		if (!intrface.isInterface())
-			throw new IllegalArgumentException();
-		
-		Class<T> implClass;
+		if (!intrface.isInterface()) throw new IllegalArgumentException();
+
 		try {
-			implClass = (Class<T>) Class.forName(implementationNameFor(intrface));
+			return instantiate(impl(intrface));
 		} catch (ClassNotFoundException e) {
 			return null;
-		}
-		try {
-			return instantiate(implClass);
 		} catch (Exception e) {
 			throw new sneer.commons.lang.exceptions.NotImplementedYet(e); // Fix Handle this exception.
 		}
 	}
 
-	private <T> T instantiate(Class<T> implClass)
-			throws Exception {
-		final Constructor<T> ctor = implClass.getDeclaredConstructor();
-		ctor.setAccessible(true);
-		return ctor.newInstance();
+	private <T> Class<T> impl(Class<T> intrface) throws ClassNotFoundException {
+		return (Class<T>)Class.forName(implNameFor(intrface)); 
 	}
 
-	private <T> String implementationNameFor(Class<T> intrface) {
+	private <T> T instantiate(Class<T> impl) throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		Constructor<T> ctor = impl.getDeclaredConstructor();
+		ctor.setAccessible(true);
+		return ctor.newInstance(); 
+	}
+
+	private <T> String implNameFor(Class<T> intrface) {
 		return BrickConventions.implementationNameFor(intrface.getName());
 	}
 }

4355f48856067cea36f178637a4344f401cc3f5a
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-03-06 06:28:51 +0000

    Method that was already declared in super deleted.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3456 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/kernel/api/src/sneer/kernel/container/Container.java
+++ sneer/kernel/api/src/sneer/kernel/container/Container.java
@@ -5,8 +5,6 @@ import sneer.brickness.environments.Environment;
 
 public interface Container extends Environment {
 	
-	<T> T provide(Class<T> intrface);
-
 	Class<? extends Brick> resolve(String brickName) throws ClassNotFoundException;
 
 }

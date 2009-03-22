de3c433c47c127d226c5cbfb8911077db366d0c0
klaus <klaus@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-03-22 07:00:42 +0000

    Cool refactoring in EventReceiver.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3492 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/wheel/src/wheel/reactive/impl/EventReceiver.java
+++ sneer/wheel/src/wheel/reactive/impl/EventReceiver.java
@@ -1,30 +1,18 @@
 package wheel.reactive.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import sneer.pulp.events.EventSource;
-
 import wheel.lang.Consumer;
 
 /** Instances of this class hold a reference to the EventSources they are receiving, so that these sources are not GCd. */
 public abstract class EventReceiver<T> implements Consumer<T> {
 	
-	private final List<EventSource<? extends T>> _sources = new ArrayList<EventSource<? extends T>>();
+	@SuppressWarnings("unused")	private final Object _referenceToAvoidGc;
 
-	public EventReceiver(EventSource<? extends T> eventSource) {
-		connectTo(eventSource);
-	}
-	
-	public EventReceiver(EventSource<? extends T>... eventSource) {
-		for (EventSource<? extends T> source : eventSource)
-			connectTo(source);
+	public EventReceiver(EventSource<? extends T>... eventSources) {
+		for (EventSource<? extends T> source : eventSources)
+			source.addReceiver(this);
+		
+		_referenceToAvoidGc = eventSources;
 	}
 
-	private void connectTo(EventSource<? extends T> eventSource) {
-		if (eventSource == null) throw new IllegalArgumentException();
-		if (_sources.add(eventSource))
-			eventSource.addReceiver(this);
-	}
-	
 }

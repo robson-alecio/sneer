276d618fdbdd238216836e7aa6263d6668d5508d
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-04-09 16:27:57 +0000

    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3644 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/contacts/actions/ContactAction.java
+++ sneer/bricks/src/snapps/contacts/actions/ContactAction.java
@@ -5,6 +5,7 @@ import wheel.io.ui.action.Action;
 
 public interface ContactAction extends Action{
 
+	//Refactor: remove setActive (see ContactsGui.selectedContact )
 	void setActive(Contact contact);
 	boolean isVisible();
 	boolean isEnabled();

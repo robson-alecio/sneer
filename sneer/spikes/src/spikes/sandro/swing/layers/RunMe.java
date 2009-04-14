837c39350403dc0d1aa45dcc662fb36d883bbc7a
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-04-14 22:42:05 +0000

    Run DashboadPane
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3734 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/spikes/src/spikes/sandro/swing/layers/RunMe.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package spikes.sandro.swing.layers;
+
+import javax.swing.JFrame;
+
+public class RunMe{
+	
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setContentPane(new DashboardPane());
+        frame.setBounds(10, 10, 300, 600);
+        frame.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() { public void run() {
+        	createAndShowGUI();
+        }});
+    }
+}

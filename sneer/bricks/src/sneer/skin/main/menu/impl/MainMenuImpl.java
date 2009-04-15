774fc7095eb7d1aa7f70b2fcd6f1759b460bb425
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-04-15 15:27:55 +0000

    Changed some packages. Preparing to new Dashboard
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3767 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

---
+++ sneer/bricks/src/sneer/skin/main/menu/impl/MainMenuImpl.java
@@ -0,0 +1,45 @@
+package sneer.skin.main.menu.impl;
+
+import static sneer.commons.environments.Environments.my;
+
+import javax.swing.JComponent;
+
+import sneer.skin.main.menu.MainMenu;
+import sneer.skin.menu.Menu;
+import sneer.skin.menu.MenuFactory;
+
+class MainMenuImpl implements MainMenu{
+
+	private static final long serialVersionUID = 1L;
+
+	private MenuFactory<JComponent> menuFactory = my(MenuFactory.class);
+	
+	private static transient Menu<JComponent> sneerMenu;
+	private static boolean initializaded = false;
+
+	private final Menu<JComponent> _delegate;
+	
+	MainMenuImpl(){
+		_delegate = menuFactory.createMenuBar();
+	}
+	
+	public void initialize() {
+		initializaded = true;
+		sneerMenu = menuFactory.createMenuGroup("Menu");
+		_delegate.addGroup(sneerMenu);
+	}
+
+	public Menu<JComponent> getSneerMenu() {
+		synchronized (this) {
+			if(!initializaded){
+				initialize();
+			}
+			return sneerMenu;
+		}
+	}
+
+	@Override
+	public JComponent getWidget() {
+		return _delegate.getWidget();
+	}
+}

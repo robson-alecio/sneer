0e9bf535b01e3687d6a1275bf77882f82771f9d6
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-04-22 20:52:28 +0000

    Refactoring, cleaning code.
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3874 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/meter/memory/gui/impl/MemoryMeterGuiImpl.java
+++ sneer/bricks/src/snapps/meter/memory/gui/impl/MemoryMeterGuiImpl.java
@@ -2,9 +2,7 @@ package snapps.meter.memory.gui.impl;
 
 import static sneer.commons.environments.Environments.my;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -16,6 +14,7 @@ import snapps.meter.memory.gui.MemoryMeterGui;
 import sneer.commons.lang.Functor;
 import sneer.hardware.gui.guithread.GuiThread;
 import sneer.pulp.memory.MemoryMeter;
+import sneer.pulp.reactive.Signal;
 import sneer.pulp.reactive.Signals;
 import sneer.skin.main.dashboard.InstrumentPanel;
 import sneer.skin.main.instrumentregistry.InstrumentRegistry;
@@ -31,31 +30,9 @@ class MemoryMeterGuiImpl implements MemoryMeterGui {
 	private final JButton gc = new JButton();
 	private final Synth _synth = my(Synth.class);
 	
-	JLabel _totalMemory = new JLabel();
-	
-	private final TextWidget<JLabel> _maxUsedMemory;{//Fix Use GUI Nature
-		final Object ref[] = new Object[1];
-		my(GuiThread.class).invokeAndWait(new Runnable(){ @Override public void run() {
-			ref[0] = _factory.newLabel(my(Signals.class).adapt(_meter.usedMBsPeak(), 
-					new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
-						return "Peak: " + value;
-					}}
-			), "MaxUsedMemoryLabel");
-		}});
-		_maxUsedMemory = (TextWidget<JLabel>) ref[0];
-	}
-	
-	private final TextWidget<JLabel> _currentMemory; {//Fix Use GUI Nature
-		final Object ref[] = new Object[1];
-		my(GuiThread.class).invokeAndWait(new Runnable(){ @Override public void run() {
-			ref[0]  = _factory.newLabel(my(Signals.class).adapt(_meter.usedMBs(), 
-					new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
-						return "MB Used: " + value;
-					}}
-			), "CurrentMemoryLabel");
-		}});
-		_currentMemory = (TextWidget<JLabel>) ref[0];
-	}
+	private final JLabel _totalMemory = new JLabel();
+	private final TextWidget<JLabel> _maxUsedMemory = newLabel(_meter.usedMBsPeak(), "Peak: ", "MaxUsedMemoryLabel");
+	private final TextWidget<JLabel> _currentMemory = newLabel(_meter.usedMBs(), "MB Used: ", "CurrentMemoryLabel");
 
 	public MemoryMeterGuiImpl() {
 		_instruments.registerInstrument(this);
@@ -77,29 +54,28 @@ class MemoryMeterGuiImpl implements MemoryMeterGui {
 		JComponent container = (JComponent) window.contentPane();
 		
 		_totalMemory.setText("(Max " + _meter.maxMBs() + ")");
-		container.setLayout(new GridBagLayout());
+		container.setLayout(new FlowLayout(FlowLayout.CENTER));
 
 		gc.addActionListener(new ActionListener(){ @Override public void actionPerformed(ActionEvent e) {
 			System.gc();
 		}});
 		
-		container.add(_currentMemory.getMainWidget(), new GridBagConstraints(0, 0, 1, 1, 1., 0,
-				GridBagConstraints.EAST, GridBagConstraints.NONE,
-				new Insets(2, 0, 2, 0), 0, 0));
-
-		container.add(gc, new GridBagConstraints(1, 0, 1, 1, 1., 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE,
-				new Insets(2, 0, 2, 0), 0, 0));
-		
-		container.add(_maxUsedMemory.getMainWidget(), new GridBagConstraints(2,0, 1, 1, 1., 0,
-				GridBagConstraints.CENTER, GridBagConstraints.NONE,
-				new Insets(2, 0, 2, 0), 0, 0));
-		
-		container.add(_totalMemory, new GridBagConstraints(3, 0, 1, 1, 1., 0,
-				GridBagConstraints.WEST, GridBagConstraints.NONE,
-				new Insets(2, 0, 2, 0), 0, 0));
+		container.add(_currentMemory.getComponent());
+		container.add(gc);
+		container.add(_maxUsedMemory.getComponent());
+		container.add(_totalMemory);
 	}
-
+	
+	private TextWidget<JLabel> newLabel(final Signal<Integer> source, final String prefix, final String synthName) {
+		final Object ref[] = new Object[1];
+		my(GuiThread.class).invokeAndWait(new Runnable(){ @Override public void run() {//Fix Use GUI Nature
+			ref[0] = _factory.newLabel(my(Signals.class).adapt(source, 	new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
+				return prefix + value;
+			}}), synthName);
+		}});
+		return (TextWidget<JLabel>) ref[0];
+	}
+	
 	@Override
 	public int defaultHeight() {
 		return DEFAULT_HEIGHT;

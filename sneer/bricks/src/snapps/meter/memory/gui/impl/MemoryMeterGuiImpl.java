4d074511b8dfb64cb917754ac419a463f61c9e8c
sandro <sandro@ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4>
2009-04-22 20:37:03 +0000

    Moved to Synth
    git-svn-id: https://sovereigncomputing.net/svn/sneer/trunk@3872 ee3b4d21-14f5-0310-a1fa-c5d8b2925ec4

--- sneer/bricks/src/snapps/meter/memory/gui/impl/MemoryMeterGuiImpl.java
+++ sneer/bricks/src/snapps/meter/memory/gui/impl/MemoryMeterGuiImpl.java
@@ -2,125 +2,102 @@ package snapps.meter.memory.gui.impl;
 
 import static sneer.commons.environments.Environments.my;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import snapps.meter.memory.gui.MemoryMeterGui;
 import sneer.commons.lang.Functor;
-import sneer.hardware.gui.images.Images;
+import sneer.hardware.gui.guithread.GuiThread;
 import sneer.pulp.memory.MemoryMeter;
 import sneer.pulp.reactive.Signals;
-import sneer.skin.colors.Colors;
 import sneer.skin.main.dashboard.InstrumentPanel;
 import sneer.skin.main.instrumentregistry.InstrumentRegistry;
+import sneer.skin.main.synth.Synth;
 import sneer.skin.widgets.reactive.ReactiveWidgetFactory;
 import sneer.skin.widgets.reactive.TextWidget;
 
 class MemoryMeterGuiImpl implements MemoryMeterGui {
 
 	private final InstrumentRegistry _instruments = my(InstrumentRegistry.class);
-	
 	private final ReactiveWidgetFactory _factory = my(ReactiveWidgetFactory.class);
-	
 	private final MemoryMeter _meter = my(MemoryMeter.class);
+	private final JButton gc = new JButton();
+	private final Synth _synth = my(Synth.class);
 	
-	JLabel _totalMemory =     new JLabel();
-	TextWidget<JLabel> _maxUsedMemory;
-	TextWidget<JLabel> _currentMemory;
-
-	private static final Icon _memoryIconOn = load("recycleOn.png");
-	private static final Icon _memoryIconOff = load("recycleOff.png");
+	JLabel _totalMemory = new JLabel();
+	
+	private final TextWidget<JLabel> _maxUsedMemory;{//Fix Use GUI Nature
+		final Object ref[] = new Object[1];
+		my(GuiThread.class).invokeAndWait(new Runnable(){ @Override public void run() {
+			ref[0] = _factory.newLabel(my(Signals.class).adapt(_meter.usedMBsPeak(), 
+					new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
+						return "Peak: " + value;
+					}}
+			), "MaxUsedMemoryLabel");
+		}});
+		_maxUsedMemory = (TextWidget<JLabel>) ref[0];
+	}
+	
+	private final TextWidget<JLabel> _currentMemory; {//Fix Use GUI Nature
+		final Object ref[] = new Object[1];
+		my(GuiThread.class).invokeAndWait(new Runnable(){ @Override public void run() {
+			ref[0]  = _factory.newLabel(my(Signals.class).adapt(_meter.usedMBs(), 
+					new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
+						return "MB Used: " + value;
+					}}
+			), "CurrentMemoryLabel");
+		}});
+		_currentMemory = (TextWidget<JLabel>) ref[0];
+	}
 
 	public MemoryMeterGuiImpl() {
 		_instruments.registerInstrument(this);
+		initSynth();
 	} 
-	
-	static Icon load(String resourceName){
-		return new ImageIcon(my(Images.class).getImage(MemoryMeterGuiImpl.class.getResource(resourceName)));
+
+	private void initSynth() {
+		_synth.load(this);
+		
+		gc.setName("GCButton");
+		_totalMemory.setName("TotalMemoryLabel");
+		
+		_synth.attach(gc);
+		_synth.attach(_totalMemory);
 	}
 	
 	@Override
 	public void init(InstrumentPanel window) {
-		Container container = window.contentPane();
-		initGui(container);
-	}
-
-	private void initGui(Container container) {
+		JComponent container = (JComponent) window.contentPane();
+		
 		_totalMemory.setText("(Max " + _meter.maxMBs() + ")");
-		initReactiveLabels();
-		
-		JPanel root = new JPanel();
-		root.setOpaque(false);
-		root.setLayout(new GridBagLayout());
-		
-		final JButton gc = new JButton(_memoryIconOff);
-		gc.setMargin(new Insets(0,0,0,0));
-		gc.setBorder(new EmptyBorder(0,0,0,0));
-		gc.setOpaque(true);
-		gc.setBackground(my(Colors.class).solid());
+		container.setLayout(new GridBagLayout());
+
 		gc.addActionListener(new ActionListener(){ @Override public void actionPerformed(ActionEvent e) {
 			System.gc();
 		}});
-		gc.addMouseListener(new MouseAdapter(){
-			@Override public void mouseEntered(MouseEvent e) { gc.setIcon(_memoryIconOn);	}
-			@Override public void mouseExited(MouseEvent e) { gc.setIcon(_memoryIconOff); }
-		});
 		
-		root.add(_currentMemory.getMainWidget(), new GridBagConstraints(0, 0, 1, 1, 1., 0,
+		container.add(_currentMemory.getMainWidget(), new GridBagConstraints(0, 0, 1, 1, 1., 0,
 				GridBagConstraints.EAST, GridBagConstraints.NONE,
 				new Insets(2, 0, 2, 0), 0, 0));
 
-		root.add(gc, new GridBagConstraints(1, 0, 1, 1, 1., 0,
+		container.add(gc, new GridBagConstraints(1, 0, 1, 1, 1., 0,
 				GridBagConstraints.CENTER, GridBagConstraints.NONE,
 				new Insets(2, 0, 2, 0), 0, 0));
 		
-		root.add(_maxUsedMemory.getMainWidget(), new GridBagConstraints(2,0, 1, 1, 1., 0,
+		container.add(_maxUsedMemory.getMainWidget(), new GridBagConstraints(2,0, 1, 1, 1., 0,
 				GridBagConstraints.CENTER, GridBagConstraints.NONE,
 				new Insets(2, 0, 2, 0), 0, 0));
 		
-		root.add(_totalMemory, new GridBagConstraints(3, 0, 1, 1, 1., 0,
+		container.add(_totalMemory, new GridBagConstraints(3, 0, 1, 1, 1., 0,
 				GridBagConstraints.WEST, GridBagConstraints.NONE,
 				new Insets(2, 0, 2, 0), 0, 0));
-		
-		
-		changeLabelFont(_totalMemory);
-		changeLabelFont(_maxUsedMemory.getMainWidget());
-		changeLabelFont(_currentMemory.getMainWidget());
-		
-		container.setBackground(my(Colors.class).solid());
-		container.setLayout(new BorderLayout());
-		container.add(root, BorderLayout.CENTER);
-	}
-
-	private void changeLabelFont(JLabel label) {
-		label.setFont(label.getFont().deriveFont(Font.ITALIC));
-	}
-
-	private void initReactiveLabels() {
-		Functor<Integer, String> functor = new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
-			return "Peak: " + value;
-		}};
-		_maxUsedMemory = _factory.newLabel(my(Signals.class).adapt(_meter.usedMBsPeak(), functor));
-		
-		functor = new Functor<Integer, String>(){@Override public String evaluate(Integer value) {
-			return "MB Used: " + value;
-		}};
-		_currentMemory = _factory.newLabel(my(Signals.class).adapt(_meter.usedMBs(), functor));
 	}
 
 	@Override

41f2c6af0f8554fa32cd80871949b5725e532304
Igor Arouca <igor.arouca@gmail.com>
2009-05-05 16:57:49 -0300

    Cleaning up wheel: FrozenTime
    Signed-off-by: Igor Arouca <igor.arouca@gmail.com>

--- sneer/tests/src/main/Sneer.java
+++ sneer/tests/src/main/Sneer.java
@@ -76,6 +76,7 @@ public class Sneer {
 				sneer.pulp.memory.MemoryMeter.class,
 				sneer.skin.sound.kernel.Audio.class,
 				sneer.pulp.lang.StringUtils.class,
+				sneer.pulp.lang.FrozenTime.class,
 				sneer.pulp.bandwidth.BandwidthCounter.class,
 				sneer.skin.screenshotter.Screenshotter.class,
 				sneer.pulp.connection.ConnectionManager.class,

1ad2ce8ccadfed2a4f62c02578c948895bf8bd36
Sandro Bihaiko <bihaiko@gmail.com>
2009-05-06 15:54:28 -0300

    MapValueChange simplified

--- sneer/bricks/src/sneer/pulp/reactive/collections/MapValueChange.java
+++ sneer/bricks/src/sneer/pulp/reactive/collections/MapValueChange.java
@@ -1,14 +1,12 @@
 package sneer.pulp.reactive.collections;
 
-public interface MapValueChange<K,V>{
+public interface MapValueChange<K,V> {
 	
 	void accept(Visitor<K,V> visitor);
 	
 	public interface Visitor<K,V> {
 		void entryAdded(K key, V value);
-		void entryToBeRemoved(K key, V value);
 		void entryRemoved(K key, V value);
-		void entryToBeReplaced(K key, V value);
 		void entryReplaced(K key, V value);
 	}
 }

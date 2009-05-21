31706800188d77531c81a1be189411a369526c3a
Sandro Bihaiko <bihaiko@gmail.com>
2009-05-21 17:21:20 -0300

    sysout --> Logger.log()

--- sneer/bricks/src/sneer/pulp/reactive/tests/SignalsTest.java
+++ sneer/bricks/src/sneer/pulp/reactive/tests/SignalsTest.java
@@ -8,6 +8,7 @@ import sneer.brickness.testsupport.BrickTest;
 import sneer.commons.lang.Functor;
 import sneer.hardware.cpu.lang.Consumer;
 import sneer.pulp.clock.Clock;
+import sneer.pulp.log.Logger;
 import sneer.pulp.reactive.Register;
 import sneer.pulp.reactive.Signal;
 import sneer.pulp.reactive.Signals;
@@ -72,7 +73,7 @@ public class SignalsTest extends BrickTest {
 			System.gc();
 			Clock clock = my(Clock.class);
 			clock.advanceTime(60000);
-			System.out.println("Test's Clock --> " + this.hashCode());
+			my(Logger.class).log("Test's Clock --> {}", this.hashCode());
 
 			received.delete(0, received.length());
 			register1.setter().consume("something else");

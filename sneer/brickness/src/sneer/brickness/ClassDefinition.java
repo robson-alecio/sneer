8c4948f14d485cf29d8c8cdac2323054a7358752
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-05-27 15:49:01 -0300

    Minor refactoring.

--- sneer/brickness/src/sneer/brickness/ClassDefinition.java
+++ sneer/brickness/src/sneer/brickness/ClassDefinition.java
@@ -2,19 +2,11 @@ package sneer.brickness;
 
 public class ClassDefinition extends Tuple {
 
-	public final byte[] _bytes;
-	public final String _name;
+	public final String name;
+	public final byte[] bytes;
 
-	public ClassDefinition(String name, byte[] bytes) {
-		_name = name;
-		_bytes = bytes;
-	}
-
-	public byte[] bytes() {
-		return _bytes;
-	}
-
-	public String name() {
-		return _name;
+	public ClassDefinition(String name_, byte[] bytes_) {
+		name = name_;
+		bytes = bytes_;
 	}
 }

6f58719e19d177736a797b43f2bc3087fa71e9ae
Sandro Bihaiko <bihaiko@gmail.com>
2009-05-28 19:01:14 -0300

    removed error msg dialog, using webstart console to log.

--- sneer/installer/src/sneer/installer/Main.java
+++ sneer/installer/src/sneer/installer/Main.java
@@ -1,31 +1,20 @@
 package sneer.installer;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.PrintWriter;
 
 import main.SneerStoragePath;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		try {
-			SneerStoragePath sneerStoragePath = new SneerStoragePath();
-			File sneerHome = new File(sneerStoragePath.get());
-			
-			if(sneerHome.exists() && sneerHome.isDirectory()){
-				new SneerJockey(sneerStoragePath);
-				return;
-			}
-			
-			new Wizard(sneerStoragePath);
-		} catch (Throwable throwable) {
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			PrintWriter _log = new PrintWriter(out, true);
-			throwable.printStackTrace(_log);
-			
-			Dialogs.show("Sneer Error:", throwable.getMessage() + "\n\n" + new String(out.toByteArray()),	"Exit");
-			System.exit(1);
+
+	public static void main(String[] args) throws Exception {
+		SneerStoragePath sneerStoragePath = new SneerStoragePath();
+		File sneerHome = new File(sneerStoragePath.get());
+
+		if (sneerHome.exists() && sneerHome.isDirectory()) {
+			new SneerJockey(sneerStoragePath);
+			return;
 		}
+
+		new Wizard(sneerStoragePath);
 	}
 }

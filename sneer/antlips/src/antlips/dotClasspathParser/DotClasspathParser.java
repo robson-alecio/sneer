580f32ac9105be4161c0dd826f727a7bdc4fa230
igorarouca <igor.arouca@gmail.com>
2009-05-29 14:33:19 -0300

    A minor refactoring to avoid compiling the same regex more than once and to make it easier to spot the Pattern.

--- sneer/antlips/src/antlips/dotClasspathParser/DotClasspathParser.java
+++ sneer/antlips/src/antlips/dotClasspathParser/DotClasspathParser.java
@@ -9,7 +9,10 @@ import org.apache.commons.lang.Validate;
 
 
 public class DotClasspathParser {
-	
+
+	private static final Pattern _libPattern = Pattern.compile(".+classpathentry kind=\"lib\"(.+)?\\spath=\"([^\"]+)\".+");
+	private static final Pattern _sourcePattern = Pattern.compile(".+classpathentry kind=\"src\"(?:\\soutput=\"(.+)\")?(\\s)?path=\"(.+)\".+");
+
 	public static DotClasspath parse(final String classpathString) {
 
 		Validate.notNull(classpathString, "classpathString was null");
@@ -24,21 +27,19 @@ public class DotClasspathParser {
 	}
 
 	private static List<String> getLibs(final String classpathString) {
-		final Pattern sourcePattern = Pattern.compile(".+classpathentry kind=\"lib\"(.+)?\\spath=\"([^\"]+)\".+");
-		final Matcher matcher = sourcePattern.matcher(classpathString);
+		final Matcher matcher = _libPattern.matcher(classpathString);
 
 		final List<String> libPaths = new ArrayList<String>();
 		while (matcher.find()){
 			final String lib = matcher.group(2);
 			libPaths .add(lib);
 		}
-		return libPaths;
 
+		return libPaths;
 	}
 
 	private static List<DotClasspath.Entry> getSrcPaths(final String classpathString) {
-		final Pattern sourcePattern = Pattern.compile(".+classpathentry kind=\"src\"(?:\\soutput=\"(.+)\")?(\\s)?path=\"(.+)\".+");
-		final Matcher matcher = sourcePattern.matcher(classpathString);
+		final Matcher matcher = _sourcePattern.matcher(classpathString);
 
 		final List<DotClasspath.Entry> sourcePaths = new ArrayList<DotClasspath.Entry>();
 		while (matcher.find()){
@@ -46,8 +47,7 @@ public class DotClasspathParser {
 			final String srcPath = matcher.group(3);
 			sourcePaths.add(DotClasspath.entry(srcPath, outputPath));
 		}
+
 		return sourcePaths;
 	}
-	
-
 }

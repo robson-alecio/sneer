0c3ec93c48c80402aa4f4ecb4d7d13da8cda2e21
Sandro Bihaiko <bihaiko@gmail.com>
2009-06-09 10:54:16 -0300

    Roadmap Item Done (Commons IO removed from JavaFilter Brick).

--- code/bricks/src/sneer/software/code/filefilters/java/impl/JavaFilterImpl.java
+++ code/bricks/src/sneer/software/code/filefilters/java/impl/JavaFilterImpl.java
@@ -3,7 +3,6 @@ package sneer.software.code.filefilters.java.impl;
 import static sneer.commons.environments.Environments.my;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -15,42 +14,54 @@ import sneer.software.code.metaclass.MetaClasses;
 /**
  * Includes all class files outside hidden directories
  */
-class JavaFilterImpl extends SimpleFilter implements JavaFilter {
+class JavaFilterImpl implements JavaFilter {
 
 	private List<MetaClass> _cache;
+	private final File _root;
 
 	public JavaFilterImpl(File root) {
-		super(root, JAVA_CLASS_FILE_FILTER);
+		_root = root;
 	}
 
-	@Override
-	public List<MetaClass> listMetaClasses() {
-		if(_cache != null) {
-			return _cache;
-		}
-		_cache = (List<MetaClass>) walkAndCollect(new ArrayList<MetaClass>());
-		return _cache; 
+	protected Collection<MetaClass> walkAndCollect(Collection<MetaClass> c) {
+			walk(_root, c);
+			return c;
 	}
 
 	@Override
 	public List<File> listFiles() {
 		List<File> result = new ArrayList<File>();
-		List<MetaClass> metaClasses = listMetaClasses();
-		for (MetaClass metaClass : metaClasses) {
+		
+		for (MetaClass metaClass : listMetaClasses()) 
 			result.add(metaClass.classFile());
-		}
+		
 		return result;
 	}
-
-	@SuppressWarnings("unchecked")
+	
 	@Override
-	protected void handleFile(File file, int depth, Collection results) throws IOException {
-		MetaClass metaClass = my(MetaClasses.class).metaClass(_root, file);
-		handleClass(metaClass, depth, results);
+	public List<MetaClass> listMetaClasses() {
+		if (_cache == null) 
+			_cache = (List<MetaClass>) walkAndCollect(new ArrayList<MetaClass>());
+		
+		return _cache;
+	}
+	
+	protected final void walk(File startDirectory, Collection<MetaClass> results) {
+		if (startDirectory == null) 
+			throw new NullPointerException("Start Directory is null");
+		
+		walk(startDirectory, 0, results);
 	}
 
-	@SuppressWarnings("unused")
-	protected void handleClass(MetaClass metaClass, int depth, Collection<MetaClass> results) throws IOException {
-		results.add(metaClass);
+	private void walk(File directory, int depth, Collection<MetaClass> results) {
+		int childDepth = depth + 1;
+		File[] childFiles = directory.listFiles();
+		
+		if (childFiles != null) 
+			for (File childFile : childFiles) 
+				if (childFile.isDirectory()) 
+					walk(childFile, childDepth, results);
+				else 
+					results.add(my(MetaClasses.class).metaClass(_root, childFile));
 	}
 }

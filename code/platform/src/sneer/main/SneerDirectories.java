d86c3fdbea48ce9e10e7948d97972c9e6e809fa1
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-06-22 03:35:42 -0300

    Dummy Progress

--- code/platform/src/sneer/main/SneerDirectories.java
+++ code/platform/src/sneer/main/SneerDirectories.java
@@ -5,19 +5,25 @@ import java.io.File;
 public class SneerDirectories {
 
 	public static final File SNEER_HOME 	= sneerHome();
-	public static final File DATA 			= new File(SNEER_HOME, "data");
+	public static final File DATA 			= new File(SNEER_HOME, dummyPrefix() + "data");
+	public static final File LOG_FILE 		= new File(SNEER_HOME, dummyPrefix() + "log/log.txt");
 	public static final File CODE 			= new File(SNEER_HOME, "code");
 	public static final File OWN_CODE 		= new File(SNEER_HOME, "code/own");
 	public static final File OWN_BIN 		= new File(SNEER_HOME, "code/own/bin");
 	public static final File PLATFORM_CODE 	= new File(SNEER_HOME, "code/platform");
 	public static final File PLATFORM_BIN 	= new File(SNEER_HOME, "code/platform/bin");
-	public static final File LOG_FILE 		= new File(SNEER_HOME, "log/log.txt");
 
 	private static File sneerHome() {
 		String override = System.getProperty("sneer.home");
 		if (override != null) return new File(override);
 
 		return new File(System.getProperty("user.home"), "sneer");
+	}
+
+	private static String dummyPrefix() {
+		return "true".equals(System.getProperty("sneer.dummy"))
+			? "dummy"
+			: "";
 	}
 
 }

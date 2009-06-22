4b9b70fe83a39e17998b6ffec03008845aab9e9e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-06-22 09:51:55 -0300

    SneerHome @Deprecated removed.

--- code/platform/src/sneer/foundation/brickness/SneerHome.java
+++ code/platform/src/sneer/foundation/brickness/SneerHome.java
@@ -2,7 +2,6 @@ package sneer.foundation.brickness;
 
 import sneer.foundation.environments.EnvironmentProperty;
 
-@Deprecated
 @Brick
 /** The place where you should persist your files. */
 public interface SneerHome extends EnvironmentProperty<String> {}

0aedf798dbd7e951cc0b3f2de1ec798e2a872535
Sandro Bihaiko <bihaiko@gmail.com>
2009-06-22 14:41:12 -0300

    Roadmap Item Done (Remove Installer duplication  and Update).

--- code/installer/src/sneer/installer/Main.java
+++ code/installer/src/sneer/installer/Main.java
@@ -1,15 +1,17 @@
 package sneer.installer;
 
 import sneer.main.Sneer;
+import sneer.main.SneerDirectories;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		new InstallationWizard();
+		if(SneerDirectories.SNEER_HOME.exists()) new Installation();
+		else new InstallationWizard();
+		
 		overcomeWebstartSecurityRestrictions();
 		new Sneer();
 	}
-
 
 	private static void overcomeWebstartSecurityRestrictions() {
 		System.setSecurityManager(new PermissiveSecurityManager());

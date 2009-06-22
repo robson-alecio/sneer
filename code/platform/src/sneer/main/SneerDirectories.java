4f5364d0428a2178b60c91b006e7e50ae02e3b32
Sandro Bihaiko <bihaiko@gmail.com>
2009-06-22 14:46:09 -0300

    .\log\ --> .\logs\

--- code/platform/src/sneer/main/SneerDirectories.java
+++ code/platform/src/sneer/main/SneerDirectories.java
@@ -6,7 +6,7 @@ public class SneerDirectories {
 
 	public static final File SNEER_HOME 	= sneerHome();
 	public static final File DATA 			= new File(SNEER_HOME, dummyPrefix() + "data");
-	public static final File LOG_FILE 		= new File(SNEER_HOME, dummyPrefix() + "log/log.txt");
+	public static final File LOG_FILE 		= new File(SNEER_HOME, dummyPrefix() + "logs/log.txt");
 	public static final File CODE 			= new File(SNEER_HOME, "code");
 	public static final File OWN_CODE 		= new File(SNEER_HOME, "code/own");
 	public static final File OWN_BIN 		= new File(SNEER_HOME, "code/own/bin");

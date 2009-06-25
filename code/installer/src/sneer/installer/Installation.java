0dee35689a7a5578f1bcfdeecf82dbecee282ee7
Sandro Bihaiko <bihaiko@gmail.com>
2009-06-25 14:08:00 -0300

    Roadmap Item Done (Make installer create code/own project).

--- code/installer/src/sneer/installer/Installation.java
+++ code/installer/src/sneer/installer/Installation.java
@@ -12,11 +12,21 @@ import java.util.jar.JarInputStream;
 class Installation {
 
 	private final URL jarFileName = this.getClass().getResource("/sneer.jar");
+	private final URL ownFileName = this.getClass().getResource("/own.jar");
 	
 	Installation() throws IOException {
 		cleanup();
 		createDirectories();
 		addBinaries();
+		createOwnProjectIfNecessary();
+	}
+
+	private void createOwnProjectIfNecessary() throws IOException {
+		if(ownCode().exists()) return;
+		
+		IOUtils.write(logFile(), "jar file url: " + ownFileName.toString());
+		File file = extractJar(ownFileName, "own", "jar");
+		extractFiles(file, ownCode().getParentFile());		
 	}
 
 	private void cleanup() throws IOException {
@@ -31,12 +41,12 @@ class Installation {
 
 	private void addBinaries() throws IOException {
 		IOUtils.write(logFile(), "jar file url: " + jarFileName.toString());
-		File file = extractJar(jarFileName);
-		extractFiles(file);
+		File file = extractJar(jarFileName, "sneer", "jar");
+		extractFiles(file, platformCode());
 	}
 
-	private File extractJar(URL url) throws IOException {
-		File file =  File.createTempFile("sneer", ".jar");
+	private File extractJar(URL url, String prefix, String suffix) throws IOException {
+		File file =  File.createTempFile(prefix, suffix);
 		file.deleteOnExit();
 
 		InputStream input = url.openStream();
@@ -45,23 +55,23 @@ class Installation {
 		return file;
 	}
 	
-	private void extractFiles(File src) throws IOException {
+	private void extractFiles(File src, File toDir) throws IOException {
 		IOUtils.write(logFile(), "expand files from: " + src.getAbsolutePath());
 		if(!(src.exists()))
 			throw new IOException("File '" + src.getAbsolutePath() + "' not found!");	
 
 		FileInputStream inputStream = new FileInputStream(src);
-		extractFiles(src, inputStream);
+		extractFiles(src, toDir, inputStream);
 		inputStream.close();
 	}
 
-	private void extractFiles(File src, FileInputStream inputStream) throws IOException {
+	private void extractFiles(File src, File toDir, FileInputStream inputStream) throws IOException {
 		JarInputStream jis = new JarInputStream(inputStream);
 		JarFile jar = new JarFile(src);
 		JarEntry entry = null;
 		
         while ((entry = jis.getNextJarEntry()) != null) {
-        	File file = new File(platformCode(), entry.getName());
+        	File file = new File(toDir, entry.getName());
 
         	if(entry.isDirectory()) {
         		file.mkdirs();
@@ -70,7 +80,8 @@ class Installation {
         	IOUtils.writeEntry(jar, entry, file);
         }
 	}
-	
+
+	private File ownCode() { return Directories.OWN_CODE(); }
 	private File platformCode() { return Directories.PLATFORM_CODE(); }
 	private File sneerHome() { return Directories.SNEER_HOME(); }
 	private File logFile() { return Directories.LOG_FILE(); }

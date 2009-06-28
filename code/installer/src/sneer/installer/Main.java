68c19eb0c2a40e3c553317af465322149fe615a9
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-06-28 01:06:48 -0300

    Minor refactoring.

--- code/installer/src/sneer/installer/Main.java
+++ code/installer/src/sneer/installer/Main.java
@@ -5,8 +5,10 @@ import sneer.main.Sneer;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		if(Directories.SNEER_HOME().exists()) new Installation();
-		else new InstallationWizard();
+		if(Directories.SNEER_HOME().exists())
+			new Installation();
+		else
+			new InstallationWizard();
 		
 		overcomeWebstartSecurityRestrictions();
 		new Sneer();

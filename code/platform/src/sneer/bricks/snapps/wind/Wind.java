7380eb6ba2b1f0317745a67b2442e91ed80428ab
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-07-01 20:12:10 -0300

    Wind now registered as a Snapp

--- code/platform/src/sneer/bricks/snapps/wind/Wind.java
+++ code/platform/src/sneer/bricks/snapps/wind/Wind.java
@@ -1,9 +1,11 @@
 package sneer.bricks.snapps.wind;
 
 import sneer.bricks.pulp.reactive.collections.ListSignal;
+import sneer.bricks.software.bricks.snappstarter.Snapp;
 import sneer.foundation.brickness.Brick;
 import sneer.foundation.lang.Consumer;
 
+@Snapp
 @Brick
 public interface Wind {
 
--- code/platform/src/sneer/tests/adapters/impl/SneerPartyProbeImpl.java
+++ code/platform/src/sneer/tests/adapters/impl/SneerPartyProbeImpl.java
@@ -168,6 +168,7 @@ class SneerPartyProbeImpl implements SneerPartyProbe, SneerParty {
 		startAndKeep(sneer.bricks.network.computers.sockets.connections.originator.SocketOriginator.class);
 		startAndKeep(sneer.bricks.network.computers.sockets.connections.receiver.SocketReceiver.class);
 		startAndKeep(sneer.bricks.pulp.probe.ProbeManager.class);
+		startAndKeep(sneer.bricks.snapps.wind.Wind.class);
 		startAndKeep(sneer.bricks.network.social.heartbeat.Heart.class);
 	}
 

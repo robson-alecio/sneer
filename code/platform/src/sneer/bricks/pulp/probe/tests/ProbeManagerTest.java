4d7db46c348e5a44770c945ef7a11190bd3a6aa8
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-07-01 22:22:35 -0300

    Unfinished test deleted.

--- code/platform/src/sneer/bricks/pulp/probe/tests/ProbeManagerTest.java
+++ code/platform/src/sneer/bricks/pulp/probe/tests/ProbeManagerTest.java
@@ -7,7 +7,6 @@ import java.io.OutputStream;
 import org.jmock.Expectations;
 import org.jmock.api.Invocation;
 import org.jmock.lib.action.CustomAction;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.bricks.network.computers.sockets.connections.ByteConnection;
@@ -15,13 +14,13 @@ import sneer.bricks.network.computers.sockets.connections.ConnectionManager;
 import sneer.bricks.network.computers.sockets.connections.ByteConnection.PacketScheduler;
 import sneer.bricks.network.social.Contact;
 import sneer.bricks.network.social.ContactManager;
-import sneer.bricks.pulp.bandwidth.BandwidthCounter;
 import sneer.bricks.pulp.distribution.filtering.TupleFilterManager;
 import sneer.bricks.pulp.probe.ProbeManager;
-import sneer.bricks.pulp.reactive.SignalUtils;
 import sneer.bricks.pulp.reactive.Signals;
 import sneer.bricks.pulp.serialization.Serializer;
 import sneer.bricks.pulp.tuples.TupleSpace;
+import sneer.bricks.software.bricks.statestore.BrickStateStore;
+import sneer.bricks.software.bricks.statestore.tests.BrickStateStoreMock;
 import sneer.foundation.brickness.testsupport.Bind;
 import sneer.foundation.brickness.testsupport.BrickTest;
 import sneer.foundation.lang.Consumer;
@@ -30,7 +29,8 @@ public class ProbeManagerTest extends BrickTest {
 
 	@Bind private final ConnectionManager _connectionManager = mock(ConnectionManager.class);
 	@Bind private final Serializer _serializer = mock(Serializer.class);
-	@Bind private final BandwidthCounter _bandwidthCounter = mock(BandwidthCounter.class);
+	@SuppressWarnings("unused")
+	@Bind private final BrickStateStore _store = new BrickStateStoreMock();
 
 	@SuppressWarnings("unused")
 	private final ProbeManager _subject = my(ProbeManager.class);
@@ -42,7 +42,6 @@ public class ProbeManagerTest extends BrickTest {
 	@SuppressWarnings("unused")
 	private Consumer<byte[]> _packetReceiver;
 
-	@Ignore
 	@Test (timeout = 1000)
 	public void testTupleBlocking() throws Exception {
 		checking(new Expectations(){{
@@ -84,43 +83,4 @@ public class ProbeManagerTest extends BrickTest {
 		assertEquals(id, packet[0]);
 	}
 
-	@Test (timeout = 1000)
-	@Ignore
-	public void testBandwidthReporting() throws Exception {
-		checking(new Expectations(){{
-			one(_connectionManager).connectionFor(with(aNonNull(Contact.class))); will(returnValue(_connection));
-			one(_connection).isConnected(); will(returnValue(my(Signals.class).constant(true)));
-			one(_connection).initCommunications(with(aNonNull(PacketScheduler.class)), with(aNonNull(Consumer.class)));
-				will(new CustomAction("capturing params") { @Override public Object invoke(Invocation invocation) throws Throwable {
-					_scheduler = (PacketScheduler) invocation.getParameter(0);
-					_packetReceiver = (Consumer<byte[]>)invocation.getParameter(1);
-					return null;
-				}});
-			one(_bandwidthCounter).sent(1024);
-			allowing(_serializer).serialize(with(aNonNull(TupleWithId.class)));
-				will(new CustomAction("serializing") { @Override public Object invoke(Invocation invocation) throws Throwable {
-					return new byte[1024];
-				}});
-
-		}});
-
-		my(ContactManager.class).addContact("Neide");
-
-		_tuples.acquire(new TupleTypeA(1));
-		my(SignalUtils.class).waitForValue(1, _bandwidthCounter.uploadSpeed());
-		
-		
-		_tuples.acquire(new TupleTypeB(2));
-		assertPacketToSend(2);
-
-		_filter.block(TupleTypeB.class);
-		
-		_tuples.acquire(new TupleTypeA(3));
-		_tuples.acquire(new TupleTypeB(4));
-		assertPacketToSend(3);
-		_tuples.acquire(new TupleTypeA(5));
-		assertPacketToSend(5);
-	}
-
-	
 }

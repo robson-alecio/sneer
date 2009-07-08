a6b1feb9f70a954e9b0269237a6d71682b12636e
igorarouca <igor.arouca@gmail.com>
2009-07-08 16:48:34 -0300

    Change in ExceptionLoggerImpl (suggested by Celio Cidral at SneerCoders)

--- code/platform/src/sneer/bricks/hardware/io/log/exceptions/impl/ExceptionLoggerImpl.java
+++ code/platform/src/sneer/bricks/hardware/io/log/exceptions/impl/ExceptionLoggerImpl.java
@@ -6,21 +6,30 @@ import java.lang.Thread.UncaughtExceptionHandler;
 
 import sneer.bricks.hardware.io.log.Logger;
 import sneer.bricks.hardware.io.log.exceptions.ExceptionLogger;
+import sneer.foundation.environments.Environment;
+import sneer.foundation.environments.Environments;
 
-class ExceptionLoggerImpl implements ExceptionLogger, UncaughtExceptionHandler{
+class ExceptionLoggerImpl implements ExceptionLogger, UncaughtExceptionHandler {
 
-	private final Logger _logger = my(Logger.class);
-	
+	private final Environment _environment = my(Environment.class);
+
 	{
 		Thread.setDefaultUncaughtExceptionHandler(this);
 	}
 
 	@Override
-	public void uncaughtException(Thread ignored, Throwable t1) {
+	public void uncaughtException(Thread ignored, final Throwable t1) {
 		try {
-			_logger.log(t1);
+			Environments.runWith(_environment, new Runnable() {
+				@Override
+				public void run() {
+					my(Logger.class).log(t1);
+				}
+			});
 		} catch (Throwable t2) {
 			t2.printStackTrace();
+			System.err.println("The above was thrown while trying to log this throwable:");
+			t1.printStackTrace();
 		}
 	}
 }

3ddcd5882128de77add88d6e3575a6c627b085b8
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-08-10 17:01:30 -0300

    SocketOriginatorTest adapted to new protocol.

--- code/platform/src/sneer/bricks/network/computers/sockets/connections/originator/tests/SocketOriginatorTest.java
+++ code/platform/src/sneer/bricks/network/computers/sockets/connections/originator/tests/SocketOriginatorTest.java
@@ -5,12 +5,12 @@ import static sneer.foundation.environments.Environments.my;
 import org.jmock.Expectations;
 import org.jmock.api.Invocation;
 import org.jmock.lib.action.CustomAction;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.bricks.hardware.clock.Clock;
 import sneer.bricks.hardware.cpu.threads.Latch;
 import sneer.bricks.hardware.cpu.threads.Threads;
+import sneer.bricks.network.computers.sockets.connections.ByteConnection;
 import sneer.bricks.network.computers.sockets.connections.ConnectionManager;
 import sneer.bricks.network.computers.sockets.connections.originator.SocketOriginator;
 import sneer.bricks.network.social.Contact;
@@ -18,10 +18,11 @@ import sneer.bricks.network.social.ContactManager;
 import sneer.bricks.pulp.internetaddresskeeper.InternetAddressKeeper;
 import sneer.bricks.pulp.network.ByteArraySocket;
 import sneer.bricks.pulp.network.Network;
+import sneer.bricks.pulp.reactive.Signal;
+import sneer.bricks.pulp.reactive.Signals;
 import sneer.foundation.brickness.testsupport.Bind;
 import sneer.foundation.brickness.testsupport.BrickTest;
 
-@Ignore
 public class SocketOriginatorTest extends BrickTest {
 
 	@SuppressWarnings("unused")
@@ -30,16 +31,23 @@ public class SocketOriginatorTest extends BrickTest {
 	@Bind private final Network _networkMock = mock(Network.class);
 	@Bind private final ConnectionManager _connectionManagerMock = mock(ConnectionManager.class);
 
+	private final ByteConnection _byteConnection = mock(ByteConnection.class);
+	private final Signal<Boolean> _isConnected = my(Signals.class).constant(false);
 	private final ByteArraySocket _openedSocket = mock(ByteArraySocket.class);
-	private final InternetAddressKeeper _internetAddressKeeper = my(InternetAddressKeeper.class);
-	private final ContactManager _contactManager = my(ContactManager.class);
+
 
 	@Test (timeout = 2000)
 	public void openConnection() throws Exception {
 		final Latch _ready = my(Threads.class).newLatch();
-		final Contact neide = _contactManager.produceContact("Neide");
-		
+		final Contact neide = my(ContactManager.class).produceContact("Neide");
+
 		checking(new Expectations() {{
+			oneOf(_connectionManagerMock).connectionFor(neide);
+				will(returnValue(_byteConnection));
+
+			oneOf(_byteConnection).isConnected();
+				will(returnValue(_isConnected));
+
 			oneOf(_networkMock).openSocket("neide.selfip.net", 5000);
 				will(returnValue(_openedSocket));
 
@@ -51,7 +59,7 @@ public class SocketOriginatorTest extends BrickTest {
 
 		_subject = my(SocketOriginator.class);
 
-		_internetAddressKeeper.add(neide, "neide.selfip.net", 5000);
+		my(InternetAddressKeeper.class).add(neide, "neide.selfip.net", 5000);
 		my(Clock.class).advanceTime(1);
 		_ready.waitTillOpen();
 	}

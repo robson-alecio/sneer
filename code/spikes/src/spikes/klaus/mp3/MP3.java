a254f6444bc782128586927e7a99940e03700ff2
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-08-10 03:38:44 -0300

    jlayer spike with stop()

--- code/spikes/src/spikes/klaus/mp3/MP3.java
+++ code/spikes/src/spikes/klaus/mp3/MP3.java
@@ -1,56 +1,63 @@
 package spikes.klaus.mp3;
- 
+
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
- 
+
 public class MP3 {
- 
-	public static void main(String[] args) throws FileNotFoundException, JavaLayerException {
-		String path = "C:/Documents and Settings/User1/Desktop/mp3/musica/Avril Lavigne - Girlfriend.mp3";
-		File mp3File = new File(path);
-		MP3 musica = new MP3(mp3File);
+
+	public static void main(String[] args) throws FileNotFoundException,	JavaLayerException {
+		File songs = new File("C:/Documents and Settings/User1/Desktop/mp3/musica");
+
+		for (File song : songs.listFiles())
+			playIntro(song);
+	}
+
+	private static void playIntro(File song) throws FileNotFoundException, JavaLayerException {
+		MP3 musica = new MP3(song);
 		musica.play();
 	}
 
-	
 	private File mp3;
- 
+
 	private Player player;
- 
+
 	public MP3(File mp3) {
 		this.mp3 = mp3;
 	}
- 
+
 	@SuppressWarnings("deprecation")
 	public void play() throws FileNotFoundException, JavaLayerException {
-            FileInputStream fis     = new FileInputStream(mp3);
-            //BufferedInputStream bis = new BufferedInputStream(fis);
-            player = new Player(fis);
-            
-            
-            final Thread playerThread = new Thread() { @Override public void run() {
-                System.out.println("Tocando!");
-                try {
-					player.play();
-				} catch (JavaLayerException e) {
-					e.printStackTrace();
-				}
-                System.out.println("Terminado!");
-			}};
-			
-			playerThread.start();
-            
-			while (true) {
-				sleepForABit();
-				playerThread.suspend();
-				sleepForABit();
-				playerThread.resume();
+		FileInputStream fis = new FileInputStream(mp3);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		player = new Player(bis);
+
+		final Thread playerThread = new Thread() { @Override public void run() {
+			System.out.println("Tocando!");
+			try {
+				player.play();
+			} catch (JavaLayerException e) {
+				e.printStackTrace();
 			}
-     }
+			System.out.println("Terminado!");
+		}};
+
+		playerThread.start();
+
+		int i = 0;
+		while (i++ < 3) {
+			sleepForABit();
+			playerThread.suspend();
+			sleepForABit();
+			playerThread.resume();
+		}
+
+		playerThread.stop();
+	}
 
 	private void sleepForABit() {
 		try {
